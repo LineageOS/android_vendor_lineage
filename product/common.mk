5e75a672fe91eace11bdfed2e8770c7131207f52
ctso <ctsoyars@gmail.com>
2010-06-25 02:42:22 +0000

    Add some comments

--- product/common.mk
+++ product/common.mk
@@ -1,19 +1,26 @@
+# Generic cyanogenmod product
 PRODUCT_NAME := cyanogen
 PRODUCT_BRAND := cyanogen
 PRODUCT_DEVICE := generic
 
+# Use edify for otapackage
 TARGET_OTA_SCRIPT_MODE = edify
 
+# Add ROMManager build property
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=cyanogenmod
 
+# CyanogenMod specific product packages
 PRODUCT_PACKAGES += \
     CMParts \
     Superuser
 
+# Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/CHANGELOG:system/etc/CHANGELOG-CM.txt
 
+# Allow the use of overlays
 PRODUCT_PACKAGE_OVERLAYS := vendor/cyanogen/overlay
 
+# Always run in insecure mode, enables root on user build variants
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
