8d1442d737e839365470d6f2a561997757d80d22
ctso <ctsoyars@gmail.com>
2010-06-25 02:14:22 +0000

    Added get-rommanager script

---
+++ get-rommanager.sh
@@ -0,0 +1,3 @@
+mkdir -p proprietary
+cd proprietary
+curl -O http://www.droidaftermarket.com/koush/recoveries/RomManager.apk
--- product/cyanogen_passion.mk
+++ product/cyanogen_passion.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/htc/passion/full_passion.mk)
+$(call inherit-product, vendor/cyanogen/product/common.mk)
 
 PRODUCT_NAME := cyanogen_passion
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := passion
 PRODUCT_MODEL := Nexus One
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_POLICY := android.policy_phone
