074be68e275ce1c9f337cd8e8c2b9d6cb8b6fcec
ctso <ctsoyars@gmail.com>
2010-06-25 02:35:47 +0000

    Add bootanimation, build prop overrides

--- product/cyanogen_passion.mk
+++ product/cyanogen_passion.mk
@@ -4,13 +4,20 @@ $(call inherit-product, device/htc/passion/full_passion.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/product/common.mk)
 
+#
+# Setup device specific product configuration.
+#
 PRODUCT_NAME := cyanogen_passion
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := passion
 PRODUCT_MODEL := Nexus One
 PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF83 BUILD_DISPLAY_ID=FRF83 PRODUCT_NAME=passion BUILD_FINGERPRINT=/passion/passion/mahimahi:2.2/FRF83/42295:user/release-keys
+PRIVATE_BUILD_DESC="passion-user 2.2 FRF83 42295 release-keys"
 
+#
 # Set ro.modversion
+#
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-N1
@@ -18,3 +25,9 @@ else
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.modversion=CyanogenMod-6.0.0-N1-test0
 endif
+
+#
+# Copy passion specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/passion/media/bootanimation.zip:system/media/bootanimation.zip
