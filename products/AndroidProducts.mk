ba467e7a410e3c9eabd1764bfaaea3e397501d6f
Lox <lox.dev@knc.nc>
2010-07-03 00:41:53 +1100

    Hero support

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,7 @@
 PRODUCT_MAKEFILES := \
         $(LOCAL_DIR)/cyanogen_passion.mk \
         $(LOCAL_DIR)/cyanogen_inc.mk \
+        $(LOCAL_DIR)/cyanogen_hero.mk \
         $(LOCAL_DIR)/cyanogen_heroc.mk \
         $(LOCAL_DIR)/cyanogen_sholes.mk \
 	$(LOCAL_DIR)/cyanogen_dream_sapphire.mk \
