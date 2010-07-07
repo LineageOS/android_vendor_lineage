b3cd9b10c569e9eeb96468aad832c9923c4b8011
Chris Soyars <ctsoyars@gmail.com>
2010-07-07 02:56:20 +0000

    Hacky fix to include the correct espresso kernel.  This needs to be fixed in kernel.mk.

--- products/cyanogen_espresso.mk
+++ products/cyanogen_espresso.mk
@@ -34,3 +34,5 @@ endif
 #
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/espresso/media/bootanimation.zip:system/media/bootanimation.zip
+
+TARGET_PREBUILT_KERNEL := device/htc/espresso/kernel
