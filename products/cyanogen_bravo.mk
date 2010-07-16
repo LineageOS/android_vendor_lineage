0d1e1369bdb42d040b7b432f76a75e376282157d
Kali- <kalimero@ngi.it>
2010-07-17 01:45:00 +0200

    [bravo] Add Italian emergency number

--- products/cyanogen_bravo.mk
+++ products/cyanogen_bravo.mk
@@ -27,7 +27,7 @@ TARGET_PREBUILT_KERNEL = device/htc/bravo/kernel/kernel
 # Extra RIL settings
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.managed.roaming=1 \
-    ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
+    ro.ril.oem.nosim.ecclist=911,112,113,115,117,999,000,08,118,120,122,110,119,995 \
     ro.ril.emc.mode=2
 
 # Enable Windows Media
