1a2d5fa8ce5f3d5c3c61dbd8abdb1ee66cb5a1ec
Steve Kondik <shade@chemlab.org>
2010-07-19 20:58:35 -0400

    supersonic: update build info

--- products/cyanogen_supersonic.mk
+++ products/cyanogen_supersonic.mk
@@ -12,8 +12,7 @@ PRODUCT_BRAND := sprint
 PRODUCT_DEVICE := supersonic
 PRODUCT_MODEL := PC36100
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 PRODUCT_NAME=htc_supersonic BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys
-PRIVATE_BUILD_DESC="supersonic-user 2.2 FRF91 43546 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 PRODUCT_NAME=htc_supersonic BUILD_FINGERPRINT=sprint/htc_supersonic/supersonic/supersonic:2.1-update1/ERE27/195459:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="1.47.651.1 CL195459 release-keys"
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/htc/supersonic/kernel
 
@@ -22,8 +21,8 @@ PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/htc/supersonic/kernel
 #PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
 #PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_mahimahi_defconfig
 
-# Extra Passion overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/passion
+# Extra Supersonic overlay (nothing currently)
+#PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/supersonic
 
 #
 # Set ro.modversion
