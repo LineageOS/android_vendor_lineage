613a6dd37b7094a0d6b3f5c22e64add7bd4e2239
Steve Kondik <shade@chemlab.org>
2010-08-04 08:06:46 -0400

    espresso: Remove junk

--- products/cyanogen_espresso.mk
+++ products/cyanogen_espresso.mk
@@ -15,7 +15,7 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := espresso
 PRODUCT_MODEL := myTouch 3G Slide
 PRODUCT_MANUFACTURER := T-Mobile
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 PRODUCT_NAME=passion BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="passion-user 2.2 FRF91 43546 release-keys" TARGET_DEVICE=passion
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 PRODUCT_NAME=passion BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2 FRF91 43546 release-keys"
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-espresso.map
 
