50a236ee81600b303125c7bb683eaf8760b3bf7d
Steve Kondik <shade@chemlab.org>
2010-08-05 09:29:36 -0400

    liberty: Add support for HTC Liberty (Aria) - attn1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,4 @@ add_lunch_combo cyanogen_dream_sapphire-eng
 add_lunch_combo cyanogen_bravo-eng
 add_lunch_combo cyanogen_espresso-eng
 add_lunch_combo cyanogen_supersonic-eng
+add_lunch_combo cyanogen_liberty-eng
