7f6aaed64ef9fc94bac8ec5f56baace9ffc37f56
Steve Kondik <shade@chemlab.org>
2010-08-09 08:18:05 -0400

    JNI lib for Torch not needed anymore.

--- products/cyanogen_espresso.mk
+++ products/cyanogen_espresso.mk
@@ -23,7 +23,7 @@ PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-
 PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/htc/espresso/kernel
 
 # Include the Torch app
-PRODUCT_PACKAGES += Torch libjni_flash
+PRODUCT_PACKAGES += Torch
 
 #
 # Set ro.modversion
