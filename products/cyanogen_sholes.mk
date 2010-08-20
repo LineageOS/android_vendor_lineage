7bb0a9f87393f65ca3f8a5b8bb5c8321b0603a48
Koushik Dutta <koushd@gmail.com>
2010-08-19 21:47:38 -0700

    rc3 for inc, rc4 or whatever for sholes

--- products/cyanogen_sholes.mk
+++ products/cyanogen_sholes.mk
@@ -24,7 +24,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Droid
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.0.0-Droid-RC3
+        ro.modversion=CyanogenMod-6.0.0-Droid-RC4
 endif
 
 PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=FRG01B BUILD_DISPLAY_ID=FRG01B PRODUCT_NAME=voles TARGET_DEVICE=sholes BUILD_FINGERPRINT=verizon/voles/sholes/sholes:2.2/FRG01B/45394:user/release-keys PRODUCT_BRAND=verizon PRIVATE_BUILD_DESC="voles-user 2.2 FRG01B 45394 release-keys" BUILD_NUMBER=45394 BUILD_UTC_DATE=1279140904 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
