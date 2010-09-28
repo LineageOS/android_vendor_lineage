3f62d6dd3a900f92b5756031d2bf78fd053c16a8
Steve Kondik <shade@chemlab.org>
2010-09-28 18:14:03 -0400

    vision: Add cyanogen_vision product.
    Change-Id: I976d4da12e2e02006be00ef2657c1e27a1fb71d7

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -11,4 +11,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_liberty.mk \
     $(LOCAL_DIR)/cyanogen_vibrant.mk \
-    $(LOCAL_DIR)/cyanogen_legend.mk
+    $(LOCAL_DIR)/cyanogen_legend.mk \
+    $(LOCAL_DIR)/cyanogen_vision.mk
