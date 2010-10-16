bab56215931e84cfc6ae4574800e4748c1608fda
Kali- <kalimero@ngi.it>
2010-10-16 22:15:37 +0200

    Verify cache size, output can't be redirected to /dev/null

--- prebuilt/common/bin/verify_cache_partition_size.sh
+++ prebuilt/common/bin/verify_cache_partition_size.sh
@@ -4,7 +4,7 @@
 #
 
 CACHESIZE=$(df -k /cache | tail -n1 | tr -s ' ' | cut -d ' ' -f2)
-if [ $CACHESIZE \< 60000 ]
+if [ $CACHESIZE -lt 60000 ]
 then
-  echo "dalvik.vm.dexopt-data-only=1" >> /system/build.prop > /dev/null 2>&1
+  echo "dalvik.vm.dexopt-data-only=1" >> /system/build.prop
 fi
