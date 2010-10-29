ae73f44e64a383140c8122486fc91fd105cc020a
Jacob Crawley <jak0b_2006@hotmail.co.uk>
2010-10-29 14:10:22 +0100

    Added HTC Wildfire files
    Change-Id: Iaf38ed050d82da03d983bbf3f95f426be876d575

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,6 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_dream_sapphire.mk \
     $(LOCAL_DIR)/cyanogen_bravo.mk \
     $(LOCAL_DIR)/cyanogen_bravoc.mk \
+    $(LOCAL_DIR)/cyanogen_buzz.mk \
     $(LOCAL_DIR)/cyanogen_espresso.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_liberty.mk \
---
+++ products/cyanogen_buzz.mk
@@ -0,0 +1,50 @@
+# Inherit AOSP device configuration for buzz.
+$(call inherit-product, device/htc/buzz/full_buzz.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Build kernel
+PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/htc/buzz/kernel
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_buzz
+PRODUCT_BRAND := htc_wwe
+PRODUCT_DEVICE := buzz
+PRODUCT_MODEL := HTC Wildfire
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+
+# Include the Torch app
+PRODUCT_PACKAGES += Torch
+
+# Extra Passion overlay
+PRODUCT_PACKAGE_OVERLAYS += device/htc/buzz/overlay
+
+# Extra RIL settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.oem.nosim.ecclist=911,112,113,115,117,999,000,08,118,120,122,110,119,995 \
+    ro.ril.emc.mode=2
+
+# Enable Windows Media
+WITH_WINDOWS_MEDIA := true
+
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-buzz
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-6.1.0-RC0-buzz
+endif
