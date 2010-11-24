056424b2c8c025d217c56b0ea06dc156cc59820c
Brint E. Kriebel <github@bekit.net>
2010-11-23 23:38:01 -0700

    Updated harmony build values to OTA values
    Change-Id: I5f08a11be25fdb654f172529ead409adf0ad46e0

--- products/cyanogen_harmony.mk
+++ products/cyanogen_harmony.mk
@@ -1,4 +1,3 @@
-# Inherit AOSP device configuration for passion.
 $(call inherit-product, device/nvidia/harmony/device_harmony.mk)
 
 # Inherit some common cyanogenmod stuff.
@@ -11,7 +10,11 @@ PRODUCT_NAME := cyanogen_harmony
 PRODUCT_BRAND := nvidia
 PRODUCT_DEVICE := harmony
 PRODUCT_MODEL := GTablet
-PRODUCT_MANUFACTURER := Viewsonic
+PRODUCT_MANUFACTURER := malata
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 PRODUCT_NAME=harmony BUILD_FINGERPRINT=Flextronics/harmony/harmony/:2.2/FRF91/hudson-20101122-122046-TnT_SVN_2967:user/test-keys PRODUCT_BRAND=Flextronics TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=test-keys PRIVATE_BUILD_DESC="harmony-user 2.2 FRF91 hudson-20101122-122046-TnT_SVN_2967 test-keys" PRODUCT_MODEL=UPC300-2.2 PRODUCT_MANUFACTURER=malata
+
+# Extra overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/harmony
 
 #
 # Set ro.modversion
@@ -21,10 +24,11 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Harmony
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.1.0-Beta3-Harmony
+        ro.modversion=CyanogenMod-6.1.0-Beta4-Harmony
 endif
 
+#
+# Copy passion specific prebuilt files
+#
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/harmony
