bfae7232a9a0b33a8556f1f68268bce35e0b56f4
Giulio Cervera <giulio.cervera@gmail.com>
2010-11-25 20:18:44 +0100

    cm: bravo/bravoc to 6.1.0-RC2
    Change-Id: If5aa6c8700d3656d95d216ecfdae2e2a12537c00

--- products/cyanogen_bravoc.mk
+++ products/cyanogen_bravoc.mk
@@ -36,7 +36,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-BravoC
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.1.0-RC1-BravoC
+        ro.modversion=CyanogenMod-6.1.0-RC2-BravoC
 endif
 
 #
