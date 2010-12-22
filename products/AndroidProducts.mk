eaf9273f67e3b4f582bd9e254bbd2692ec064850
loxK <lox.dev@knc.nc>
2010-12-22 14:10:08 +1100

    cm: Enable hero
    Change-Id: I071fccbf37d48e24e30deaaffa51c6659a9fcc8d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,6 +4,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_bravoc.mk \
     $(LOCAL_DIR)/cyanogen_buzz.mk \
     $(LOCAL_DIR)/cyanogen_crespo.mk \
+    $(LOCAL_DIR)/cyanogen_hero.mk \
     $(LOCAL_DIR)/cyanogen_inc.mk \
     $(LOCAL_DIR)/cyanogen_generic.mk \
     $(LOCAL_DIR)/cyanogen_glacier.mk \
@@ -18,6 +19,4 @@ PRODUCT_MAKEFILES := \
 #    $(LOCAL_DIR)/cyanogen_dream_sapphire.mk \
 #    $(LOCAL_DIR)/cyanogen_espresso.mk \
 #    $(LOCAL_DIR)/cyanogen_harmony.mk \
-#    $(LOCAL_DIR)/cyanogen_hero.mk \
 #    $(LOCAL_DIR)/cyanogen_heroc.mk \
-#    $(LOCAL_DIR)/cyanogen_legend.mk \
