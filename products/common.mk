e72120563cbfea458a83afda17c1467bb12b0654
Steve Kondik <shade@chemlab.org>
2011-01-02 01:41:06 -0500

    cm: Build CMWallpapers
    Change-Id: Ib154bfcf24c4997f24d24b3548e641273e6b5b9f

--- products/common.mk
+++ products/common.mk
@@ -42,8 +42,8 @@ PRODUCT_PACKAGES += \
     FileManager \
     CMParts \
     CMStats \
-    CMUpdateNotify
-#    CMWallpapers \
+    CMUpdateNotify \
+    CMWallpapers 
 #    DSPManager
 
 # Extra tools in CyanogenMod
