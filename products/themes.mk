151a434e6217fc09e258f3223bf6a6dc2a06f3c5
Steve Kondik <shade@chemlab.org>
2011-01-21 20:46:54 -0500

    Add feature definition for themes.
    Change-Id: I15223fc69bb19de2fe0f8987267c537839e76d2e

--- products/themes.mk
+++ products/themes.mk
@@ -5,3 +5,7 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes \
        Androidian \
        Cyanbread
+
+PRODUCT_COPY_FILES += \
+       vendor/cyanogen/prebuilt/common/etc/permissions/com.tmobile.themes.xml:system/etc/permissions/com.tmobile.themes.xml
+
