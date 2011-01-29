0d6d3991377e3d75cd6f931604b862c0ee3c4788
Steve Kondik <shade@chemlab.org>
2011-01-28 19:03:12 -0500

    cm: Add default alarm tone
    Change-Id: Ic4143410b4b36c04724ac69703b3c2cde8bc174d

--- products/common_full.mk
+++ products/common_full.mk
@@ -10,5 +10,5 @@ include vendor/cyanogen/products/themes.mk
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Playa.ogg \
-    ro.config.notification_sound=regulus.ogg
-
+    ro.config.notification_sound=regulus.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
