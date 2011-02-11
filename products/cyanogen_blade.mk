3c9d0df67688e8ac731c1fc6a4e3cbeee37204ad
Tom Giordano <tomgiordano83@gmail.com>
2011-02-11 21:44:39 +1100

    Merge ZTE Blade vendor config.
    The device config should be forked from https://github.com/TomGiordano/android_device_zte_blade
    
    Change-Id: If9c6ebdbc361c0e10c5009b8baf9c27343417ac7

---
+++ products/cyanogen_blade.mk
@@ -0,0 +1,40 @@
+# Inherit AOSP device configuration for blade.
+$(call inherit-product, device/zte/blade/device_blade.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_blade
+PRODUCT_BRAND := zte
+PRODUCT_DEVICE := blade
+PRODUCT_MODEL := Blade
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cyanogen_blade BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Blade
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-RC0-Blade
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-RC0-Blade-KANG
+    endif
+endif
+
+#
+# Copy legend specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
