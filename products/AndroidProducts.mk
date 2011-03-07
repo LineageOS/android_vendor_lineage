a8b21444a986ad8855792faf67a6029876559a73
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-03-07 14:43:24 +0000

    Add ZERO product
    Change-Id: I208af430c4a5f17c45b6276e166a25a349c19a7f

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,9 +21,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_one.mk \
     $(LOCAL_DIR)/cyanogen_passion.mk \
     $(LOCAL_DIR)/cyanogen_sholes.mk \
+    $(LOCAL_DIR)/cyanogen_speedy.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_vega.mk \
     $(LOCAL_DIR)/cyanogen_vision.mk \
     $(LOCAL_DIR)/cyanogen_z71.mk \
-    $(LOCAL_DIR)/cyanogen_speedy.mk
-
+    $(LOCAL_DIR)/cyanogen_zero.mk
---
+++ products/cyanogen_zero.mk
@@ -0,0 +1,43 @@
+# Inherit AOSP device configuration for zero.
+$(call inherit-product, device/geeksphone/zero/zero.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_zero
+PRODUCT_BRAND := geeksphone
+PRODUCT_DEVICE := zero
+PRODUCT_MODEL := Geeksphone ZERO
+PRODUCT_MANUFACTURER := Geeksphone
+
+## Needs to start with platform to be recognized by 
+## automagic proprietary blobs...
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=msm7627_zero PRODUCT_NAME=geeksphone_zero BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-ZERO
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-RC2-ZERO
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-RC2-ZERO-KANG
+    endif
+endif
+
+#
+# Copy legend specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
