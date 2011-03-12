39602dd9e28d42c731f5a29c853eb22276eba646
Arne Coucheron <arco68@gmail.com>
2011-03-12 08:14:14 +0100

    buzz: Complete vendor overhaul, part 2/2
    Change-Id: Ibf87e2b383f6f7b7c154e1398aa32e070a70e25f

--- products/cyanogen_buzz.mk
+++ products/cyanogen_buzz.mk
@@ -1,14 +1,11 @@
 # Inherit AOSP device configuration for buzz.
-$(call inherit-product, device/htc/buzz/full_buzz.mk)
+$(call inherit-product, device/htc/buzz/buzz.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
-
-# Build kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/htc/buzz/prebuilt/kernel
 
 #
 # Setup device specific product configuration.
@@ -18,26 +15,18 @@ PRODUCT_BRAND := htc_wwe
 PRODUCT_DEVICE := buzz
 PRODUCT_MODEL := HTC Wildfire
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_buzz BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
 
-# Extra Passion overlay
-PRODUCT_PACKAGE_OVERLAYS += device/htc/buzz/overlay
-
-# Extra RIL settings
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.enable.managed.roaming=1 \
-    ro.ril.oem.nosim.ecclist=911,112,113,115,117,999,000,08,118,120,122,110,119,995 \
-    ro.ril.emc.mode=2
-
-# Add the Torch app
-PRODUCT_PACKAGES += Torch
+# Extra overlay for LDPI
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
 # Broadcom FM radio
 $(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
-# Enable Windows Media
-WITH_WINDOWS_MEDIA := true
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
 
+# Copy bootanimation
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
 
