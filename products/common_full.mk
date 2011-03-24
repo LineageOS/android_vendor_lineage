7935a8e4c3840cfe3fca1dd5066b379fd7bb3db3
Steve Kondik <shade@chemlab.org>
2011-03-24 03:22:06 -0400

    Moving extra dictionaries to a separate overlay.
    This reduces size greatly when using small_base.
    
    Change-Id: I135e54c1f225c807d1f7a11af2f2cc9aa75889b1

--- products/common_full.mk
+++ products/common_full.mk
@@ -7,6 +7,9 @@ include frameworks/base/data/sounds/AllAudio.mk
 # Theme packages
 include vendor/cyanogen/products/themes.mk
 
+# Include extra dictionaries for LatinIME
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/dictionaries
+
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Playa.ogg \
