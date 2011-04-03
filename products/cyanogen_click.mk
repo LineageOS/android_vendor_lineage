7f60c49ae4bf48ab8d53dca82a427f1b7af3d795
KalimochoAz <calimochoazucarado@gmail.com>
2011-04-03 20:00:48 +0200

    Added RC4.1 due to all issues solved for Click device
    Change-Id: I38014debb4dd7673d045fa076ab7b3a81dbaaa9a

--- products/cyanogen_click.mk
+++ products/cyanogen_click.mk
@@ -39,9 +39,9 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-RC4-click
+            ro.modversion=CyanogenMod-7.0.0-RC4.1-click
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-RC4-click-KANG
+            ro.modversion=CyanogenMod-RC4.1-click-KANG
     endif
 endif
