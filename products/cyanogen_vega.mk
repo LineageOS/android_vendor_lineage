edfc0b74f57ec06a05d9d4349584eb25dd15d923
FUKAUMI Naoki <naobsd@gmail.com>
2011-05-13 15:37:10 +0900

    Revert "Vega: skip themes at the right place"
    This reverts commit 581539d85e63278940fad100276698ee25660e9b.

--- products/cyanogen_vega.mk
+++ products/cyanogen_vega.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/advent/vega/vega.mk)
 
-# skips themes and keeps the resulting rom tiny so it fits on /system/ with all languages included
-SKIP_THEMES := true
-
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
