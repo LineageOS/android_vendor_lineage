e00c5065f8d9b8a1d0d91e2eedf14b8ae4aec301
Koushik Dutta <koushd@gmail.com>
2011-05-18 11:36:54 -0700

    add vibrant
    Change-Id: I9508eaab56007295ac3773f3424f6118c3f0351c

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -29,6 +29,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_speedy.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_vega.mk \
+    $(LOCAL_DIR)/cyanogen_vibrantmtd.mk \
     $(LOCAL_DIR)/cyanogen_vision.mk \
     $(LOCAL_DIR)/cyanogen_z71.mk \
     $(LOCAL_DIR)/cyanogen_zeppelin.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -28,6 +28,7 @@ add_lunch_combo cyanogen_sholes-eng
 add_lunch_combo cyanogen_speedy-eng
 add_lunch_combo cyanogen_supersonic-eng
 add_lunch_combo cyanogen_vega-eng
+add_lunch_combo cyanogen_vibrantmtd-eng
 add_lunch_combo cyanogen_vision-eng
 add_lunch_combo cyanogen_z71-eng
 add_lunch_combo cyanogen_zeppelin-eng
