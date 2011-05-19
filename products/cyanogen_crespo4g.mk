e66a28ba6fd542511665d9694494b70388748d25
joeykrim <joeykrim@teamw.in>
2011-05-19 00:09:15 -0400

    crespo4g dekang cyanogen vendor to better suit the ns4g
    Change-Id: I9674cf1d33437066dd3d57ed23b9b6a843b5ed23

--- products/cyanogen_crespo4g.mk
+++ products/cyanogen_crespo4g.mk
@@ -4,16 +4,13 @@ $(call inherit-product, device/samsung/crespo4g/full_crespo4g.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
-# Include GSM stuff
-$(call inherit-product, vendor/cyanogen/products/gsm.mk)
-
 #
 # Setup device specific product configuration.
 #
 PRODUCT_NAME := cyanogen_crespo4g
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := crespo4g
-PRODUCT_MODEL := Nexus S
+PRODUCT_MODEL := Nexus S 4G
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys" BUILD_NUMBER=121341
 
@@ -22,12 +19,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/crespo4g
 
 # Add the Torch app
 PRODUCT_PACKAGES += Torch
-
-# Extra RIL settings
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.enable.managed.roaming=1 \
-    ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
-    ro.ril.emc.mode=2
 
 #
 # Set ro.modversion
