262bd3ce6cc2d2d7665394ea6fa00c487f9d8c9d
Ladios Jonquil <ladios.jonquil@gmail.com>
2011-05-28 05:18:02 +0800

    gsm: Add SPN overrides list for localized carrier names
    Change-Id: I8c51eb9e571f049d9b93e81fee4e574abca9d68a

--- products/gsm.mk
+++ products/gsm.mk
@@ -2,6 +2,10 @@
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# GSM SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/cyanogen/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
