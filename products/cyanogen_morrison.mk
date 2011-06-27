7ce05a2061d7e7bf1519ef0a7a36a5c643247c99
Emilio López <turl@tuxfamily.org>
2011-06-27 02:22:43 +0000

    morrison: remove -KANG on release builds
    Someone added the KANG prefix on release builds, this removes it.
    
    Change-Id: Ic944be5a65afdac6b01e779ca6647735d730e3b0

--- products/cyanogen_morrison.mk
+++ products/cyanogen_morrison.mk
@@ -26,7 +26,7 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC1-CLIQ-KANG
+            ro.modversion=CyanogenMod-7.1.0-RC1-CLIQ
     else
         PRODUCT_PROPERTY_OVERRIDES += \
             ro.modversion=CyanogenMod-7.1.0-RC1-CLIQ-KANG
