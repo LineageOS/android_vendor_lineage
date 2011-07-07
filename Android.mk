1568978ecaa143d6a63e3fcfd31778332f25f2dc
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-07-07 10:17:06 +0200

    Use only present proprietary files some apk doesnt exists anymore, and HTC stuff must not be present here Also dont ignore LatinIME required to have a least one keyboard
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>
    
    Change-Id: I8c5852c6038d3baec65d8f6af7caae5372770679

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # We need a way to prevent the stuff Google Apps replaces from being included in the build.
 # This is a hacky way to do that.
 ifdef CYANOGEN_WITH_GOOGLE
-    PACKAGES.Email.OVERRIDES := Provision LatinIME QuickSearchBox
+    PACKAGES.Email.OVERRIDES := Provision QuickSearchBox
 endif
