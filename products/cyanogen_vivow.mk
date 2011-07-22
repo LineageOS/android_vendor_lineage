d2d7216bad4e7f92926ce0ac77f6eb45e7b42a32
Markus Guidry <markus.guidry@gmail.com>
2011-07-22 18:43:41 -0400

    vivow: update fingerprint
    Change-Id: Id9468f30904a235c62eaff31cc0925b27f78c1ea

--- products/cyanogen_vivow.mk
+++ products/cyanogen_vivow.mk
@@ -8,11 +8,11 @@ $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 # Setup device specific product configuration.
 #
 PRODUCT_NAME := cyanogen_vivow
-PRODUCT_BRAND := htc_wwe
+PRODUCT_BRAND := verizon_wwe
 PRODUCT_DEVICE := vivow
 PRODUCT_MODEL := Incredible 2
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=htc_wwe/htc_vivow/vivow:2.3.3/GRI40/47853:user/release-keys PRIVATE_BUILD_DESC="2.36.405.8 CL47853 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.3/GRI40/81006:user/release-keys PRIVATE_BUILD_DESC="2.18.605.4 CL81006 release-keys"
 
 # Build kernel
 #PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
