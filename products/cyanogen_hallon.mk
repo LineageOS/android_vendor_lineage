7412ec953f07b9ea88d76b59eb58c2015d3a45fb
Diogo Ferreira <defer@cyanogenmod.com>
2011-09-19 20:06:56 +0100

    hallon: Enable torch
    Change-Id: I80ae1c2c0d29089d7d5069fbbb3107da03029c1c

--- products/cyanogen_hallon.mk
+++ products/cyanogen_hallon.mk
@@ -26,7 +26,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT15i BUILD_ID=3.0.1.A.0.145 BUILD_
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/hallon
 
 # Add the Torch app
-#PRODUCT_PACKAGES += Torch
+PRODUCT_PACKAGES += Torch
 
 
 # BCM FM radio
