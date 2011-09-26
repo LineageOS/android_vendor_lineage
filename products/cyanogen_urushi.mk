ee8be0b096489b656c70630e98c744d88a954306
Diogo Ferreira <defer@cyanogenmod.com>
2011-09-26 16:42:59 +0100

    cyanogen: Add Sony Ericsson Xperia Ray (urushi, ST18i)
    Build target: cyanogen_urushi-eng
    
    Change-Id: Iaebe0d7e6a97331d07596f58cab398e223fe58f6

---
+++ products/cyanogen_urushi.mk
@@ -0,0 +1,55 @@
+# Inherit device configuration for urushi.
+$(call inherit-product, device/semc/urushi/device_urushi.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_urushi
+PRODUCT_BRAND := SEMC
+PRODUCT_DEVICE := urushi
+PRODUCT_MODEL := ST18i
+PRODUCT_MANUFACTURER := Sony Ericsson
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST18i BUILD_ID=4.0.A.2.368 BUILD_DISPLAY_ID=4.0.A.2.368 BUILD_FINGERPRINT=SEMC/LT15i_1247-1061/LT15i:2.3.3/3.0.1.A.0.145/bn_P:user/release-keys PRIVATE_BUILD_DESC="LT15i-user 2.3.3 3.0.1.A.0.145 bn_P test-keys"
+
+# Build kernel
+#PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_urushi_defconfig
+
+# Extra urushi overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/urushi
+
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
+
+
+# BCM FM radio
+#$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-XperiaRay-ST18i
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC1-XperiaArc-ST18i
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC1-XperiaArc-ST18i-KANG
+    endif
+endif
+
+#
+# Copy passion specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
