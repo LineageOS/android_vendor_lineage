fb25087b0a00ea5b85c4ab666dc82d96ce807dc1
Danny Baumann <dannybaumann@web.de>
2011-10-17 17:04:55 +0200

    Clean up Defy product overrides.
    Change-Id: Id56d216cf589b031c05b43c18750a57d6ad454f9

--- products/cyanogen_jordan.mk
+++ products/cyanogen_jordan.mk
@@ -7,6 +7,12 @@ $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
+# Extra jordan overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/jordan
+
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
+
 #
 # Setup device specific product configuration.
 #
@@ -19,32 +25,27 @@ PRODUCT_SBF := 3.4.2-179-4
 PRODUCT_SFX := JOREM_U3
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := Jordan
+PRODUCT_RELEASE_NAME := Defy
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
 UTC_DATE := $(shell date +%s)
 DATE     := $(shell date +%Y%m%d)
 
-PRODUCT_BUILD_PROP_OVERRIDES := \
-BUILD_ID=GWK74 \
-PRODUCT_NAME=${PRODUCT_MODEL}_${PRODUCT_SFX} \
-TARGET_DEVICE=umts_jordan \
-BUILD_FINGERPRINT=MOTO/${PRODUCT_MODEL}_${PRODUCT_SFX}/umts_jordan/jordan:2.3.7/${PRODUCT_SBF}/${UTC_DATE}:user/release-keys \
-PRODUCT_BRAND=MOTO \
-PRIVATE_BUILD_DESC="umts_jordan-user 2.3.7 ${PRODUCT_SFX}_${PRODUCT_SBF} ${UTC_DATE} release-keys" \
-BUILD_NUMBER=${DATE} \
-BUILD_VERSION_TAGS=release-keys \
-TARGET_BUILD_TYPE=user \
-USER=CyanogenDefy
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_ID=GWK74 \
+    PRODUCT_NAME=${PRODUCT_MODEL}_${PRODUCT_SFX} \
+    TARGET_DEVICE=umts_jordan \
+    BUILD_FINGERPRINT=MOTO/${PRODUCT_MODEL}_${PRODUCT_SFX}/umts_jordan/jordan:2.3.7/${PRODUCT_SBF}/${UTC_DATE}:user/release-keys \
+    PRIVATE_BUILD_DESC="umts_jordan-user 2.3.7 ${PRODUCT_SFX}_${PRODUCT_SBF} ${UTC_DATE} release-keys" \
+    BUILD_NUMBER=${DATE} \
+    BUILD_VERSION_TAGS=release-keys \
+    TARGET_BUILD_TYPE=user
 
+#
+# Copy jordan specific prebuilt files
+#
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/cyanogen/overlay/jordan
-
-# Add the Torch app
-PRODUCT_PACKAGES += \
-    Torch
 
