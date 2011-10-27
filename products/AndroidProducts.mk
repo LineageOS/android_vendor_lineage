91097a6627d16a51835ba5408ff2dc5e718e6bb8
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-10-27 23:48:39 +0100

    CM: Add p970 device (LGE Optimus Black)
    Change-Id: Ia0e512738f7133e5175502417ca78837f6b8be72

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -40,6 +40,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_motus.mk \
     $(LOCAL_DIR)/cyanogen_one.mk \
     $(LOCAL_DIR)/cyanogen_olympus.mk \
+    $(LOCAL_DIR)/cyanogen_p970.mk \
     $(LOCAL_DIR)/cyanogen_p990.mk \
     $(LOCAL_DIR)/cyanogen_p999.mk \
     $(LOCAL_DIR)/cyanogen_passion.mk \
