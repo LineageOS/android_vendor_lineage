f66a117935a76c2a7120f44585e4cbb5923bc2a4
Arne Coucheron <arco68@gmail.com>
2011-10-30 05:20:04 +0100

    buzz: Add needed prelinker map for updated blobs
    Pull request for updated blobs: https://github.com/koush/proprietary_vendor_htc/pull/35
    
    Change-Id: Ic122d83e4f5df60d6c84e89fbe897ad0f2284899

--- products/cyanogen_buzz.mk
+++ products/cyanogen_buzz.mk
@@ -17,6 +17,8 @@ PRODUCT_MODEL := HTC Wildfire
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_buzz BUILD_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
 
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-msm722x.map
+
 # Add LDPI assets, in addition to MDPI
 PRODUCT_LOCALES += ldpi mdpi
 
