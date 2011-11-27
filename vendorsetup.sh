eb50dfef87331b7dcbf4c0c69540323087f4b11d
Diogo Ferreira <defer@cyanogenmod.com>
2011-11-27 11:26:50 +0000

    vendor_cm: Reorder the vendorsetup entries
    This will make my OCD happy for now.
    
    Change-Id: I4bc85c1acee8fd7b222ebb15ddf6ea39428fa758

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
+add_lunch_combo cm_crespo-userdebug
+add_lunch_combo cm_GT_P7510-userdebug
+add_lunch_combo cm_maguro-userdebug
 add_lunch_combo cm_p920-userdebug
 add_lunch_combo cm_p990-userdebug
 add_lunch_combo cm_p999-userdebug
-add_lunch_combo cm_crespo-userdebug
-add_lunch_combo cm_maguro-userdebug
-add_lunch_combo cm_GT_P7510-userdebug
