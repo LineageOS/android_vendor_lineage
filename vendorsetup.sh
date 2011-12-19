336c61838bd612a448f4e8f6e2bf2c62ac52434c
Steve Kondik <shade@chemlab.org>
2011-12-18 16:21:41 -0800

    Rename GT_P7510 to p4wifi
    Change-Id: I85e698cbcdaf7aca513c9d32e62fae2c47c5fd35

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 add_lunch_combo cm_crespo-userdebug
 add_lunch_combo cm_crespo4g-userdebug
-add_lunch_combo cm_GT_P7510-userdebug
+add_lunch_combo cm_p4wifi-userdebug
 add_lunch_combo cm_maguro-userdebug
 add_lunch_combo cm_p920-userdebug
 add_lunch_combo cm_p970-userdebug
