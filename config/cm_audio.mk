7dfaca379bf9276fb32d7c982cd795f3ab1cac94
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-05-22 23:36:03 +0200

    added audio files contributed by "Dennis Cleiren"
    Change-Id: I4266d72dc8975308fe47408a3a97ca234616a539

--- config/cm_audio.mk
+++ config/cm_audio.mk
@@ -16,9 +16,18 @@ PRODUCT_COPY_FILES += \
 	$(NOTIFICATION_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
 	$(NOTIFICATION_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
 	$(NOTIFICATION_PATH)/CyanPing.ogg:system/media/audio/notifications/CyanPing.ogg \
+	$(NOTIFICATION_PATH)/Doink.ogg:system/media/audio/notifications/Doink.ogg \
 	$(NOTIFICATION_PATH)/Naughty.ogg:system/media/audio/notifications/Naughty.ogg \
-	$(NOTIFICATION_PATH)/Pong.ogg:system/media/audio/notifications/Pong.ogg
+	$(NOTIFICATION_PATH)/Pong.ogg:system/media/audio/notifications/Pong.ogg \
+	$(NOTIFICATION_PATH)/Rang.ogg:system/media/audio/notifications/Rang.ogg \
+	$(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/Stone.ogg
 
 # Ringtones
 PRODUCT_COPY_FILES += \
-	$(RINGTONE_PATH)/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg
+	$(RINGTONE_PATH)/Bongo.ogg:system/media/audio/ringtones/Bongo.og \
+	$(RINGTONE_PATH)/Boxbeat.ogg:system/media/audio/ringtones/Boxbeat.ogg \
+	$(RINGTONE_PATH)/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg \
+	$(RINGTONE_PATH)/Gigolo.ogg:system/media/audio/ringtones/Gigolo.ogg \
+	$(RINGTONE_PATH)/House_of_house.ogg:system/media/audio/ringtones/House_of_house.ogg \
+	$(RINGTONE_PATH)/Silmarillia.ogg:system/media/audio/ringtones/Silmarillia.ogg
+
