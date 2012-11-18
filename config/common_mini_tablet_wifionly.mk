fb9eb2963789d0b183c73f5845b050b9ade58505
Takazumi Matsumoto <tdr@taksmon.com>
2012-11-18 04:56:49 +0000

    cm: Add mini tablet config
    For tablets with constrained system partitions (i.e. Xoom),
    leave out some optional packages.
    
    (From CM10 I86e5e98e269915189bb99b7b04a6d7e638dfbe38)
    
    Change-Id: I35ef53d37e234ab05d641d6f536d0155a68d9ee2

---
+++ config/common_mini_tablet_wifionly.mk
@@ -0,0 +1,23 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/cm/config/common.mk)
+
+# Bring in all audio files
+include frameworks/base/data/sounds/NewAudio.mk
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Default ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Orion.ogg \
+    ro.config.notification_sound=Deneb.ogg \
+    ro.config.alarm_alert=Hassium.ogg
+
+# BT config
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+endif
