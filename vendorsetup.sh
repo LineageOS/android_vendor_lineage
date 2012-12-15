eb74521850497399d3c7ecf536fdeb2bcbf3fa83
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-12-15 21:12:15 +0000

    Adjust vendorsetup to current build target location
    Change-Id: I1f6ff721f65e574cf46e967f1a76dffd3ab384f6

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(cat vendor/cm/jenkins-build-targets)
+for combo in $(wget -o /dev/null -O - https://raw.github.com/CyanogenMod/hudson/master/cm-daily-build-targets | grep cm-10.1 | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
