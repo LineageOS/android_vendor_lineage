918a7eaf8d0ddfad8a2c8f705417076ef74b7bbd
sleeplessninja <schafer.live@gmail.com>
2013-01-30 14:45:20 -0600

    Enable wireless Xbox 360 controller support

--- config/common.mk
+++ config/common.mk
@@ -109,6 +109,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+
 # This is CM!
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
