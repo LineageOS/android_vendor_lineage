b8e7c1519cce6704371a79edb089e1933712bd26
Chirayu Desai <cdesai@cyanogenmod.org>
2013-03-25 14:13:14 +0530

    add bash completion script for git
    From git.git, contrib/completion/git-completion.bash
    
    Change-Id: Ic7eb3cb01b1831ec20a1d99dee15171f7c919d54

--- bash_completion/repo.bash
+++ bash_completion/repo.bash
@@ -1,6 +1,6 @@
 # Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# found in the repo-LICENSE file.
 
 # Programmable completion for some Chromium OS build scripts.
 
