44ba0ac88c5a94d3581e94fdbde42be0b4b7b3b3
Brandon Bennett <bennetb@gmail.com>
2013-04-13 15:12:01 -0600

    Remove comments before parsing vendorsetup.sh
    Remove any comments that are preceeded by a '#' to ingore them as potential
    build targets.  This allows targets to be temporarily removed.
    
    Change-Id: Ie634f2d215ecdc8995a4324bbe874261bcd33c2f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(wget -o /dev/null -O - https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | grep cm-10.1 | awk {'print $1'})
+for combo in $(wget -o /dev/null -O - https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-10.1 | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
