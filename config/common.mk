19425dde4081f9308c4e4dab14ecfde2956d1148
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-25 19:48:55 +0530

    remove leading hypen from extraversion before adding one
    Change-Id: Ia33f564d272cae14f300c18d783a108ad09dfef6

--- config/common.mk
+++ config/common.mk
@@ -212,6 +212,8 @@ ifdef CM_BUILDTYPE
     ifdef CM_EXTRAVERSION
         # Force build type to EXPERIMENTAL
         CM_BUILDTYPE := EXPERIMENTAL
+        # Remove leading dash from CM_EXTRAVERSION
+        CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
         # Add leading dash to CM_EXTRAVERSION
         CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
     endif
