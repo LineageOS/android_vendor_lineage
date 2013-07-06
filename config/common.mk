2978e20d747968a82e419032ee89d6bb3901b319
Steve Kondik <shade@chemlab.org>
2013-07-06 04:07:56 -0700

    cm: Add ntfs-3g and exfat tools to the build
    Change-Id: Ie4e3a82deb39a108c559fe698fefdf03f2237a71

--- config/common.mk
+++ config/common.mk
@@ -174,7 +174,12 @@ PRODUCT_PACKAGES += \
     nano \
     htop \
     powertop \
-    lsof
+    lsof \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    ntfsfix \
+    ntfs-3g
 
 # Openssh
 PRODUCT_PACKAGES += \
