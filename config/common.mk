45e6598078e3a919ec644b8ad22bf9ad2912e876
Koushik Dutta <koushd@gmail.com>
2013-08-12 15:41:14 -0700

    add a hook to add extra packages
    Change-Id: I1f955a59a18bda22c4431d05a426aab91764d20d

--- config/common.mk
+++ config/common.mk
@@ -208,6 +208,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rsync
 
+# easy way to extend to add more packages
+-include vendor/extra/product.mk
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
