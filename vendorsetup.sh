a3310a38681cffdee44dbd1ad966c9d7b2447376
dhacker29 <davidhackerdvm@gmail.com>
2013-08-14 15:25:28 -0500

    vendorsetup: Use cm-10.2 lunch targets
    Change-Id: I6137840ae94904aa86f38f75055e6e35e5397bfb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-10.1 | awk {'print $1'})
+for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-10.2 | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
