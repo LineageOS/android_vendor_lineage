53df1df11e0718ea12d7e9b52ea6b7bab876b3f2
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-29 17:48:25 +0000

    Don't assume phones are the only clients for the telephony configuration bits
    Data-enabled tablets need love, too
    
    Change-Id: I239823f198260d0f045c6e7305cf04082a8ef1e9

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -15,15 +15,4 @@ ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
         vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
 endif
 
-# World APN list
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
-
+$(call inherit-product, vendor/cm/config/telephony.mk)
---
+++ config/telephony.mk
@@ -0,0 +1,12 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
+
