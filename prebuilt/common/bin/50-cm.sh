fc613a8cd4fa32f500dbc495b09f818341b26a03
David Gasaway <ponsasinorem@gmail.com>
2013-12-22 12:49:37 -1000

    50-cm.sh: update comment to CM11
    Change-Id: I678b9b2815dfda52189e69aa95b9cc0fb12f2d5a

--- prebuilt/common/bin/50-cm.sh
+++ prebuilt/common/bin/50-cm.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 # 
 # /system/addon.d/50-cm.sh
-# During a CM10 upgrade, this script backs up /system/etc/hosts,
+# During a CM11 upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
