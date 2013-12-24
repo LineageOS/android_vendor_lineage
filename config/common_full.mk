32bbdf6f5763c92b88f6de4c956603a403b7212f
Abhisek Devkota <ciwrl@cyanogenmod.com>
2013-12-23 22:55:26 -0800

    Remove unused audio mk files
    These are empty and useless.
    
    Change-Id: I23cd1be105f7bddbf555a35e10efcc92a7c1eb47

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,12 +1,6 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,9 +1,6 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
