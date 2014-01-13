2cfe6b260bf743cbf3091095807b373037581181
Dan Pasanen <dan.pasanen@gmail.com>
2014-01-13 07:33:19 -0600

    cm: add selective based spn (2/2)
    * patch set 1 to 7 doesn't fix the problem, it bruce forces the spn
    list to ALL DEVICES.
    * this change and http://review.cyanogenmod.org/#/c/57393/
    will attack the problem at lowest level, customized it for speed
    and memory.
    *  the best part is that it handles letters and exit the
    function when it find a letter in the response from ril.
    
    Change-Id: Ia63309acf38617e68b98ff1068463a942fa793df

--- config/telephony.mk
+++ config/telephony.mk
@@ -6,6 +6,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
+# Selective SPN list for operator number who has the problem. 
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     Mms \
