b9dd400b385b18519cad768b5ec45fcbe9973f22
Steve Kondik <shade@chemlab.org>
2014-09-10 15:41:43 -0700

    cm: Update use of vendor/extra `
     * Use inherit-product to pick up extra configuration
    
    Change-Id: Icb0c8b09cd10b503e2dca09b7a972a613c354e7d

--- config/common.mk
+++ config/common.mk
@@ -231,9 +231,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 endif
 
-# easy way to extend to add more packages
--include vendor/extra/product.mk
-
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION_MAJOR = 11
@@ -344,3 +341,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
 -include vendor/cyngn/product.mk
+
+$(call inherit-product-if-exists, vendor/extra/product.mk)
