e6a352f2d04ec2bc62610c74c8be3c38d9294663
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-01-05 12:33:19 -0800

    CM Legal - use https
    Change-Id: If802190875e5106e8f2bf5827069b87fbb738155

--- config/common.mk
+++ config/common.mk
@@ -289,7 +289,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
   ro.cm.releasetype=$(CM_BUILDTYPE) \
   ro.modversion=$(CM_VERSION) \
-  ro.cmlegal.url=http://www.cyanogenmod.org/docs/privacy
+  ro.cmlegal.url=https://www.cyanogenmod.org/docs/privacy
 
 -include vendor/cm-priv/keys/keys.mk
 
