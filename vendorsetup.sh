ba52ed6d4206c69d1dab7a433ad1ad44fffd98ef
AngelBob <cambercreak@gmail.com>
2015-01-11 05:39:16 +0000

    Lets do lunch with cm-12.0
    Change-Id: I36bf9dcc2d8b1ce4cb2fffd6ad40a2e7b30d221b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
+for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-12.0 | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
