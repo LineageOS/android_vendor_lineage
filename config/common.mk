f3550d1187a7baea95ca0621b1be057aa6fc55d5
Michael Bestas <mikeioannina@gmail.com>
2015-01-12 17:59:19 +0200

    Remove obsolete Superuser configuration
    Change-Id: I30b3c4a22f1b4cf747b96d83e3724ba2b9b9306b

--- config/common.mk
+++ config/common.mk
@@ -1,8 +1,5 @@
 PRODUCT_BRAND ?= cyanogenmod
 
-SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
-
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 # determine the smaller dimension
 TARGET_BOOTANIMATION_SIZE := $(shell \
