2fe8b5159ed4270a4439e0b6619a633a97540638
Michael Bestas <mikeioannina@gmail.com>
2015-01-28 02:06:28 +0200

    Don't build VoiceDialer
    Change-Id: I522934f88aed9a0daa35995944d49e3ab2b66416

--- config/telephony.mk
+++ config/telephony.mk
@@ -10,13 +10,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
-
 # Telephony packages
 PRODUCT_PACKAGES += \
     Mms \
     Stk \
     CellBroadcastReceiver \
-    VoiceDialer \
     WhisperPush
 
 # Mms depends on SoundRecorder for recorded audio messages
