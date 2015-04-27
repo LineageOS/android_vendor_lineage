70d7e18828953d43381e7e7a0dc70d74ae10ba35
Ricardo Cerqueira <ricardo@cyngn.com>
2015-04-27 15:06:49 +0100

    Kill spn-conf overrides
    These were overloading the ASCII names to region-specific charsets,
    which looks nice but it's horribly confusing if you're not native
    from those regions and happen to be roaming there.
    
    So... like our upstreams, just show the original carrier names
    
    Change-Id: Icb3c6c9be2a4ee57866f2f57411a4f441be88f7c

--- config/telephony.mk
+++ config/telephony.mk
@@ -2,10 +2,6 @@
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
