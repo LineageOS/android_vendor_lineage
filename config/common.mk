a6b8d30c4247fbaedc77cf2f1a47f87fd79145f4
Adnan Begovic <adnan@cyngn.com>
2015-05-13 03:57:05 -0700

    cm: Ignore filters for platform library.
    Change-Id: Iaf06872676e9e2745cb2f5ea7dcf1caf71443cb6

--- config/common.mk
+++ config/common.mk
@@ -151,13 +151,11 @@ PRODUCT_PACKAGES += \
     CMHome \
     CyanogenSetupWizard
 
-ifeq ($(filter NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
 # CM Platform Library
 PRODUCT_PACKAGES += \
     org.cyanogenmod.platform-res \
     org.cyanogenmod.platform \
     org.cyanogenmod.platform.xml
-endif
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
