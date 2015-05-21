e190d66b8aa9d3ed04074b354df4d1c4c2ed4552
Adnan Begovic <adnan@cyngn.com>
2015-05-21 14:59:07 -0700

    CM: Update droiddoc template for cmsdk
      Resolve index page.
    
    Change-Id: I5546f49f3d4b277686c2e07ed1ef0082b4a4b6ce

--- build/tools/droiddoc/templates-cmsdk/components/masthead.cs
+++ build/tools/droiddoc/templates-cmsdk/components/masthead.cs
@@ -1,8 +1,8 @@
 <?cs def:custom_masthead() ?>
   <div id="header">
       <div id="headerLeft">
-          <a href="<?cs var:toroot ?>index.html" tabindex="-1"><img
-              src="<?cs var:toroot ?>assets/cid_smart.png" alt="CID" width="30" height="30"/>
+          <a href="<?cs var:toroot ?>reference/packages.html" tabindex="-1"><img
+              src="<?cs var:toroot ?>assets/cid_smart.png" alt="CID" width="50" height="50"/>
           <span id="masthead-title">CyanogenMod Platform SDK</span></a>
       </div>
       <div id="headerRight">
