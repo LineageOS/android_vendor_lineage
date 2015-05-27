60051ef55686580a69701d6bf907d59615518b65
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-05-27 01:27:08 -0400

    config: Create TV targets (full and mini)
    * To follow naming and allow proper device type makefile inclusion
         (phone calls _phone, tablet calls _tablet, etc.)
    
    Change-Id: Ie29548b7160ee24331d9833ff769acc82ff10e7b
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

---
+++ config/common_mini_tv.mk
@@ -0,0 +1,15 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/cm/config/common.mk)
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+endif
