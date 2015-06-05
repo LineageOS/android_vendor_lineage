b2bd3216068665e57bc8f243bc5fa16ab51a3c5b
Adnan Begovic <adnan@cyngn.com>
2015-06-04 19:23:35 -0700

    vendor/cm: Create CM_PLATFORM_SDK_VERSION.
    Change-Id: I7126b1ac93fbe3e0d5542279cb1f96dfc214bb23

--- config/common.mk
+++ config/common.mk
@@ -322,8 +322,22 @@ endif
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 1
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.display.version=$(CM_DISPLAY_VERSION)
+
+# CyanogenMod Platform SDK Version
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
