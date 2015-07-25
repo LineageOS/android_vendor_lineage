bf1bcdbc44ea05bd2d209d66830a86634fd40278
Chris Sarbora <sarbs@cyngn.com>
2015-07-24 20:16:06 -0700

    Specify the maven-dependency-plugin version directly so as to force Maven 3.0.5 to use the correct version.
    Change-Id: Ia8b943feeb13d8835e0510404d067a2760608aa6

--- build/core/maven_artifact.mk
+++ build/core/maven_artifact.mk
@@ -32,36 +32,22 @@ artifact_filename := $(LOCAL_MAVEN_GROUP).$(LOCAL_MAVEN_ARTIFACT)-$(LOCAL_MAVEN_
 
 LOCAL_PREBUILT_MODULE_FILE := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE),,COMMON)/$(artifact_filename)
 
-$(LOCAL_PREBUILT_MODULE_FILE): specifier := $(LOCAL_MAVEN_GROUP):$(LOCAL_MAVEN_ARTIFACT):$(LOCAL_MAVEN_VERSION):$(LOCAL_MAVEN_PACKAGING)$(if $(LOCAL_MAVEN_CLASSIFIER),:$(LOCAL_MAVEN_CLASSIFIER))
 $(LOCAL_PREBUILT_MODULE_FILE): repo := $(LOCAL_MAVEN_REPO)
-
-ifeq ($(shell mvn --version | head -1),Apache Maven 3.0.5)
-  copy_task :=
-  repo_param := -DrepoUrl=
-  output_param := -Ddestination=
-  $(LOCAL_PREBUILT_MODULE_FILE): nogroup_file := $(LOCAL_MAVEN_ARTIFACT)-$(LOCAL_MAVEN_VERSION)$(if $(LOCAL_MAVEN_CLASSIFIER),-$(LOCAL_MAVEN_CLASSIFIER)).$(LOCAL_MAVEN_PACKAGING) 
-  $(LOCAL_PREBUILT_MODULE_FILE): copy_command := $(hide) cp ~/.m2/repository/$(subst .,/,$(LOCAL_MAVEN_GROUP))/$(LOCAL_MAVEN_ARTIFACT)/$(LOCAL_MAVEN_VERSION)/$(nogroup_file)
-else
-  copy_task := dependency:copy
-  repo_param := -DremoteRepositories=central::::
-  output_param := -DoutputDirectory=
-  $(LOCAL_PREBUILT_MODULE_FILE): copy_command := $(hide) mvn -q dependency:copy \
-		-Dartifact=$(specifier) \
+$(LOCAL_PREBUILT_MODULE_FILE): group := $(LOCAL_MAVEN_GROUP)
+$(LOCAL_PREBUILT_MODULE_FILE): artifact := $(LOCAL_MAVEN_ARTIFACT)
+$(LOCAL_PREBUILT_MODULE_FILE): version := $(LOCAL_MAVEN_VERSION)
+$(LOCAL_PREBUILT_MODULE_FILE): packaging := $(LOCAL_MAVEN_PACKAGING)
+$(LOCAL_PREBUILT_MODULE_FILE): classifier := $(LOCAL_MAVEN_CLASSIFIER)
+$(LOCAL_PREBUILT_MODULE_FILE):
+	$(hide) mvn -q org.apache.maven.plugins:maven-dependency-plugin:2.10:get \
+				   org.apache.maven.plugins:maven-dependency-plugin:2.10:copy \
+		-DremoteRepositories=central::::$(repo) \
+		-Dartifact=$(group):$(artifact):$(version):$(packaging)$(if $(classifier),:$(classifier)) \
 		-Dmdep.prependGroupId=true \
 		-Dmdep.overWriteSnapshots=true \
 		-Dmdep.overWriteReleases=true \
-		$(output_param)
-  $(LOCAL_PREBUILT_MODULE_FILE): nogroup_file := nonexistent
-endif
-
-$(LOCAL_PREBUILT_MODULE_FILE):
-	$(hide) mvn -q dependency:get \
-		$(repo_param)$(repo) \
-		-Dartifact=$(specifier) \
-		-Dtransitive=false
-	$(hide) mkdir -p $(dir $@)
-	$(copy_command)$(dir $@)
-	$(hide) mv $(dir $@)/$(nogroup_file) $@ 2>/dev/null || true
+		-Dtransitive=false \
+		-DoutputDirectory=$(dir $@)
 	@echo -e ${CL_GRN}"Download:"${CL_RST}" $@"
 
 include $(BUILD_PREBUILT)
