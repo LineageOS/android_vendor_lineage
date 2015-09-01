1e116da3f0f70a8f6b5ae0cb22fcbc25c7aec6f1
Adnan Begovic <adnan@cyngn.com>
2015-09-01 13:58:10 -0700

    vendor/cm: Start building in CMSettingsProvider.
    Change-Id: I982d0d607e61f6cd8a1aaf180b13b835190893d4

--- config/common_full.mk
+++ config/common_full.mk
@@ -16,7 +16,8 @@ PRODUCT_PACKAGES += \
     VisualizationWallpapers \
     PhotoTable \
     SoundRecorder \
-    PhotoPhase
+    PhotoPhase \
+    CMSettingsProvider
 
 # Extra tools in CM
 PRODUCT_PACKAGES += \
