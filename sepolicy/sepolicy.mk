6ac91cb6d3bf4d35f32677bff58a0381c20db026
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-07 12:57:22 -0500

    sepolicy: remove BOARD_SEPOLICY_UNION
    * this is a no-op now
    
    Change-Id: I3703a9670285017ce7aec9ac20c63a6f733b8ffa

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -5,36 +5,3 @@
 
 BOARD_SEPOLICY_DIRS += \
     vendor/cm/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    property_contexts \
-    seapp_contexts \
-    service_contexts \
-    adbd.te \
-    app.te \
-    auditd.te \
-    bootanim.te \
-    drmserver.te \
-    healthd.te \
-    hostapd.te \
-    installd.te \
-    livedisplay.te \
-    mediaserver.te \
-    netd.te \
-    property.te \
-    recovery.te \
-    shell.te \
-    su.te \
-    sysinit.te \
-    system.te \
-    system_app.te \
-    system_server.te \
-    ueventd.te \
-    uncrypt.te \
-    userinit.te \
-    vold.te \
-    zygote.te \
-    mac_permissions.xml
