223ccecfff1fb58c8d2123f9bddd82e546f79240
Scott Mertz <scott@cyngn.com>
2015-11-10 11:15:43 -0800

    adb insecure by default
    Change-Id: Iec14c80bb43b2f58cfad36e05177f4fec3b1fd64

--- config/common.mk
+++ config/common.mk
@@ -18,7 +18,7 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 else
 # Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
