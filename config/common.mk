34b5b9756cdc86ab9491938c8519b7ae8beecb6d
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-01-30 23:39:39 +0200

    cm: Add live lockscreen feature
    Change-Id: I62e5229c9688619c2f7809f001d761b4bdf13a10

--- config/common.mk
+++ config/common.mk
@@ -121,6 +121,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
 
+# Live lockscreen
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
+
 # Theme engine
 include vendor/cm/config/themes_common.mk
 
