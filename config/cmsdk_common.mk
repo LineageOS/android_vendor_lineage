78a12edacfa48a3d3c026ecfda708ee88f4a4a93
Adnan Begovic <adnan@cyngn.com>
2016-03-15 16:43:58 -0700

    cm: Introduce profiles feature xml.
    TICKET: CYNGNOS-2288
    Change-Id: I7ebc085732bc09e2c2737d2acfa090a74e40c9fb

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -2,7 +2,8 @@
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
     vendor/cm/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml \
-    vendor/cm/config/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml
+    vendor/cm/config/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml \
+    vendor/cm/config/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml
 
 # CM Platform Library
 PRODUCT_PACKAGES += \
