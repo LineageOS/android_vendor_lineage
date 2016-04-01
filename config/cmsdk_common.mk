7ae471d7ca3269cace5a59a6822b1b168c4c8814
Adnan Begovic <adnan@cyngn.com>
2016-04-01 15:38:57 -0700

    cm: Prepare for api level 6.
    TICKET: CYNGNOS-2303
    Change-Id: Ife365b606abe8043a4c6e9c0eef206e629a470cd

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -28,7 +28,7 @@ ifndef CM_PLATFORM_SDK_VERSION
   # the SDK are released.  It should only be incremented when the APIs for
   # the new release are frozen (so that developers don't write apps against
   # intermediate builds).
-  CM_PLATFORM_SDK_VERSION := 5
+  CM_PLATFORM_SDK_VERSION := 6
 endif
 
 ifndef CM_PLATFORM_REV
