243d3696e7c365946b29a52ee40bd404a3dd4f61
AdrianDC <radian.dc@gmail.com>
2016-05-25 11:01:08 +0200

    cm: Fix the backuptool importation logic
     * backuptool.sh and its dependencies are to be used
        on devices without the flags being checked, not with
    
    Change-Id: Ia2e1e32c4878f52a0a781e64583a95b32f8d3611

--- config/common.mk
+++ config/common.mk
@@ -78,7 +78,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
 
 # Backup Tool
-ifneq (,$(filter true, $(CYNGN_TARGET) $(EXTERNAL_CLEAN_TARGET)))
+ifeq (,$(filter true, $(CYNGN_TARGET) $(EXTERNAL_CLEAN_TARGET)))
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/cm/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
