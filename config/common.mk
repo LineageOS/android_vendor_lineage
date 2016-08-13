049d4c52bed067861289c12e175a227da36f656f
Luis Vidal <lvidal@cyngn.com>
2016-08-13 01:32:58 -0700

    config: Add WeatherManagerService to PRODUCT_PACKAGES
    Change-Id: Ic6d18ebeec31d17104a4093f5da1f956417e345b

--- config/common.mk
+++ config/common.mk
@@ -135,7 +135,8 @@ PRODUCT_PACKAGES += \
     Development \
     BluetoothExt \
     Profiles \
-    ThemeManagerService
+    ThemeManagerService \
+    WeatherManagerService
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
