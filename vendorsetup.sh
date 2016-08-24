69354102bc3dee3b1a3c28c6283c868d7ed853f4
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-08-24 10:19:50 -0500

    cm-13.0 -> cm-14.0
    Change-Id: I3820cd42208bf92aea4cf945eaade3ae59ed549a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-13.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-14.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
