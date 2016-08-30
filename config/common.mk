a70323c1d00df0d6799d1e1447192898a2b41d96
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-08-30 14:52:01 -0500

    common.mk: don't forget your quotes
    * Not having these causes kati to wig out in unpredictable ways
    
    Change-Id: I9d0932f5e5719204eae75c25acfdf60e9c533d3f

--- config/common.mk
+++ config/common.mk
@@ -3,7 +3,7 @@ PRODUCT_BRAND ?= cyanogenmod
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 # determine the smaller dimension
 TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
     echo $(TARGET_SCREEN_WIDTH); \
   else \
     echo $(TARGET_SCREEN_HEIGHT); \
@@ -17,7 +17,7 @@ bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_siz
 define check_and_set_bootanimation
 $(eval TARGET_BOOTANIMATION_NAME := $(shell \
   if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
       echo $(1); \
       exit 0; \
     fi;
