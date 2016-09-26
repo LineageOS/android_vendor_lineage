6a3782824fce91b79d5f6441b9901d545a538a08
Steve Kondik <steve@cyngn.com>
2016-09-25 22:37:01 -0700

    cm: Add missing bits from system-ccache commit
    Change-Id: I5936a9a3f3ded09aa7d3ec4f1061cbccf324c759

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -231,9 +231,14 @@ endif
 endif
 
 ifneq ($(USE_CCACHE),)
-    ccache := $(ANDROID_BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache
-    # Check that the executable is here.
-    ccache := $(strip $(wildcard $(ccache)))
+    # Detect if the system already has ccache installed to use instead of the prebuilt
+    ccache := $(shell which ccache)
+
+    ifeq ($(ccache),)
+        ccache := $(ANDROID_BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache
+        # Check that the executable is here.
+        ccache := $(strip $(wildcard $(ccache)))
+    endif
 endif
 
 KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
