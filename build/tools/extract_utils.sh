5f173157c452554311e33ac9c074a4ebaf9c37c4
Rashed Abdel-Tawab <rashed@linux.com>
2016-10-01 20:33:00 -0400

    extract_utils: Add ability to set custom vendor.mk name
    In some circumstances we might want to set a custom vendor.mk
    filename. If not set, then just default to the current device name
    
    Change-Id: Icb59ebee67ce17b41ac613020685773efb01103d

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -48,6 +48,7 @@ trap cleanup EXIT INT TERM ERR
 # $3: CM root directory
 # $4: is common device - optional, default to false
 # $5: cleanup - optional, default to true
+# $6: custom vendor makefile name - optional, default to false
 #
 # Must be called before any other functions can be used. This
 # sets up the internal state for a new vendor configuration.
@@ -76,7 +77,12 @@ function setup_vendor() {
         mkdir -p "$CM_ROOT/$OUTDIR"
     fi
 
-    export PRODUCTMK="$CM_ROOT"/"$OUTDIR"/"$DEVICE"-vendor.mk
+    VNDNAME="$6"
+    if [ -z "$VNDNAME" ]; then
+        VNDNAME="$DEVICE"
+    fi
+
+    export PRODUCTMK="$CM_ROOT"/"$OUTDIR"/"$VNDNAME"-vendor.mk
     export ANDROIDMK="$CM_ROOT"/"$OUTDIR"/Android.mk
     export BOARDMK="$CM_ROOT"/"$OUTDIR"/BoardConfigVendor.mk
 
