df96b447ef9930cba0495f410c42080f1f48432b
Steve Kondik <steve@cyngn.com>
2016-10-13 11:37:08 -0700

    cm: Re-add WallpaperPicker
     * And a little OCD organization
    
    Change-Id: I139a0a704a06fae581175e6523a34e372e04646b

--- config/common.mk
+++ config/common.mk
@@ -151,9 +151,9 @@ PRODUCT_PACKAGES += \
 # Optional CM packages
 PRODUCT_PACKAGES += \
     libemoji \
-    Terminal \
     LiveWallpapersPicker \
-    PhotoTable
+    PhotoTable \
+    Terminal
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
@@ -161,21 +161,22 @@ PRODUCT_PACKAGES += \
 
 # Custom CM packages
 PRODUCT_PACKAGES += \
-    Launcher3 \
-    Trebuchet \
     AudioFX \
-    CMWallpapers \
     CMFileManager \
-    Eleven \
-    LockClock \
-    CMUpdater \
-    CyanogenSetupWizard \
     CMSettingsProvider \
+    CMUpdater \
+    CMWallpapers \
+    CyanogenSetupWizard \
+    Eleven \
     ExactCalculator \
+    Launcher3 \
     LiveLockScreenService \
-    WeatherProvider \
+    LockClock \
+    Screencast \
     SoundRecorder \
-    Screencast
+    Trebuchet \
+    WallpaperPicker \
+    WeatherProvider
 
 # Exchange support
 PRODUCT_PACKAGES += \
@@ -183,26 +184,25 @@ PRODUCT_PACKAGES += \
 
 # Extra tools in CM
 PRODUCT_PACKAGES += \
-    libsepol \
-    mke2fs \
-    tune2fs \
-    nano \
-    htop \
-    mkfs.ntfs \
-    fsck.ntfs \
-    mount.ntfs \
-    gdbserver \
-    micro_bench \
-    oprofiled \
-    sqlite3 \
-    strace \
-    pigz \
     7z \
-    lib7z \
     bash \
     bzip2 \
     curl \
+    fsck.ntfs \
+    gdbserver \
+    htop \
+    lib7z \
+    libsepol \
+    micro_bench \
+    mke2fs \
+    mkfs.ntfs \
+    mount.ntfs \
+    oprofiled \
+    pigz \
     powertop \
+    sqlite3 \
+    strace \
+    tune2fs \
     unrar \
     unzip \
     vim \
