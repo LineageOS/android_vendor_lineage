26e669badc0154baf8fd4b7e2da84fea1d91249a
Steve Kondik <steve@cyngn.com>
2016-11-04 12:05:19 -0700

    cm: Autodetect SDCLANG in prebuilts/snapdragon-llvm
     * Will be used by the build servers
    
    Change-Id: I5d31a7c41adc283cb9336314e9d54412cc740e9e

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -934,3 +934,10 @@ function fixup_common_out_dir() {
         mkdir -p ${common_out_dir}
     fi
 }
+
+# Enable SD-LLVM if available
+if [ -d $(gettop)/prebuilts/snapdragon-llvm/toolchains ]; then
+    export SDCLANG=true
+    export SDCLANG_PATH=$(gettop)/prebuilts/snapdragon-llvm/toolchains/llvm-Snapdragon_LLVM_for_Android_3.8/prebuilt/linux-x86_64/bin
+    export SDCLANG_LTO_DEFS=$(gettop)/device/qcom/common/sdllvm-lto-defs.mk
+fi
