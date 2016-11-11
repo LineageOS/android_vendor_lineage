1b19f16eb7f49c95a9b91c951abf73bb36f9e927
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-10 21:21:07 -0600

    [2/2] cm: set cache partition location
    * In init.rc, this prop will be used to set DOWNLOAD_CACHE
    * If a device specifies having a dedicated /cache partition, by setting
      BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE, use it!
    * Default to /data/cache per AOSP
    
    Change-Id: I838672ca4638c9665333c1a67257fc435f33f388

--- config/BoardConfigCM.mk
+++ config/BoardConfigCM.mk
@@ -2,3 +2,11 @@
 ifneq ($(WITH_CM_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.cm
 endif
+
+ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
+  ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.device.cache_dir=/data/cache
+else
+  ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.device.cache_dir=/cache
+endif
