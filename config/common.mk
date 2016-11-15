aae6c62687b263bd2a758e4cd8a35956ed29b9bf
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-15 17:45:20 +0800

    config: common: Remove obsolete RomManager props
    RomManager hasn't been properly updated in years.
    Is this still even a reliable ROM updater anymore?
    
    Change-Id: I3ea57ec0121e6d6f933dd80031bb53514a44e362

--- config/common.mk
+++ config/common.mk
@@ -32,14 +32,6 @@ PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTAN
 endif
 endif
 
-ifdef CM_NIGHTLY
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmodnightly
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmod
-endif
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
