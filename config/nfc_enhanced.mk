8dcba6834b8374ea5878f1cc52a7faa07c70434b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 10:09:35 +0800

    cm: config: Remove enhanced NFC feature
    This was used for the HCE implementation by SimplyTapp in CM 9 - 10.2.
    Since then, Android has an HCE implementation and this is no longer used.
    
    Change-Id: Icd9db39829c1863c4962aab53959ad527da28bcd

--- config/nfc_enhanced.mk
+++
@@ -1,3 +0,0 @@
-# This device supports CM enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/cm/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
