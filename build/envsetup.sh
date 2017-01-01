321dde5f709872e491dea6a2a687b3ec23ef2b97
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-01 15:24:38 +0800

    cm: Rebrand CM_VERSION to LINEAGE_VERSION
    Change-Id: I79433c4d2fa14784c5b41985ed91e3712aacafa4

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -79,7 +79,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=$(get_build_var CM_VERSION)
+        MODVERSION=$(get_build_var LINEAGE_VERSION)
         ZIPFILE=lineage-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
