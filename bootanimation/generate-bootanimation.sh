db8d7afd54410cbc088d8d03ba7d070c41fe1f6a
Scott Warner <Tortel1210@gmail.com>
2017-01-27 06:54:59 -0500

    bootanimation: Limit the PNGs to 250 colors
    Some systems seem to have with the convert command as-is:
    convert: Cannot write PNG8 or color-type 3; colormap is NULL
    
    Because we are writing PNG8, there area a max of 256 colors total.
    Explicitly telling convert to stay under the max fixes the issue.
    
    Change-Id: I595fb4503396ca20226ea76bf7b15ed9878752fd

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -26,7 +26,7 @@ for part_cnt in 0 1 2 3 4
 do
     mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
 done
-tar xfp "vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
+tar xfp "vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' -colors 250 \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
 
 # Create desc.txt
 echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT" 60 > "$OUT/bootanimation/desc.txt"
