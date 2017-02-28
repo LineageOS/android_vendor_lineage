fc9a4c579340d0ba103587f1e93c6be33fc64e55
Sean hoyt <seanhoyt963@gmail.com>
2017-02-28 02:04:32 +0000

    Make a data only config
    Change-Id: I99d60de61384aa413f64fbd60842c42ee1a169af

---
+++ config/data_only.mk
@@ -0,0 +1,8 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk \
+    CellBroadcastReceiver
