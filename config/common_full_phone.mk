396a56feea1ad2b8aca2851b0b638d4e59216032
Dan Pasanen <dan.pasanen@gmail.com>
2017-07-06 08:21:30 -0500

    vendor: change vendor to lineage
    Change-Id: I2df13091f5d62b552ef434aef1a389f4fe1249e0

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,11 +1,11 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common_full.mk)
+$(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required CM packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include CM LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/telephony.mk)
