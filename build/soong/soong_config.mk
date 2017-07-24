6810c414c1d3a32bc7fd9f5c0b4b16d56b7363a4
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:04:34 +0200

    soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
    * Soong product variable 'has_legacy_camera_hal1'
    
    Change-Id: I10800cba0c431297b5d7aad6e97d57a01232d73e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,4 +1,5 @@
 lineage_soong:
 	$(hide)(\
     echo '{';\
+    echo '      "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
     echo '')  > $(SOONG_VARIABLES_TMP)
