87378c192710bec7fb78b72024514c2dfbbbbdb9
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:05:30 +0200

    soong_config: Add TARGET_USES_MEDIA_EXTENSIONS variable
    * Soong product variable 'uses_media_extensions'
    
    Change-Id: Icf36c7e4a90ace7a4c603c63ab1126c1554ffab8
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,4 +3,5 @@ lineage_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
+	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
