23c91e8769648fc4e5b9c8ab2039a95da3dad429
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:04:34 +0200

    soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
    * Soong product variable 'has_legacy_camera_hal1'
    
    Change-Id: I10800cba0c431297b5d7aad6e97d57a01232d73e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
@@ -12,6 +15,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
