288514009d9ba5496f09532703570faed8dc6509
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:05:30 +0200

    soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS
    * Soong product variable 'needs_text_relocations'
    
    Change-Id: Ia22c94922f37c49f0d66a67747efa0ee97b4e477
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,6 +1,10 @@
 package android
 type Product_variables struct {
+	Needs_text_relocations struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
+	Needs_text_relocations  *bool `json:",omitempty"`
 }
