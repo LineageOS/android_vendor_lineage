ba0de726b663e04a5ced0ee5870028195558bb58
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:05:30 +0200

    soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS
    * Soong product variable 'needs_text_relocations'
    
    Change-Id: Ia22c94922f37c49f0d66a67747efa0ee97b4e477
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -7,9 +7,14 @@ type Product_variables struct {
 	Uses_media_extensions struct {
 		Cflags []string
 	}
+
+	Needs_text_relocations struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
+	Needs_text_relocations  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,4 +4,5 @@ lineage_soong:
 	echo '{'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
