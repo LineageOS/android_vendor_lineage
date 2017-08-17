0562cc5cb1d7d3346ac9d0d01b363826de0a4087
Luca Stefani <luca020400@lineageos.org>
2017-08-17 20:30:00 +0200

    vendor: build: Add mk_timer
    Change-Id: Ib7ce1e7c662fe0555d296e125241c222f89f671d

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -22,6 +22,35 @@ Additional LineageOS functions:
 EOF
 }
 
+function mk_timer()
+{
+    local start_time=$(date +"%s")
+    $@
+    local ret=$?
+    local end_time=$(date +"%s")
+    local tdiff=$(($end_time-$start_time))
+    local hours=$(($tdiff / 3600 ))
+    local mins=$((($tdiff % 3600) / 60))
+    local secs=$(($tdiff % 60))
+    local ncolors=$(tput colors 2>/dev/null)
+    echo
+    if [ $ret -eq 0 ] ; then
+        echo -n "#### make completed successfully "
+    else
+        echo -n "#### make failed to build some targets "
+    fi
+    if [ $hours -gt 0 ] ; then
+        printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
+    elif [ $mins -gt 0 ] ; then
+        printf "(%02g:%02g (mm:ss))" $mins $secs
+    elif [ $secs -gt 0 ] ; then
+        printf "(%s seconds)" $secs
+    fi
+    echo " ####"
+    echo
+    return $ret
+}
+
 function brunch()
 {
     breakfast $*
