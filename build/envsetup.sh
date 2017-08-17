b6f60cd8e5aa2f3fa2fd3153e9362b11c94b65ac
Luca Stefani <luca020400@lineageos.org>
2017-08-17 19:29:00 +0200

    envsetup: Update mms/mka for O

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -674,20 +674,14 @@ function lineagerebase() {
 }
 
 function mka() {
-    local T=$(gettop)
-    if [ "$T" ]; then
-        case `uname -s` in
-            Darwin)
-                make -C $T -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-                ;;
-            *)
-                mk_timer schedtool -B -n 10 -e ionice -n 7 make -C $T -j$(grep "^processor" /proc/cpuinfo | wc -l) "$@"
-                ;;
-        esac
-
-    else
-        echo "Couldn't locate the top of the tree.  Try setting TOP."
-    fi
+    case `uname -s` in
+        Darwin)
+            m -j "$@"
+            ;;
+        *)
+            mk_timer schedtool -B -n 10 -e ionice -n 7 m -j "$@"
+            ;;
+    esac
 }
 
 function cmka() {
@@ -720,15 +714,12 @@ function mms() {
 
     case `uname -s` in
         Darwin)
-            local NUM_CPUS=$(sysctl hw.ncpu|cut -d" " -f2)
             ONE_SHOT_MAKEFILE="__none__" \
-                make -C $T -j $NUM_CPUS "$@"
+                m -j "$@"
             ;;
         *)
-            local NUM_CPUS=$(grep "^processor" /proc/cpuinfo | wc -l)
             ONE_SHOT_MAKEFILE="__none__" \
-                mk_timer schedtool -B -n 1 -e ionice -n 1 \
-                make -C $T -j $NUM_CPUS "$@"
+                mk_timer schedtool -B -n 1 -e ionice -n 1 m -j "$@"
             ;;
     esac
 }
