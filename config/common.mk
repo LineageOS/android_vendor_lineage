3a8777574d0f2bb0df338f3466591a30930656b4
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:43:22 +0200

    lineage: Disable vendor restrictions
    Change-Id: I9bd368f04605df469f02b26dae47e587bccc2e52

--- config/common.mk
+++ config/common.mk
@@ -123,6 +123,9 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Disable vendor restrictions
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
 # Bootanimation
 PRODUCT_PACKAGES += \
     bootanimation.zip
