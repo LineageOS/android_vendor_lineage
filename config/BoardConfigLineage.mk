2724913a8029f3987fea067d8d4457b73cff70ce
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:44:27 +0200

    core: Move ro.device.cache_dir to common.mk
    Change-Id: Ic2e2284de064c6f9fa1aa8d9ba6b7af19c671c4e

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -2,11 +2,3 @@
 ifneq ($(WITH_LINEAGE_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
-
-ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
-  ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.device.cache_dir=/data/cache
-else
-  ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.device.cache_dir=/cache
-endif
