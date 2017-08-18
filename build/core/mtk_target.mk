154df245a55d948f4881deae2d071016e736aef5
Adrian DC <radian.dc@gmail.com>
2017-08-18 16:14:47 +0200

    build: Remove breaking global flags
    Change-Id: I9bbc75ce4701dc70c17b1d80c40e8b98e961dd2a

--- build/core/mtk_target.mk
+++ build/core/mtk_target.mk
@@ -1,16 +1,2 @@
 # Bring in Mediatek helper macros
 include vendor/lineage/build/core/mtk_utils.mk
-
-ifeq ($(BOARD_USES_MTK_HARDWARE),true)
-    mtk_flags := -DMTK_HARDWARE
-
-    TARGET_GLOBAL_CFLAGS += $(mtk_flags)
-    TARGET_GLOBAL_CPPFLAGS += $(mtk_flags)
-    CLANG_TARGET_GLOBAL_CFLAGS += $(mtk_flags)
-    CLANG_TARGET_GLOBAL_CPPFLAGS += $(mtk_flags)
-
-    2ND_TARGET_GLOBAL_CFLAGS += $(mtk_flags)
-    2ND_TARGET_GLOBAL_CPPFLAGS += $(mtk_flags)
-    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(mtk_flags)
-    2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(mtk_flags)
-endif
