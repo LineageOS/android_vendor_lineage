a835e402d47c1ff13f09a114b8991722813b0eb7
Simon Shields <simon@lineageos.org>
2017-08-23 12:03:36 +1000

    lineage: add soong bits for boardflags
    Change-Id: If01db1eb6b1432a6c4d26d5c9701895ab4dc3ff5

---
+++ build/soong/android/variable.go
@@ -0,0 +1,10 @@
+package android
+type Product_variables struct {
+    Test struct {
+        Cflags []string
+    }
+}
+
+type ProductVariables struct {
+    Test *bool `json:",omitempty"`
+}
---
+++ build/soong/soong_config.mk
@@ -0,0 +1,4 @@
+lineage_soong:
+	$(hide)(\
+    echo '{';\
+    echo '  "Test": true,')  > $(SOONG_VARIABLES_TMP)
