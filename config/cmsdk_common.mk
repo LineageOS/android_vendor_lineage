9c720fd55d4f1ef936ab0de529cf2ba4bc11cbfb
Michael Bestas <mkbestas@lineageos.org>
2017-08-29 19:09:06 +0300

    lineage: Remove partner interface
    Change-Id: I22ad161f4587703b7f3c2919890a19e69c1a8e0f

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -2,7 +2,6 @@
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.cyanogenmod.audio.xml:system/etc/permissions/org.cyanogenmod.audio.xml \
     vendor/lineage/config/permissions/org.cyanogenmod.livedisplay.xml:system/etc/permissions/org.cyanogenmod.livedisplay.xml \
-    vendor/lineage/config/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml \
     vendor/lineage/config/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml \
     vendor/lineage/config/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml \
     vendor/lineage/config/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml \
