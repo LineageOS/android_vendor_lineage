b72f8c924e1e7075bc8de3b32b673671b2081e0d
Anshuman Dani <adani@codeaurora.org>
2017-09-01 23:47:06 +0530

    Always use my_installed_module_stem instead of my_built_module_stem
    This will prevent link errors in case my_installed_module_stem is different
    from my_built_module_stem.
    
    Change-Id: Ic72571637ea404c92128b9cd5a28e82a36bc0033

--- build/core/sdllvm-lto-defs.mk
+++ build/core/sdllvm-lto-defs.mk
@@ -15,7 +15,7 @@ $(LOCAL_BUILT_MODULE) : $(combo_var_prefix)GLOBAL_ARFLAGS := $(ARFLAGS)
 
 # For 64 bit
 intermediates := $(call local-intermediates-dir,,$(LOCAL_2ND_ARCH_VAR_PREFIX))
-LOCAL_BUILT_MODULE_64 := $(intermediates)/$(my_built_module_stem)
+LOCAL_BUILT_MODULE_64 := $(intermediates)/$(my_installed_module_stem)
 
 $(LOCAL_BUILT_MODULE_64) : TARGET_AR := $(AR)
 $(LOCAL_BUILT_MODULE_64) : TARGET_GLOBAL_ARFLAGS := $(ARFLAGS)
@@ -24,12 +24,11 @@ else
 # For SHARED_LIBRARIES and EXECUTABLES we need to filter out flags not
 # needed/understood by SD LLVM's Linker.
 
-linked_module_32 := $(intermediates)/LINKED/$(my_built_module_stem)
-intermediates    := $(call local-intermediates-dir,,$(LOCAL_2ND_ARCH_VAR_PREFIX))
-linked_module_64 := $(intermediates)/LINKED/$(my_built_module_stem)
-
 FLAGS_TO_BE_FILTERED := -Wl,--icf=safe -Wl,--no-undefined-version -Wl,--fix-cortex-a53-843419 -fuse-ld=gold
+linked_module_32 := $(intermediates)/LINKED/$(my_installed_module_stem)
+intermediates    := $(call local-intermediates-dir,,$(LOCAL_2ND_ARCH_VAR_PREFIX))
+linked_module_64 := $(intermediates)/LINKED/$(my_installed_module_stem)
+
 $(linked_module_32) : PRIVATE_TARGET_GLOBAL_LDFLAGS := $(filter-out $(FLAGS_TO_BE_FILTERED),$(PRIVATE_TARGET_GLOBAL_LDFLAGS))
 $(linked_module_64) : PRIVATE_TARGET_GLOBAL_LDFLAGS := $(filter-out $(FLAGS_TO_BE_FILTERED),$(PRIVATE_TARGET_GLOBAL_LDFLAGS))
-
 endif
