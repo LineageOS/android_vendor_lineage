d2ed30399e6284558478487401b41fe93dc41b65
jrior001 <jriordan001@gmail.com>
2017-09-02 18:52:56 -0400

    soong_config: Add BOARD_NEEDS_PRE_ION_X86
    * Soong product variable 'Pre_ion_x86'
    
    Change-Id: I6c55cd74ec0a1f43b7343427d7351561f53611ca

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,5 +6,6 @@ lineage_soong:
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Mtk_hardware": $(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false),'; \
-        echo '    "Device_recovery_modules":$(if $(filter true,$(TARGET_RECOVERY_DEVICE_MODULES)),true,false),'; \
+        echo '    "Device_recovery_modules":$(if $(filter true,$(TARGET_RECOVERY_DEVICE_MODULES))true,false),'; \
+        echo '    "Pre_ion_x86":$(if $(filter true,$(BOARD_USES_PRE_ION_X86)),true,false),'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
