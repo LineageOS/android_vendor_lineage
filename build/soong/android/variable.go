bf1c8f5c17ad0b50a8dc259459f284f8f8582343
Dominggoes Isakh <drjisakh@gmail.com>
2017-09-03 19:14:17 +0200

    soong_config: Add TARGET_NEEDS_EXYNOS4_ENHANCEMENTS
     * Soong product variable 'Exynos4_enhancements'
    
    Change-Id: Ied29417e9e1ecc424d99f452d7879bd2e723a1cc

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,10 +1,15 @@
 package android
 type Product_variables struct {
+	Exynos4_enhancements struct {
+		Cflags []string
+		Cppflags []string
+	}
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
+	Exynos4_enhancements    *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,7 @@ lineage_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Lineage": {'; \
+	echo '    "Exynos4_enhancements": $(if $(filter true,$(TARGET_NEEDS_EXYNOS4_ENHANCEMENTS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
