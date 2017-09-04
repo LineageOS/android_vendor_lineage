7ef0e17066d7201152e511641042aa48bc24d157
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put lineage variables in the "Lineage" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Lineage structure
 lineage_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Lineage": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
