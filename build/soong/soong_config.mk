5e0593b4ce3b60a04eaebc6cc5c2dfcae2b4d2a2
LuK1337 <priv.luk@gmail.com>
2017-09-05 09:51:22 +0200

    soong_config: Add TARGET_USES_NON_TREBLE_CAMERA variable
    * Soong product variable 'uses_non_treble_camera'
    
    Change-Id: I38c01769dfeb376a1de474e99f0d3e1faaa6b15f

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,7 @@ lineage_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Lineage": {'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
