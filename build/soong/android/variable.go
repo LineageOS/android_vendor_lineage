140aa754062ce541e1ae3f515a06a7f95b430a7d
LuK1337 <priv.luk@gmail.com>
2017-09-05 09:51:22 +0200

    soong_config: Add TARGET_USES_NON_TREBLE_CAMERA variable
    * Soong product variable 'uses_non_treble_camera'
    
    Change-Id: I38c01769dfeb376a1de474e99f0d3e1faaa6b15f

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,13 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+
+	Uses_non_treble_camera struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
 }
