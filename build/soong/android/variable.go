82d8a0a2c91924c732b67fde58da49368a85f942
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 14:55:00 -0400

    lineage: Add qcom board flags to soong
    Change-Id: I21397b5b765e323ec6d5cdab23b93c9b885d7d23

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,10 +6,35 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
+	BoardUsesQTIHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
+	BoardUsesQCOMHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
+	TargetUsesQCOMBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+	TargetUsesQCOMLegacyBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+	BoardUsesLegacyAlsa struct {
+		Cflags []string
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,4 +8,11 @@ lineage_soong:
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
 	echo '},'; \
+	echo '"Qualcomm": {'; \
+	echo '    "BoardUsesQTIHardware": $(if $(filter true,$(BOARD_USES_QTI_HARDWARE)),true,false),'; \
+	echo '    "BoardUsesQCOMHardware": $(if $(filter true,$(BOARD_USES_QCOM_HARDWARE)),true,false),'; \
+	echo '    "TargetUsesQCOMBsp": $(if $(filter true,$(TARGET_USES_QCOM_BSP)),true,false),'; \
+	echo '    "TargetUsesQCOMLegacyBsp": $(if $(filter true,$(TARGET_USES_QCOM_LEGACY_BSP)),true,false),'; \
+	echo '    "BoardUsesLegacyAlsa": $(if $(filter true,$(BOARD_USES_LEGACY_ALSA_AUDIO)),true,false)'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
