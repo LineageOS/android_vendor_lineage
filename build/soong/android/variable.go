9dd6f0d63981fbf22c5e9b9b21043f01fe1018ea
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 14:55:00 -0400

    [WIP] lineage: Add qcom board flags to soong
    Work in progress. Will eat your cat
    
    Change-Id: I21397b5b765e323ec6d5cdab23b93c9b885d7d23

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,33 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	BoardUsesQTIHardware struct {
+		Cppflags []string -DQTI_HARDWARE
+		Cflags []string -DQTI_HARDWARE
+	}
+	BoardUsesQCOMHardware struct {
+		Cppflags []string -DQCOM_HARDWARE
+		Cflags []string -DQCOM_HARDWARE
+	}
+	TargetUsesQCOMBsp struct {
+		Cppflags []string -DQCOM_BSP -DQTI_BSP
+		Cflags []string -DQCOM_BSP -DQTI_BSP
+	}
+	TargetUsesQCOMLegacyBsp struct {
+		Cppflags []string -DQCOM_BSP_LEGACY
+		Cflags []string -DQCOM_BSP_LEGACY
+	}
+	BoardUsesLegacyAlsa struct {
+		Cppflags []string -DLEGACY_ALSA_AUDIO
+		Cflags []string -DLEGACY_ALSA_AUDIO
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
 }
