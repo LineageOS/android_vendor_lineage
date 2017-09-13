8696ed0cf8f819a2d845806e28be1a1e8843deb8
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 14:55:00 -0400

    [WIP] lineage: Add qcom board flags to soong
    Still need to extend cc/compiler.go to add the global c/cpp flags
    
    Change-Id: I21397b5b765e323ec6d5cdab23b93c9b885d7d23

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,28 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	BoardUsesQTIHardware struct {
+		Cppflags []string
+	}
+	BoardUsesQCOMHardware struct {
+		Cppflags []string
+	}
+	TargetUsesQCOMBsp struct {
+		Cppflags []string
+	}
+	TargetUsesQCOMLegacyBsp struct {
+		Cppflags []string
+	}
+	BoardUsesLegacyAlsa struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
 }
