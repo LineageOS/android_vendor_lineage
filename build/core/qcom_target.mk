6115376970ffbbf98660539f971180a87936449c
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-19 19:12:35 -0400

    qcom: Don't override board flags if already defined by device
    Some devices might want to define these flags differently (ex. disable
    TARGET_USES_QCOM_BSP) however, currently, these flags are overriden
    anyway. Set them to ?= to only define if currently undefined.
    
    Change-Id: If21a3f855a2d80d7e5424c8785b031968b7c13b3

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -30,26 +30,26 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     BR_FAMILY := msm8909 msm8916
     UM_FAMILY := msm8937 msm8953
 
-    BOARD_USES_ADRENO := true
+    BOARD_USES_ADRENO ?= true
 
-    TARGET_USES_QCOM_BSP := true
+    TARGET_USES_QCOM_BSP ?= true
 
     # Tell HALs that we're compiling an AOSP build with an in-line kernel
-    TARGET_COMPILE_WITH_MSM_KERNEL := true
+    TARGET_COMPILE_WITH_MSM_KERNEL ?= true
 
     ifneq ($(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
         # Enable legacy audio functions
         ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            USE_CUSTOM_AUDIO_POLICY := 1
+            USE_CUSTOM_AUDIO_POLICY ?= 1
         endif
     endif
 
     # Allow building audio encoders
-    TARGET_USES_QCOM_MM_AUDIO := true
+    TARGET_USES_QCOM_MM_AUDIO ?= true
 
     # Enable color metadata for modern UM targets
     ifneq ($(filter msm8996 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
-        TARGET_USES_COLOR_METADATA := true
+        TARGET_USES_COLOR_METADATA ?= true
     endif
 
     # List of targets that use master side content protection
