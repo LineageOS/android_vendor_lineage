5c650848cda624ba178b9e07c793559b7b6e670c
Bruno Martins <bgcngm@gmail.com>
2017-09-20 17:10:26 +0100

    lineage: Enable full Treble for QCOM targets
     * Several modules are now expected to be placed under /vendor|/system/vendor
       as per the newest QCOM sepolicies. With this flag set those modules are
       built and placed into the proper location.
    
    Change-Id: I6994411ab8a051793924349945d863dbda0f2461

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -32,6 +32,8 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     BOARD_USES_ADRENO := true
 
+    PRODUCT_VENDOR_MOVE_ENABLED := true
+
     TARGET_USES_QCOM_BSP := true
 
     # Tell HALs that we're compiling an AOSP build with an in-line kernel
