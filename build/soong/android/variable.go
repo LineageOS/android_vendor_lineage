f309370b9d9a1795b8a4e8568d6cc8db1faf5c98
Dominggoes Isakh <drjisakh@gmail.com>
2017-09-24 22:28:31 +0200

    soong_config: Add BOARD_EGL_WORKAROUND_BUG_10194508
         * Soong product variable 'Egl_workaround_bug_10194508'
    
    Change-Id: I7f845ca15ee7ba9d6224ba25e1003bac21feefb2

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Egl_workaround_bug_10194508 struct {
+		Cppflags []string
+	}
 	Exynos4_enhancements struct {
 		Cflags []string
 		Cppflags []string
@@ -10,6 +13,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Egl_workaround_bug_10194508    *bool `json:",omitempty"`
 	Exynos4_enhancements    *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }
