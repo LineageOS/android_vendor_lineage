5340144d83b4963bab11ea6fc5b5788d86228627
Sam Mortimer <sam@mortimer.me.uk>
2017-10-17 11:48:28 -0700

    vendor/lineage: Rebrand cleanup
    *) Rebrand remaining text references
    
    Change-Id: I70ffb30519d40237c8266e208449a5e27140d2b3

--- build/tools/droiddoc/templates-lineage-sdk/components/masthead.cs
+++ build/tools/droiddoc/templates-lineage-sdk/components/masthead.cs
@@ -3,7 +3,7 @@
       <div id="headerLeft">
           <a href="<?cs var:toroot ?>reference/packages.html" tabindex="-1"><img
               src="<?cs var:toroot ?>assets/cid_smart.png" alt="CID" width="50" height="50"/>
-          <span id="masthead-title">CyanogenMod Platform SDK</span></a>
+          <span id="masthead-title">LineageOS Platform SDK</span></a>
       </div>
   </div><!-- header --><?cs 
 /def ?>
