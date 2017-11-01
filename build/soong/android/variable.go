61f5e144d59cb1a1eab55efd5acff82534ec5767
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:01:35 -0700

    lineage: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY variable handling
    Change-Id: I6f1cbcd883e91c8c3724dbe53732c7e777e21657

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,13 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
 }
 
 type ProductVariables struct {
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,8 @@ lineage_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Lineage": {'; \
+	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
+	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
