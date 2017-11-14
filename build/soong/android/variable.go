1e9298d5effb4a567adc4ea43df28ba7e1a72f23
Shubham Das <dasshubham762@gmail.com>
2017-11-14 10:52:16 +0000

    apns_conf: Update Jio APNs
    * Rename Jio APNs "Jio Internet" to "Internet", which is default name of
      Jio APNs.
    * Remove "supl". "default" includes this by default for Location services.
      We don't need this.
    
    Change-Id: I9506b49f699c688f0882b694e4f89cb2417a6b07

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	Target_shim_libs struct {
+		Cppflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -12,8 +15,9 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
+	Target_shim_libs  *string `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 }
