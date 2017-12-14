6771cfc50f935c54b42ba1bb78cb710dd5ed3366
Aaron Kling <webgeek1234@gmail.com>
2017-12-14 15:14:08 -0600

    Include build manifest on target
    Private repos can be excluded by setting the env var
    MANIFEST_EXCLUDES | delimited. proprietary is always
    excluded.
    
    Change-Id: I9a085a7102d9905853636e0ad60ac6f80a930ae0

--- config/common.mk
+++ config/common.mk
@@ -100,6 +100,10 @@ TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \
     bootanimation.zip
 
+# Build Manifest
+PRODUCT_PACKAGES += \
+    build-manifest
+
 # Lineage packages
 PRODUCT_PACKAGES += \
     LineageParts \
