b52c70881d655cee3b0f77b48d9d4a92b06de137
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-24 22:40:31 +0200

    envsetup: Automatically replace qcom/opensource with qcom-opensource
    CAF doesn't use qcom/opensource in their paths, so automatically adjust
    the project path for this when using cafremote
    
    Change-Id: I6252fbd1c3e8a545c0d54eefb6b730b893821574

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -316,6 +316,10 @@ function cafremote()
     then
         PROJECT="build"
     fi
+    if [[ $PROJECT =~ "qcom/opensource" ]];
+    then
+        PROJECT=$(echo $PROJECT | sed -e "s#qcom\/opensource#qcom-opensource#")
+    fi
     if (echo $PROJECT | grep -qv "^device")
     then
         local PFX="platform/"
