ddd3dcad5d00bd26042637e4aeb6f4215dbcce9c
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    lineage: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -2,3 +2,13 @@
 ifneq ($(WITH_LINEAGE_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
+
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
