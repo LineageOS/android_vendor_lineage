26762dad2ee6adba844c483d5a37872b7ca67b17
Michael Bestas <mkbestas@lineageos.org>
2018-01-01 18:23:32 +0200

    Bump LINEAGE_PLATFORM_SDK_VERSION to 8
    Change-Id: If78b28d3c018c7cd58b30e48efe631adbd70dd40

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -28,7 +28,7 @@ ifndef LINEAGE_PLATFORM_SDK_VERSION
   # the SDK are released.  It should only be incremented when the APIs for
   # the new release are frozen (so that developers don't write apps against
   # intermediate builds).
-  LINEAGE_PLATFORM_SDK_VERSION := 7
+  LINEAGE_PLATFORM_SDK_VERSION := 8
 endif
 
 ifndef LINEAGE_PLATFORM_REV
