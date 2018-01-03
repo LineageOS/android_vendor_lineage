94161f32840f774d5e061b4d8e3e6cd7ce438c98
Stricted <info@stricted.net>
2018-01-04 00:50:59 +0100

    [1/2] libstagefright: Add support for NV21 color format
    Change-Id: Ie74c09a139633cdcd9e8be398df6053c41a011b0

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,10 +6,14 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
+	Uses_samsung_cameraformat_nv21 struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Uses_samsung_cameraformat_nv21  *bool `json:",omitempty"`
 }
