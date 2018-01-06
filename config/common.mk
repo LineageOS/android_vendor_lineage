9f22e538780601bde3754b365ec9899e76f82af8
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-06 11:59:30 +0100

    config: make libhealthd.lineage opt-in
    Currently broken. Whoever will use it has to fix it.
    
    Change-Id: I029ccf650e1cf4dc1080f7bd8c7000f2955b2a1e

--- config/common.mk
+++ config/common.mk
@@ -171,10 +171,13 @@ PRODUCT_PACKAGES += \
     wget \
     zip
 
-# Custom off-mode charger
-ifneq ($(WITH_LINEAGE_CHARGER),false)
+# Charger
 PRODUCT_PACKAGES += \
-    charger_res_images \
+    charger_res_images
+
+# Custom off-mode charger
+ifeq ($(WITH_LINEAGE_CHARGER),true)
+PRODUCT_PACKAGES += \
     lineage_charger_res_images \
     font_log.png \
     libhealthd.lineage
