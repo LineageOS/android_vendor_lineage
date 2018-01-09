bf6d13970ab55cb690a34c45e4a5432bb42058bb
micky387 <mickaelsaibi@free.fr>
2018-01-09 20:05:48 +0100

    soong_config: Add TARGET_NO_SENSOR_PERMISSION_CHECK variable
    Change-Id: I5390a22ff801208ac4b518130b48d3fbd3757b1e

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	No_sensors_permission_check struct {
+		Cppflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -12,4 +15,5 @@ type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	No_sensors_permission_check  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,6 +6,7 @@ lineage_soong:
 	echo '"Lineage": {'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "No_sensors_permission_check": $(if $(filter true,$(TARGET_NO_SENSOR_PERMISSION_CHECK)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
