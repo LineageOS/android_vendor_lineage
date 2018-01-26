390f0b42b7e7491aa3e90231930d731c1f4a9a30
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-26 10:26:47 -0600

    backuptool: introduce addon.d script versioning
    * Since A/B addon.d scripts are going to need to do things in a
      specific way or things could go horribly wrong for a user, let's
      introduce versioning so that scripts can claim to be compatible.
    
    * A script can denote it is compatible with addon.d version 2 by
      adding: "# ADDOND_VERSION=2" somewhere in its script.
    
    * Only A/B will require version 2 scripts for now, and version 2
      scripts will still run on non-A/B. Additionally if a script does
      not explicitly denote its version, assume its version 1.
    
    * Version 1: The same old scripts we've always used. We cannot assume
                 these will all work with A/B backuptools.
    
    * Version 2: Scripts that denote they are compatible with version 2
                 must be aware of the fact that A/B devices will run this
                 script for a rom, during a seamless update, mounted at
                 /postinstall. The best way to ensure compatibility would
                 be to use the pre-designated functions found in the
                 backuptool[,_ab].functions scripts.
    
    Change-Id: I5573018dabd21bb64c7c964e2081806072a75243

--- prebuilt/common/bin/50-lineage.sh
+++ prebuilt/common/bin/50-lineage.sh
@@ -1,5 +1,7 @@
 #!/sbin/sh
 #
+# ADDOND_VERSION=2
+#
 # /system/addon.d/50-lineage.sh
 # During a LineageOS 15.1 upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
