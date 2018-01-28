58c64522206368a079c1d2cdd99a12a8194e159a
Sean hoyt <seanhoyt963@gmail.com>
2018-01-28 11:23:26 +0000

    soong: Forced shim
    Change-Id: Ifc121864803ab702ecb9a12bfca22dc27f0ee84e

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,6 +8,5 @@ lineage_soong:
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false)'; \
-        echo '    "Mtk_hardware": $(if $(filter true,$(BOARD_HAS_MTK_HARDWARE)),true,false),'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
