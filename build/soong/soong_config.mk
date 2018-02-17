52602baa49f6dc63519aa039b60e522f1b496480
Stricted <info@stricted.net>
2018-02-17 17:28:52 +0100

    lineage: soong: replace space with colon on TARGET_LD_SHIM_LIBS
    that way we can split the shim list logically into multiple lines like
    
    TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
        /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
        /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so
    
    to not have one single huge line
    
    Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,6 +8,6 @@ lineage_soong:
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
-	echo '    "Target_shim_libs": "$(TARGET_LD_SHIM_LIBS)"'; \
+	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
