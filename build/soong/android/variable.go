339edfaf16de76f0f04f9d34743ec614d8334ac0
Adrian DC <radian.dc@gmail.com>
2018-02-18 14:51:05 +0100

    lineage: Sort soong variables and configurations
    Change-Id: I4a191f8ce61123e30f780f8e39411a7723fb6197

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,21 +3,21 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	Target_shim_libs struct {
+		Cppflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
-	Target_shim_libs struct {
-		Cppflags []string
-	}
 }
 
 type ProductVariables struct {
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
-	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 }
