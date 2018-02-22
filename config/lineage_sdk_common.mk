464e39131d33249f3f85eac2c4240ae7ae3caf66
Joey <joey@lineageos.org>
2018-02-22 14:11:48 +0100

    vendor: add style api
    Change-Id: I52455cbf827aa5708e1eee6c8469d267979eddc8
    Signed-off-by: Joey <joey@lineageos.org>

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -4,6 +4,7 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.livedisplay.xml:system/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/lineage/config/permissions/org.lineageos.performance.xml:system/etc/permissions/org.lineageos.performance.xml \
     vendor/lineage/config/permissions/org.lineageos.profiles.xml:system/etc/permissions/org.lineageos.profiles.xml \
+    vendor/lineage/config/permissions/org.lineageos.style.xml:system/etc/permissions/org.lineageos.style.xml \
     vendor/lineage/config/permissions/org.lineageos.weather.xml:system/etc/permissions/org.lineageos.weather.xml
 
 # Lineage Platform Library
