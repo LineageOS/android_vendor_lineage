6f68de1a32f02251c4c990a5c584420af68affc4
Simon Shields <simon@lineageos.org>
2018-03-20 23:03:49 +1100

    lineage: add generic lineage targets
    These are used for building addonsu.
    
    Change-Id: If6def9729328ea2ce1e977fd7b9ff42cfc2f6dda

---
+++ build/target/product/lineage_arm.mk
@@ -0,0 +1,20 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_arm.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+TARGET_USES_64_BIT_BINDER := true
+
+PRODUCT_NAME := lineage_arm
