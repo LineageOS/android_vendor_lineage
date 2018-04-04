af0d53141b674263b8f9e55a6bfbcde6e1b3dfc2
Joey <joey@lineageos.org>
2018-04-04 20:21:18 +0200

    vendor: introduce Trust interface
    Change-Id: I1af2e4caab08b6b77db56e1be62247e55cd5a270
    Signed-off-by: Joey <joey@lineageos.org>

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -5,6 +5,7 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.performance.xml:system/etc/permissions/org.lineageos.performance.xml \
     vendor/lineage/config/permissions/org.lineageos.profiles.xml:system/etc/permissions/org.lineageos.profiles.xml \
     vendor/lineage/config/permissions/org.lineageos.style.xml:system/etc/permissions/org.lineageos.style.xml \
+    vendor/lineage/config/permissions/org.lineageos.trust.xml:system/etc/permissions/org.lineageos.trust.xml \
     vendor/lineage/config/permissions/org.lineageos.weather.xml:system/etc/permissions/org.lineageos.weather.xml
 
 # Lineage Platform Library
