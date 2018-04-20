441a1337ecd751447e6254c75e16edcc1b6ad9ab
Paul Keith <javelinanddart@gmail.com>
2018-04-20 03:15:04 +0200

    lineage: Transition to a list for text relocations
    Change-Id: Ib2f56e1cbdca1cc63446db13506b01561e0e7ad4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,10 +3,10 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
-	Needs_text_relocations struct {
+	Target_shim_libs struct {
 		Cppflags []string
 	}
-	Target_shim_libs struct {
+	Target_textrel_libs struct {
 		Cppflags []string
 	}
 	Uses_generic_camera_parameter_library struct {
@@ -23,9 +23,9 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_textrel_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
