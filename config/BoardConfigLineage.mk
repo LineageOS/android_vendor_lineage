5c17ceb2160002fff6b064fd30b663e02caf1a1a
Adrian DC <radian.dc@gmail.com>
2018-05-08 14:09:45 +0200

    lineage: Enable BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED
     * Our properties were supposed to go to /system/etc/prop.default
        after the following commit:
        "lineage: Move to Google's method of defining system default props"
        Change-Id: I6cb0e28a7599b010b389cc541015a37010a00f4b
    
     * However if BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED is not true,
        only /default.prop will contain the contents of
        ADDITIONAL_DEFAULT_PROPERTIES and PRODUCT_SYSTEM_DEFAULT_PROPERTIES
    
     * Enabling BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED globally adds
        system/etc/prop.default and (system/|)vendor/default.prop properly
    
    Change-Id: I6975960a83db0f61b57db0e145e9d51068e71360

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -3,6 +3,9 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
 # QCOM HW crypto
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
     TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
