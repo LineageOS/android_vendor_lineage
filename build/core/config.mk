dcc2dcc4037f2053c2b6cfc1e45b582b3d9d9cf2
Ethan Chen <intervigil@gmail.com>
2018-05-20 16:57:31 -0700

    build: Remove maven make rules
    * These were leftovers from cyngn days
    
    Change-Id: I11b17e7c8bbba9cf5ca2ccad304654180423f6ad

--- build/core/config.mk
+++ build/core/config.mk
@@ -20,9 +20,6 @@ FRAMEWORK_LINEAGE_PLATFORM_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_current.
 FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_removed.txt
 FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/lineage/build/core/apicheck_msg_current.txt
 
-BUILD_MAVEN_PREBUILT := $(TOP)/vendor/lineage/build/core/maven_artifact.mk
-PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/lineage/build/core/maven_artifact_publish.mk
-
 BUILD_HTTP_PREBUILT := $(TOP)/vendor/lineage/build/core/http_prebuilt.mk
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/lineage/build/core/system_rro.mk
