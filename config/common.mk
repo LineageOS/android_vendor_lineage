7820193ca68c2f712ea3f8ca4229eeefcb17a488
Demon Singur <demonsingur@gmail.com>
2018-06-07 17:48:06 +0000

    vendor: build black berry theme
    Change-Id: I35026f9c19b5fca2402b7dc937e789aa9e997bb6

--- config/common.mk
+++ config/common.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
 # Berry styles
 PRODUCT_PACKAGES += \
     LineageDarkTheme \
+    LineageBlackTheme \
     LineageAquaAccent \
     LineageTorchAccent \
     LineageBlackAccent \
