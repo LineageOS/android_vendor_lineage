5f9e3ad59180a6815a5c6528348b22c40866d541
AnilKumar Chimata <anilc@codeaurora.org>
2018-06-20 20:25:55 +0530

    soong_config: Add new flags for HW FDE
    Add new flags for HW FDE to pass CFlags to source code.
    
    CRs-Fixed: 2210986
    Change-Id: I70d68f015ee8eb8d123ce863ce8e36d0310bc082

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,13 @@
 package android
 type Product_variables struct {
+	Device_support_hwfde struct {
+		Cflags []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Device_support_hwfde_perf struct {
+		Cflags []string
+	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
@@ -25,6 +33,8 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Device_support_hwfde  *bool `json:",omitempty"`
+	Device_support_hwfde_perf  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
