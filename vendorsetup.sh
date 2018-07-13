d09a21f2837e7ccf0d452f6ac8dc250575d75ec4
orangutan <anonymous@very-private.com>
2018-07-13 19:45:37 +0530

    add sanders
    Change-Id: I99ba37499436bca5abc52eded5c543ad3a3d9ba7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,8 @@
-for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/lineage-build-targets | sed -e 's/#.*$//' | grep lineage-15.1 | awk '{printf "lineage_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
+#for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/lineage-build-targets | sed -e 's/#.*$//' | grep lineage-15.1 | awk '{printf "lineage_%s-%s\n", $1, $2}')
+#do
+#    add_lunch_combo $combo
+#done
+add_lunch_combo lineage_sanders-eng
+add_lunch_combo lineage_sanders-user
+add_lunch_combo lineage_sanders-userdebug
+
