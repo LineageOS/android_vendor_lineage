afc5599be4c05b29d3022920877d5c30cddc0ab9
dianlujitao <dianlujitao@lineageos.org>
2018-08-06 15:30:55 +0800

    soong_config: Add NO_SECURE_DISCARD flag
    Change-Id: I44e374361bf689942e721fbc247e06d74a056ba0

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -22,6 +22,9 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
+	No_secure_discard struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -33,4 +36,5 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+	No_secure_discard  *bool `json:",omitempty"`
 }
