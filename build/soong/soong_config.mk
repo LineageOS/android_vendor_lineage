3076f5d7bb4f5381e1dd8201b4948e9dea18c5ed
dianlujitao <dianlujitao@lineageos.org>
2018-08-06 15:30:55 +0800

    soong_config: Add NO_SECURE_DISCARD flag
    Change-Id: I44e374361bf689942e721fbc247e06d74a056ba0

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,6 +6,7 @@ lineage_soong:
 	echo '"Lineage": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "No_secure_discard": $(if $(filter true,$(BOARD_NO_SECURE_DISCARD)),true,false),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
