8960e3dfd669bfe2c7e36414c5990fea202111f6
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-07 17:44:14 -0700

    kernel: Set CROSS_COMPILE_ARM32 if using aarch64
    Google added a 32-bit vDSO to the Wahoo and Marlin kernels on Android P,
    requiring a 32-bit toolchain to compile/link, and the build fails when
    it is not provided.
    
    Change-Id: I700e66a417ed431c31d82fc950f5e5acd07ab281

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -269,6 +269,12 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
 else
     KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
 endif
+
+# Needed for CONFIG_COMPAT_VDSO, safe to set for all arm64 builds
+ifeq ($(KERNEL_ARCH),arm64)
+   KERNEL_CROSS_COMPILE += CROSS_COMPILE_ARM32="arm-linux-androideabi-"
+endif
+
 ccache =
 
 ifeq ($(HOST_OS),darwin)
