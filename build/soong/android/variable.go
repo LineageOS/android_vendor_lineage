af9697550607a5e2d206384b322fe65fe285e85c
Sam Mortimer <sam@mortimer.me.uk>
2018-08-17 14:23:57 -0700

    vendor/lineage: Add support for java source overlays
    *) Set JAVA_SOURCE_OVERLAYS in a device makefile as follows:
       # Java sourcefile overlays (for Android.bp built modules only)
       # Format is a whitespace separated set of rules, each of which
       # structured as follows:
       # modulename|overlay directory|glob pattern within overlay dir
       JAVA_SOURCE_OVERLAYS := \
               org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java
    
    *) As of this commit, java sources overlays are only allowed for
       module org.lineageos.hardware.
    
    Change-Id: I6be1c12567081357f5231a84df98ac002c0563b4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -30,6 +30,7 @@ type Product_variables struct {
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Java_Source_Overlays *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,6 +5,7 @@ _contents := $(_contents)    "Lineage":{$(newline)
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
 $(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
