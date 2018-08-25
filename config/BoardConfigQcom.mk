752597c313ffa66197106412bbe318474f39e117
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:17:23 -0700

    qcom: Declare PRODUCT_SOONG_NAMESPACES for HALs
    HALs are starting to move to blueprint files, which means that pathmap
    ifeqs no longer work. This results in modules with the same name in
    different platform HAL directories. Since these modules aren't used or
    linked anywhere outside the HAL directories and a device's device.mk,
    we can safely use a soong namespace to isolate the different HAL
    directories' blueprints. This defines the PRODUCT_SOONG_NAMESPACES
    make flag so make knows which namespace to look in as well.
    
    Change-Id: I549e6da863465d79b12c7453aa169d5d963afa52

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -79,6 +79,11 @@ endif
 endif
 endif
 
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \
+    hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT) \
+    hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT)
+
 # QCOM HW crypto
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
     TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
