3f683d2a8795e0d275e235eb9abf9c06f54b29ec
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-25 20:23:19 -0600

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Additional_gralloc_10_usage_bits struct {
+		Cppflags []string
+	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
@@ -25,6 +28,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
