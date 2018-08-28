c61fabe667d95b05117485566c920c44c0253644
Michael Bestas <mkbestas@lineageos.org>
2018-08-28 20:19:05 +0300

    soong_config: Add flag for legacy HW FDE
    Change-Id: I4ced98000e7349135dfb62bba9579e1dbf6b959c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -14,6 +14,9 @@ type Product_variables struct {
 	Supports_hw_fde_perf struct {
 		Cflags []string
 	}
+	Supports_legacy_hw_fde struct {
+		Cflags []string
+	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
@@ -42,6 +45,7 @@ type ProductVariables struct {
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
+	Supports_legacy_hw_fde  *bool `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
