d1601c6c1a303ed89eb632cebdb8a4f8861f4629
Michael Bestas <mkbestas@lineageos.org>
2018-08-28 20:19:05 +0300

    soong_config: Add flag for legacy HW FDE
    Change-Id: I4ced98000e7349135dfb62bba9579e1dbf6b959c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -11,6 +11,9 @@ type Product_variables struct {
 	Device_support_hwfde_perf struct {
 		Cflags []string
 	}
+	Device_support_legacy_hwfde struct {
+		Cflags []string
+	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
@@ -39,6 +42,7 @@ type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Device_support_hwfde  *bool `json:",omitempty"`
 	Device_support_hwfde_perf  *bool `json:",omitempty"`
+	Device_support_legacy_hwfde  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Java_Source_Overlays *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
