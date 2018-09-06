6f491622b9495169a35a247074b1ff0fc6501b08
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-06 08:48:26 -0700

    lineage: Dynamically add the right Sprint APNs
    New devices need the new Sprint APNs, so add a new board flag for
    them to set that will make the build system add the new APNs to
    the APN config XML. If the flag is not set, continue to use the
    old APNs.
    
    Change-Id: I7ff12a4342de2a7663b2b66fd627244214a8dc71

--- config/data_only.mk
+++ config/data_only.mk
@@ -1,6 +1,6 @@
 # World APN list
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+PRODUCT_PACKAGESS += \
+    apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
