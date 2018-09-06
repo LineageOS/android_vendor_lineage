5c14c73260345d5c7cde0329fcff4ab38dda81ef
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-06 21:31:10 +0200

    Enable wireless Playstation 4 controller support
    Change-Id: I306e93e779133f9cd177b673871d378b44dd1813

--- config/common.mk
+++ config/common.mk
@@ -64,6 +64,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.voip.xml
 
+# Enable wireless Playstation 4 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_054c_Product_05c4.kl:system/usr/keylayout/Vendor_054c_Product_05c4.kl \
+    frameworks/base/data/keyboards/Vendor_054c_Product_09cc.kl:system/usr/keylayout/Vendor_054c_Product_09cc.kl
+
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
