dcabbcd4b375b255e4cd40175064d18fde4fc829
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-06 08:48:26 -0700

    lineage: Dynamically add the right Sprint APNs
    New devices need the new Sprint APNs, so add a new board flag for
    them to set that will make the build system add the new APNs to
    the APN config XML. If the flag is not set, continue to use the
    old APNs.
    
    Change-Id: I7ff12a4342de2a7663b2b66fd627244214a8dc71

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -8,3 +8,27 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+# generate APN list with proper Sprint compatibility based on board flag
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf.xml
+LOCAL_MODULE_CLASS := ETC
+
+DEFAULT_APNS_FILE := vendor/lineage/prebuilt/common/etc/apns-conf.xml
+
+ifeq ($(TARGET_USES_NEW_SPRINT_APNS), true)
+SPRINT_APNS_SCRIPT := vendor/lineage/tools/sprint_apns.py
+FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
+
+$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(SPRINT_APNS_SCRIPT)
+$(FINAL_APNS_FILE): $(SPRINT_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
+	rm -f $@
+	python $(PRIVATE_SCRIPT) $@
+else
+FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
+endif
+
+LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+
+include $(BUILD_PREBUILT)
