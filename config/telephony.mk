0d2fa55f2f88e73018a749a3fe34f33ec365fbbc
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-06 08:48:26 -0700

    lineage: Dynamically add custom APNs
    For Example new devices need the new Sprint APNs, so add a new board flag for
    them to set that will make the build system add the new APNs to
    the APN config XML. If the flag is not set, continue to use the
    old APNs.
    To add or replace APNs, the custom_apns.py script has been
    added. If CUSTOM_APNS_FILE is defined, custom_apns.py
    is run and generates a new version of apns-conf.xml.
    
    Change-Id: I7ff12a4342de2a7663b2b66fd627244214a8dc71

--- config/telephony.mk
+++ config/telephony.mk
@@ -3,8 +3,8 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # World APN list
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+PRODUCT_PACKAGES += \
+    apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -8,3 +8,31 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+################################
+# Copies the APN list file into system/etc for the product as apns-conf.xml.
+# In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
+# is added or replaced to the $(DEFAULT_APNS_FILE).
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf.xml
+LOCAL_MODULE_CLASS := ETC
+
+DEFAULT_APNS_FILE := vendor/lineage/prebuilt/common/etc/apns-conf.xml
+
+ifdef CUSTOM_APNS_FILE
+CUSTOM_APNS_SCRIPT := vendor/lineage/tools/custom_apns.py
+FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
+
+$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(CUSTOM_APNS_SCRIPT)
+$(FINAL_APNS_FILE): PRIVATE_CUSTOM_APNS_FILE := $(CUSTOM_APNS_FILE)
+$(FINAL_APNS_FILE): $(CUSTOM_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
+	rm -f $@
+	python $(PRIVATE_SCRIPT) $@ $(PRIVATE_CUSTOM_APNS_FILE)
+else
+FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
+endif
+
+LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+
+include $(BUILD_PREBUILT)
