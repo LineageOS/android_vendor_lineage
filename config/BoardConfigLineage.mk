33f80e6d20b50752691e0bcb48ccb0e15da9333a
LuK1337 <priv.luk@gmail.com>
2018-09-10 20:57:46 +0200

    lineage: Add kernel source to product soong namespaces
    Change-Id: I9588fd6d588fa8961bb4564b8dc0ed6bfd8e1b9f

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -6,3 +6,12 @@ endif
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/lineage/config/BoardConfigQcom.mk
 endif
+
+TARGET_AUTO_KDIR := $(shell echo $(TARGET_DEVICE_DIR) | sed -e 's/^device/kernel/g')
+TARGET_KERNEL_SOURCE ?= $(TARGET_AUTO_KDIR)
+KERNEL_SRC := $(TARGET_KERNEL_SOURCE)
+
+ifneq "$(wildcard $(KERNEL_SRC) )" ""
+PRODUCT_SOONG_NAMESPACES += \
+    $(KERNEL_SRC)
+endif
