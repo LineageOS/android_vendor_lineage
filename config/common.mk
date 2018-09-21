73f5852492d4e842a8a46b5dfe893a9425d1224c
Christopher N. Hesse <raymanfx@gmail.com>
2018-09-21 23:00:26 +0200

    common: Allow custom vendor to override properties
    Change-Id: I0deb8a6865030b09ed6ba0ede1658319ca033e67

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,6 @@
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 PRODUCT_BRAND ?= LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
@@ -367,5 +370,3 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
-
-$(call inherit-product-if-exists, vendor/extra/product.mk)
