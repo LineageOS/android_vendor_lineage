96338125abbede669480b9a4838223f792f33e43
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-21 18:24:01 -0700

    lineage: Move qcom pathmap setting into "BoardConfig"
    Since we're making pathmaps set soong namespaces now, the project
    pathmaps need to be set at BoardConfig reading time. Move them to
    the right place.
    
    Example soong namespace list:
    PRODUCT_SOONG_NAMESPACES= hardware/ril hardware/qcom/audio-caf/msm8998
        hardware/qcom/display-caf/msm8998 hardware/qcom/media-caf/msm8998
        hardware/qcom/camera device/essential/mata/gps hardware/qcom/sensors
        vendor/qcom/opensource/location vendor/qcom/opensource/dataservices
        hardware/qcom/power hardware/qcom/thermal hardware/qcom/vr
        hardware/qcom/wlan-caf hardware/qcom/bt-caf
    
    Change-Id: I718a4d042b555802f769fe931b91e0338e6aee37

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -5,9 +5,7 @@ endif
 
 include vendor/lineage/config/BoardConfigKernel.mk
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/lineage/config/BoardConfigQcom.mk
-endif
 
 include vendor/lineage/config/BoardConfigSoong.mk
 
