ebe348ec354a625b7b5d4ff5d0bf3a6a147ae826
Isaac Chen <isaacchen@isaacchen.cn>
2018-10-03 15:29:15 +0530

    build: Support SDM710 platform
    Change-Id: I958a6bfd1087f5437f4074e614198bb622d476ce
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -6,7 +6,7 @@ B64_FAMILY := msm8992 msm8994
 BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
-UM_4_9_FAMILY := sdm845
+UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := sm8150 sm6150
 UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 
@@ -54,7 +54,7 @@ ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 sm6150 sm8150
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm710 sdm660 sdm845 sm6150 sm8150
 
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(B_FAMILY)
