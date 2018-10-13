4a80e0f08d0770e450ffdec9805c83daecf8340a
Tim Schumacher <timschumi@gmx.de>
2018-10-13 14:37:54 +0200

    repopick: Try to fast-forward first
    Change-Id: I4890ce8075556eb3f57d89c283543f620f39fa02

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -448,7 +448,7 @@ if __name__ == '__main__':
                 sys.exit(result)
         # Perform the cherry-pick
         if not args.pull:
-            cmd = ['git cherry-pick FETCH_HEAD']
+            cmd = ['git cherry-pick --ff FETCH_HEAD']
             if args.quiet:
                 cmd_out = open(os.devnull, 'wb')
             else:
