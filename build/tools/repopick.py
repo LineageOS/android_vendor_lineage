4995f93c45d48cef4dcecfbf47710678896aad7c
Vladimir Oltean <olteanv@gmail.com>
2018-10-23 02:32:11 +0300

    repopick: be able to kang yourself
    * Trying to repopick a change with -f -P|--path will fail as "already
      applied" if the remote repository was also cloned locally, as the
      script first checks the presence of the remote repo in the project
      pathmap.
    * This change gives priority of the -P parameter if it was provided.
    
    Change-Id: I8562272e2946143dc1e7159edc6bdad5d3435197
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -356,10 +356,10 @@ if __name__ == '__main__':
         #   - check that the project path exists
         project_path = None
 
-        if item['project'] in project_name_to_data and item['branch'] in project_name_to_data[item['project']]:
-            project_path = project_name_to_data[item['project']][item['branch']]
-        elif args.path:
+        if args.path:
             project_path = args.path
+        elif item['project'] in project_name_to_data and item['branch'] in project_name_to_data[item['project']]:
+            project_path = project_name_to_data[item['project']][item['branch']]
         elif args.ignore_missing:
             print('WARNING: Skipping {0} since there is no project directory for: {1}\n'.format(item['id'], item['project']))
             continue
