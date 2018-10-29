59e3a1166703da947c3762eb3e1ad50cc0d499f5
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-29 00:01:47 -0700

    config: Move vendor/extra inclusion before Lineage configuration
    This allows overriding Lineage configs with custom configs in vendor/extra
    (ex. a custom default alarm and notification sound)
    
    Change-Id: I0f796b1184035e6c348799cc28a5864f7f5055b7

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,5 @@
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 PRODUCT_BRAND ?= LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
@@ -367,5 +369,3 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
-
-$(call inherit-product-if-exists, vendor/extra/product.mk)
