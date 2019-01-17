134960c7feb387737880529695bf1df227a5c2e9
Vladimir Oltean <olteanv@gmail.com>
2019-01-17 23:41:35 +0200

    extract_utils: template: make --section argument non-positional
    Change-Id: I3b991bd125c0f92900d625158cd826b4b9f53a3b
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- build/templates/extract-files.sh
+++ build/templates/extract-files.sh
@@ -58,6 +58,6 @@ fi
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
