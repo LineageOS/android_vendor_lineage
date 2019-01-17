b78dc5355fe96b5a3caa8a96a9a8650d399031a8
Vladimir Oltean <olteanv@gmail.com>
2019-01-17 23:46:23 +0200

    extract_utils: template: introduce kang mode
    Change-Id: Idc196359a7cb6fd8de53ebdc23700322486a2a7d
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- build/templates/extract-files.sh
+++ build/templates/extract-files.sh
@@ -37,10 +37,13 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 SECTION=
+KANG=
 
 while [ "$1" != "" ]; do
     case "$1" in
         -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -k | --kang)            KANG="--kang"
                                 ;;
         -s | --section )        shift
                                 SECTION="$1"
@@ -59,6 +62,6 @@ fi
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
