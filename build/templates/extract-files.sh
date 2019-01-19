61b23fc32846f935fc7f328ac03c73f11c2451e2
Vladimir Oltean <olteanv@gmail.com>
2019-01-19 18:32:25 +0200

    extract_utils: template: add support for the dependency graph function
    Change-Id: I433c3521fb81a22b23f1c59a9bfaa7d00187f282
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- build/templates/extract-files.sh
+++ build/templates/extract-files.sh
@@ -36,11 +36,14 @@ source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
+GRAPH=
 KANG=
 
 while [ "$1" != "" ]; do
     case "$1" in
         -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -g | --graph)           GRAPH="--graph"
                                 ;;
         -k | --kang)            KANG="--kang"
                                 ;;
@@ -61,6 +64,6 @@ fi
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${GRAPH}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
