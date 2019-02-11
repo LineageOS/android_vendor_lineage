142f958effec1c1c94244f52a8147424df68d3c1
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 19:04:11 +0200

    addonsu: Don't mount SAR system to /system
    Change-Id: Ic2f0a994477a5bc04b32e24b0bd4baaa2773ff55

--- addonsu/unmount-system.sh
+++ addonsu/unmount-system.sh
@@ -3,13 +3,13 @@
 # Modern devices use /system as root ("/")
 system_as_root=`getprop ro.build.system_root_image`
 if [ "$system_as_root" == "true" ]; then
-  if umount /system && umount /system_root; then
+  if umount /system_root; then
     exit 0
   fi
-fi
-
-if umount /system; then
+else
+  if umount /system; then
     exit 0
+  fi
 fi
 
 exit 1
