79e91fe4ca7624f1c2c01234c39bf5e10647ab52
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 19:04:11 +0200

    addonsu: Don't mount SAR system to /system
    Change-Id: Ic2f0a994477a5bc04b32e24b0bd4baaa2773ff55

--- addonsu/mount-system.sh
+++ addonsu/mount-system.sh
@@ -1,18 +1,12 @@
 #!/sbin/sh
 
-if mount /system; then
-    exit 0
-fi
-
-# Try to get the block from /etc/recovery.fstab
-block=`cat /etc/recovery.fstab | cut -d '#' -f 1 | grep /system | grep -o '/dev/[^ ]*' | head -1`
-if [ -n "$block" ] && mount $block /system; then
-    exit 0
-fi
-
 # Modern devices use /system as root ("/")
 system_as_root=`getprop ro.build.system_root_image`
 if [ "$system_as_root" == "true" ]; then
+  if mount /system_root; then
+    exit 0
+  fi
+
   active_slot=`getprop ro.boot.slot_suffix`
   if [ ! -z "$active_slot" ]; then
     block=/dev/block/bootdevice/by-name/system$active_slot
@@ -20,7 +14,17 @@ if [ "$system_as_root" == "true" ]; then
     block=/dev/block/bootdevice/by-name/system
   fi
   mkdir -p /system_root
-  if mount -o rw $block /system_root && mount /system_root/system /system; then
+  if mount -o rw $block /system_root; then
+    exit 0
+  fi
+else
+  if mount /system; then
+    exit 0
+  fi
+
+  # Try to get the block from /etc/recovery.fstab
+  block=`cat /etc/recovery.fstab | cut -d '#' -f 1 | grep /system | grep -o '/dev/[^ ]*' | head -1`
+  if [ -n "$block" ] && mount $block /system; then
     exit 0
   fi
 fi
