6bfccc49d7140cb51c3c76fbfffa880a1ee824a5
dianlujitao <dianlujitao@lineageos.org>
2019-02-25 22:12:46 +0800

    Make custom off-mode charging screen great again
     * Move font_log.png to images dir
     * Add custom animation to work with AOSP charger
     * Make it opt-out
    
    Change-Id: Ibc7fa15d76df393f607a8f7263d7aa7c1cb58f2b

--- config/common.mk
+++ config/common.mk
@@ -192,8 +192,9 @@ PRODUCT_PACKAGES += \
     charger_res_images
 
 # Custom off-mode charger
-ifeq ($(WITH_LINEAGE_CHARGER),true)
+ifneq ($(WITH_LINEAGE_CHARGER),false)
 PRODUCT_PACKAGES += \
+    lineage_charger_animation \
     lineage_charger_res_images \
     font_log.png
 endif
