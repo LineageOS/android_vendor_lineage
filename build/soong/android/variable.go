4355b7cfa36f7b0d001234f7449678f96ae760a3
Erfan Abdi <erfangplus@gmail.com>
2019-03-09 12:03:41 +0200

    soong_config: Add flag for devices use metadata as FDE key
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Change-Id: I5f70280e72613a8f850cb3263c45e95e80cb8b3c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -35,6 +35,9 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
+	Uses_metadata_as_fde_key struct {
+		Cflags []string
+	}
 	Uses_nvidia_enhancements struct {
 		Cppflags []string
 	}
