f96fbb4f8bc213331988821d836a1018cd1f48c7
Rashed Abdel-Tawab <rashed@linux.com>
2019-03-29 20:08:26 -0700

    gms: Include turbo on arm64 targets
    Change-Id: I5780f302f1796cf76ff432f53dfca2523907a074

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,3 +1,6 @@
 ifeq ($(WITH_GMS),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+#ifeq ($(TARGET_ARCH),arm64)
+$(call inherit-product-if-exists, vendor/partner_gms/products/turbo.mk)
+#endif
 endif
