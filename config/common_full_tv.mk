5cce12d92d0c32ece440c8ad62f1b0d4d7652e76
Aaron Kling <webgeek1234@gmail.com>
2019-05-09 21:25:24 -0500

    lineage: atv: Inherit lineage atv device tree
    Change-Id: I869d9d24a61088762ea5cb1a3cc655ef11b0c521

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,9 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
+# Inherit Lineage atv device tree
+$(call inherit-product, device/lineage/atv/lineage_atv.mk)
+
 PRODUCT_PACKAGES += \
     AppDrawer \
     LineageCustomizer
