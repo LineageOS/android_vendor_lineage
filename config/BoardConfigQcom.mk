f3dbb4e17724c366e3072b234193c51525e6a999
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-17 12:03:57 -0700

    qcom: Add UM 4.14 platforms
    Change-Id: If246c0ec06de22097d369c17ce48aa1270bab19c

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -7,7 +7,8 @@ BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY)
+UM_4_14_FAMILY := sm8150 sm6150
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 
 BOARD_USES_ADRENO := true
 
@@ -53,7 +54,7 @@ ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 sm6150 sm8150
 
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(B_FAMILY)
@@ -79,8 +80,13 @@ ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(UM_4_9_FAMILY)
     QCOM_HARDWARE_VARIANT := sdm845
 else
+ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(UM_4_14_FAMILY)
+    QCOM_HARDWARE_VARIANT := sm8150
+else
     MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+endif
 endif
 endif
 endif
