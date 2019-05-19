fc5a8fa0ad8d7ec0f00e85d5d231136057d53b1b
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-18 23:28:52 -0700

    lineage: Build Fi GMS product if WITH_GMS_FI is defined
    Change-Id: Ia141fec1168e21726718cf11da83bb5ca0f68a56

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,3 +1,7 @@
 ifeq ($(WITH_GMS),true)
+ifeq ($(WITH_GMS_FI),true)
+$(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
+else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+endif
 endif
