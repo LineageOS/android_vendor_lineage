9e515409ff77021f3aa74e8e7feeea946dfc293f
Nolen Johnson <johnsonnolen@gmail.com>
2019-06-18 03:51:03 +0000

    lineage: partner_gms: Add minimal minimal configuration flag
    Change-Id: I245138b19448a193f8cbd12d5d26db295a4f4cdd

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -2,6 +2,10 @@ ifeq ($(WITH_GMS),true)
 ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
 else
+ifeq ($(WITH_GMS_MINIMAL),true)
+$(call inherit-product-if-exists, vendor/partner_gms/products/gms_minimal.mk)
+else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+endif
 endif
 endif
