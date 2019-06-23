c573d12085845f4990cf5e467c1d2617ad2b55ea
Nolen Johnson <johnsonnolen@gmail.com>
2019-06-23 03:02:31 +0000

    lineage: partner_gms: Automatically set minimal config on <1.7 GB System Images
    Change-Id: I64cb53525b6af992192a4fd521470770a7b65993

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -2,7 +2,7 @@ ifeq ($(WITH_GMS),true)
 ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
 else
-ifeq ($(WITH_GMS_MINIMAL),true)
+ifneq ($(filter 1, $(shell echo $$(($(BOARD_SYSTEMIMAGE_PARTITION_SIZE) < 1700000))),)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms_minimal.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
