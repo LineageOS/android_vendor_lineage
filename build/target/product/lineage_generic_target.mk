5c9807db8ab553ff6d36aa3cb1682b98bd5a1daf
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-01 17:44:22 -0700

    lineage: Add goldfish privapp whitelist and move to common lineage makefile
    Lineage GSIs now work.
    
    Test: lunch lineage_arm64-eng; m -j systemimage
    Change-Id: I416b184b4f601f12871f7c4da0181b189d91a21b

--- build/target/product/lineage_generic_target.mk
+++ build/target/product/lineage_generic_target.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_x86_64.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
-include vendor/lineage/build/target/product/lineage_generic_target.mk
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \
 
-PRODUCT_NAME := lineage_x86_64
+# Allow building otatools
+TARGET_FORCE_OTA_PACKAGE := true
