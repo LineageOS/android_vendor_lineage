00ec8534f4a567c1f62eb0964925e6ec4385b5ec
Bruno Martins <bgcngm@gmail.com>
2019-09-03 15:10:54 +0100

    Revert "Enable wireless Xbox 360 controller support"
    All kl/kcm/idc files available in fw/b are copied system image
    since a long time now.
    
    This reverts commit 918a7eaf8d0ddfad8a2c8f705417076ef74b7bbd.
    
    Change-Id: Icc3d6ce7b66bcad22715904244ae2eda9589dfa3

--- config/common.mk
+++ config/common.mk
@@ -79,10 +79,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Enable wireless Xbox 360 controller support
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
-
 # This is Lineage!
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml \
