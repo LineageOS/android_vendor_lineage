84948089b64663372ad505086999ecb7b328a35f
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 23:49:07 -0700

    build: Move to new qcom HAL directory layout
    All CAF forked HALs now reside in hardware/qcom-caf/
    
    Change-Id: I9f84cebf021418c0b402bc3186f37643e63afb24

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -10,24 +10,24 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-$(call set-device-specific-path,AUDIO,audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
-$(call set-device-specific-path,DISPLAY,display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
-$(call set-device-specific-path,MEDIA,media,hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT))
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
-$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom/bt-caf)
-$(call set-device-specific-path,CAMERA,camera,hardware/qcom/camera)
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
+$(call set-device-specific-path,CAMERA,camera,hardware/qcom-caf/camera)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
-$(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
-$(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
+$(call set-device-specific-path,GPS,gps,hardware/qcom-caf/gps)
+$(call set-device-specific-path,SENSORS,sensors,hardware/qcom-caf/sensors)
 $(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
-$(call set-device-specific-path,POWER,power,hardware/qcom/power)
-$(call set-device-specific-path,THERMAL,thermal,hardware/qcom/thermal)
-$(call set-device-specific-path,VR,vr,hardware/qcom/vr)
-$(call set-device-specific-path,WLAN,wlan,hardware/qcom/wlan-caf)
+$(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
+$(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 
 PRODUCT_CFI_INCLUDE_PATHS += \
-    hardware/qcom/wlan-caf/qcwcn/wpa_supplicant_8_lib
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 else
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio)
