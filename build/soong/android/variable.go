087f252b43eea718605b87b245f658e31d1f1946
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flags for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -26,6 +26,9 @@ type Product_variables struct {
 	Supports_legacy_hw_fde struct {
 		Cflags []string
 	}
+	Target_init_vendor_lib struct {
+		Whole_static_libs []string
+	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
@@ -80,6 +83,7 @@ type ProductVariables struct {
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
 	Supports_legacy_hw_fde  *bool `json:",omitempty"`
+	Target_init_vendor_lib *string `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
