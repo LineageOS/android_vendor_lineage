55d6ec583d993e0ddce3600e732bf7edbe9e51b1
Sam Mortimer <sam@mortimer.me.uk>
2019-09-10 11:40:34 -0700

    vendor/lineage: Support relative OUT_DIR in fixup_common_out_dir
    * For use with LINEAGE_FIXUP_COMMON_OUT=1, update
      symlink handling to work with relative OUT_DIR.
    
    * Fixes error:
    FAILED: ninja: error: mkdir(out/target/common/obj): No such file or directory
    
    Change-Id: I958be470b4d9ff2074442f210b1a73541e4981c8

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -966,14 +966,15 @@ function repopick() {
 function fixup_common_out_dir() {
     common_out_dir=$(get_build_var OUT_DIR)/target/common
     target_device=$(get_build_var TARGET_DEVICE)
+    common_target_out=common-${target_device}
     if [ ! -z $LINEAGE_FIXUP_COMMON_OUT ]; then
         if [ -d ${common_out_dir} ] && [ ! -L ${common_out_dir} ]; then
             mv ${common_out_dir} ${common_out_dir}-${target_device}
-            ln -s ${common_out_dir}-${target_device} ${common_out_dir}
+            ln -s ${common_target_out} ${common_out_dir}
         else
             [ -L ${common_out_dir} ] && rm ${common_out_dir}
             mkdir -p ${common_out_dir}-${target_device}
-            ln -s ${common_out_dir}-${target_device} ${common_out_dir}
+            ln -s ${common_target_out} ${common_out_dir}
         fi
     else
         [ -L ${common_out_dir} ] && rm ${common_out_dir}
