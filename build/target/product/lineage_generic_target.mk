0ff937143f046300788e4e37c86c1156870a4c83
Tim Schumacher <timschumi@gmx.de>
2019-09-11 16:14:28 +0200

    Allow building emulator as an sdk-addon
    This can be imported into Android Studio more easily, eventually even
    as a repository of LineageOS emulator images.
    
    (Build with `m sdk_addon`, it will end up in out/host/linux-x86/sdk_addon)
    
    Change-Id: Ia78524dd729b0f2ccc96f5a6eb2503cdc8b4d253

--- build/target/product/lineage_generic_target.mk
+++ build/target/product/lineage_generic_target.mk
@@ -19,3 +19,6 @@ PRODUCT_COPY_FILES += \
 
 # Allow building otatools
 TARGET_FORCE_OTA_PACKAGE := true
+
+PRODUCT_SDK_ADDON_NAME := lineage
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
