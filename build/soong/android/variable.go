774259732140a38d5ab17e76407b3fd05414e948
Alessandro Astone <ales.astone@gmail.com>
2019-09-11 12:33:20 +0200

    lineage: allow Has_legacy_camera_hal1 to configure Shared_libs and Overrides
     * mediaserver is now bp
    
    Change-Id: I07e42ca605a3677ae431b38b0e68e5248e0a608c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -10,7 +10,9 @@ type Product_variables struct {
 		Cflags []string
 	}
 	Has_legacy_camera_hal1 struct {
+		Overrides []string
 		Cflags []string
+		Shared_libs []string
 	}
 	Should_skip_waiting_for_qsee struct {
 		Cflags []string
