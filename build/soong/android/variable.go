a9f24d0173824dbde131e2f8a82ce03308741575
Jan Altensen <info@stricted.net>
2019-09-12 06:50:49 +0200

    soong_config: add TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE
    Change-Id: I96dd2f7dd629a06de288332e1d00be827029e89c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -34,6 +34,9 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -81,6 +84,7 @@ type ProductVariables struct {
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
