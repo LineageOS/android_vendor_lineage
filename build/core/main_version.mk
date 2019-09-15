d6ae6ee22efd998f28ec57bb3649bdae5895f09a
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-15 18:54:45 +0300

    build: set build fingerprint for all devices
    * there's no per device way to add props to /system/build.prop
    * adding it here is the only way so far
    
    Change-Id: Iafc286d9e479677540a4ec617db76715c018ae8e

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,3 +1,9 @@
+# Build fingerprint
+ifneq ($(BUILD_FINGERPRINT),)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+endif
+
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
