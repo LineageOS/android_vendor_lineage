b5406d45bc3e5576ae3eca001aaa884e1a64ab9c
Bruno Martins <bgcngm@gmail.com>
2019-09-19 15:35:25 +0200

    config: Be sure to include generic definitions
    Calling is-board-platform-in-list from within BoardConfigQcom.mk
    was not returning the desirable output, thus resulting in failure
    to meet the condition for the UM4.9 and UM4.14 family platforms.
    
    Make sure that the generic definitions are included upfront,
    so that the functions are exposed to BoardConfig*mk.
    
    Change-Id: Iad889882f94167f1bc1761ddd549dac9dcdee3ba

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -3,6 +3,9 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
+# Set generic definitions
+include vendor/lineage/build/core/definitions.mk
+
 include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
