e4221c9d29ba77e6b59ec9d3462535e4bae1a93c
Bruno Martins <bgcngm@gmail.com>
2019-09-19 15:35:25 +0200

    config: Fix setting UM4.9 and UM4.14 specific flags
    For whatever reason, calling is-board-platform-in-list
    is not returning the desirable output, thus resulting
    in failure to met the condition for UM4.9 and UM4.14
    family platforms.
    
    Change-Id: Iad889882f94167f1bc1761ddd549dac9dcdee3ba

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -42,7 +42,7 @@ ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 endif
 
 # Enable DRM PP driver on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)),true)
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     TARGET_USES_DRM_PP := true
 endif
 
@@ -51,7 +51,7 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 21)
 
 # Mark GRALLOC_USAGE_PRIVATE_10BIT_TP as valid gralloc bits on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)),true)
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 27)
 endif
 
