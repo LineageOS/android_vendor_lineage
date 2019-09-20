5aa93713e35276385bdea1df9c46822cd3adabd4
YumeMichi <do4suki@gmail.com>
2019-09-20 23:47:38 +0800

    REPOPICK ONLY: Patch [2/2]
    Change-Id: I6f47fb11ee0186ce9bdebe85bcd830816d0a5a0f
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -12,6 +12,9 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
+	Has_memfd_backport struct {
+		Cppflags []string
+	}
 	Should_skip_waiting_for_qsee struct {
 		Cflags []string
 	}
@@ -78,6 +81,7 @@ type ProductVariables struct {
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Has_memfd_backport  *bool `json:",omitempty"`
 	Should_skip_waiting_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
