ed76afdfd3ff470121334e501cb8435b4196a5eb
Han Wang <416810799@qq.com>
2019-09-21 17:28:12 +0200

    config: Restore org.lineageos.hardware.xml
     * Though there is no LineageHW anymore, don't forget
       LineageHardwareService in our sdk depends on it to start.
    
    Change-Id: I8befe1a75b66f54d9ffd7b88871c187a21b08497

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -1,6 +1,7 @@
 # Permissions for lineage sdk services
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.audio.xml \
+    vendor/lineage/config/permissions/org.lineageos.hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.hardware.xml \
     vendor/lineage/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/lineage/config/permissions/org.lineageos.performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.performance.xml \
     vendor/lineage/config/permissions/org.lineageos.profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.profiles.xml \
