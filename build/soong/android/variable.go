b08fa7584b479cc255c1d33dbc6de902b845381d
Michael Bestas <mkbestas@lineageos.org>
2019-09-22 17:19:49 +0300

    Revert "soong_config: Add flag for devices use metadata as FDE key"
    * Qcom reverted the relevant commit in cryptfs_hw
    
    This reverts commit a4d6ce67e76769bea45a1538c3e2d52b12dd9a38.
    
    Change-Id: Ife1812ebc80946953e3d1a558617c94ddbc7fad4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -40,9 +40,6 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
-	Uses_metadata_as_fde_key struct {
-		Cflags []string
-	}
 	Uses_nvidia_enhancements struct {
 		Cppflags []string
 	}
@@ -89,7 +86,6 @@ type ProductVariables struct {
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
