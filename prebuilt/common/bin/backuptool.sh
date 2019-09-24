baed2b7b146aef12de5d317aeac37bf3385ad3e3
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 20:18:11 +0200

    prebuilt: backuptool: Implement dynamic system mount for A-only
    * Dynamically mount system to the path chosen by the recovery through backuptool
    * This can be helpful because of the fragmentation that will happen with system mount in recovery after Q
    
    Change-Id: I2d1e775efcf87e33319bc7790d1e54bca72116d3
    (cherry picked from commit e40c31887d92035151cb480d063a617804b9f76c)

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -4,8 +4,14 @@
 #
 
 export C=/tmp/backupdir
+<<<<<<< HEAD   (2a621b partner_gms: Support TV GMS)
 export S=$2
 export V=16.0
+=======
+export SYSDEV="$(readlink -nf "$2")"
+export SYSFS="$3"
+export V=17.1
+>>>>>>> CHANGE (e40c31 prebuilt: backuptool: Implement dynamic system mount for A-o)
 
 export ADDOND_VERSION=1
 
@@ -94,11 +100,39 @@ if [ -d /tmp/addon.d/ ]; then
 fi
 }
 
+determine_system_mount() {
+  if grep -q -e"^$SYSDEV" /proc/mounts; then
+    umount $(grep -e"^$SYSDEV" /proc/mounts | cut -d" " -f2)
+  fi
+
+  if [ -d /mnt/system ]; then
+    SYSMOUNT="/mnt/system"
+  elif [ -d /system_root ]; then
+    SYSMOUNT="/system_root"
+  else
+    SYSMOUNT="/system"
+  fi
+
+  export S=$SYSMOUNT/system
+}
+
+mount_system() {
+  mount -t $SYSFS $SYSDEV $SYSMOUNT -o rw,discard
+}
+
+unmount_system() {
+  umount $SYSMOUNT
+}
+
+determine_system_mount
+
 case "$1" in
   backup)
+    mount_system
     mkdir -p $C
     if check_prereq; then
         if check_whitelist system; then
+            unmount_system
             exit 127
         fi
     fi
@@ -107,10 +141,13 @@ case "$1" in
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
+    unmount_system
   ;;
   restore)
+    mount_system
     if check_prereq; then
         if check_whitelist tmp; then
+            unmount_system
             exit 127
         fi
     fi
@@ -121,6 +158,7 @@ case "$1" in
     restore_addon_d
     rm -rf $C
     sync
+    unmount_system
   ;;
   *)
     echo "Usage: $0 {backup|restore}"
