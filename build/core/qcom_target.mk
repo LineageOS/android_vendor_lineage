6eaad8d36adff8c76452162260137e789594f05a
Michael Bestas <mkbestas@lineageos.org>
2019-09-25 21:08:30 +0300

    build: Remove qcom power HAL pathmap
    * Power HAL is a HIDL HAL now, so you can simply build
      a device specific one with a different name
    
    Change-Id: I88b0a71dbcc98cbc8e7b5c284a3956e259d21115

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -17,7 +17,6 @@ $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VA
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
-$(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
 $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
