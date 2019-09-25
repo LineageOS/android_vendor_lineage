e9731bb5ddbe219df3cb71141772c37ce0a0abac
Michael Bestas <mkbestas@lineageos.org>
2019-09-25 21:11:43 +0300

    build: Don't set pathmap for repositories that do not exist
    * Camera, gps and sensors are device specific most of the time
      so we do not sync the generic repositories for these projects
    
    Change-Id: Ibb5d0066e362d285a862ea0c0f2a2c824f84f73d

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -15,11 +15,7 @@ $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWAR
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
-$(call set-device-specific-path,CAMERA,camera,hardware/qcom-caf/camera)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
-$(call set-device-specific-path,GPS,gps,hardware/qcom-caf/gps)
-$(call set-device-specific-path,SENSORS,sensors,hardware/qcom-caf/sensors)
-$(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
