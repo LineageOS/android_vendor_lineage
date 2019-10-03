10af19039c2409175e42037edd73d9e41d47fcd0
Michael Bestas <mkbestas@lineageos.org>
2019-10-03 16:25:16 +0300

    Revert "soong_config: Add flag for legacy HW FDE"
    * No longer needed with HIDL cryptfs HALs
    
    This reverts commit c61fabe667d95b05117485566c920c44c0253644.
    
    Change-Id: Idea6bbec97af3951a0be2c3df4068cdad7ef8b0a

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -12,7 +12,6 @@ $(call add_json_bool, Should_skip_waiting_for_qsee, $(filter true,$(TARGET_KEYMA
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_bool, Supports_hw_fde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Supports_hw_fde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
-$(call add_json_bool, Supports_legacy_hw_fde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
