20e76d3fadc0183cabd76c1c9a564560691c0b3e
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-23 08:35:32 -0500

    lineage: use AB_OTA_PARTITIONS flag for including backuptool_ab
    * Newer pixel devices get AB_OTA_UPDATER enabled from
      build/make/target/board/BoardConfigMainlineCommon.mk which
      is not included in time for this check.
    
    Change-Id: I1679709b7178398ffaa62ad14a6d042ff377fd5a

--- config/common.mk
+++ config/common.mk
@@ -38,7 +38,7 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh \
     vendor/lineage/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
 
-ifeq ($(AB_OTA_UPDATER),true)
+ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/lineage/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
