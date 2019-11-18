dfda4c89098a61803b3aab6c9fec72cab65b6e2c
Simon Shields <simon@lineageos.org>
2019-11-18 23:56:08 +1100

    repopick: fix --check-picked on Python 3
    This is very subtly broken: we look for the string 'Change-Id:'
    in an array of byte strings. Fix this by decoding the git output
    to utf-8 strings.
    
    Change-Id: I708ad0adacb61c89bfba0fd88eeb2e37648317af

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -392,7 +392,7 @@ if __name__ == '__main__':
         for i in range(0, check_picked_count):
             if subprocess.call(['git', 'cat-file', '-e', 'HEAD~{0}'.format(i)], cwd=project_path, stderr=open(os.devnull, 'wb')):
                 continue
-            output = subprocess.check_output(['git', 'show', '-q', 'HEAD~{0}'.format(i)], cwd=project_path).split()
+            output = subprocess.check_output(['git', 'show', '-q', 'HEAD~{0}'.format(i)], cwd=project_path).decode('utf-8').split()
             if 'Change-Id:' in output:
                 head_change_id = ''
                 for j,t in enumerate(reversed(output)):
