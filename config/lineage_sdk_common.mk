315e2450f3d6940e18cb7affe12e932a7e411352
Bruno Martins <bgcngm@gmail.com>
2019-11-21 15:43:49 +0000

    Revert "vendor: add style api"
    Styles API was nuked from the SDK.
    
    This reverts commit 6cf7640e96fe4c7665d4c9349bb31fdefa447e5e.
    
    Change-Id: I99405c493b57045c5c7c6331ab64fad49d160840

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -6,7 +6,6 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.performance.xml \
     vendor/lineage/config/permissions/org.lineageos.profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.profiles.xml \
     vendor/lineage/config/permissions/org.lineageos.settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.settings.xml \
-    vendor/lineage/config/permissions/org.lineageos.style.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.style.xml \
     vendor/lineage/config/permissions/org.lineageos.trust.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.trust.xml \
     vendor/lineage/config/permissions/org.lineageos.weather.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.weather.xml
 
