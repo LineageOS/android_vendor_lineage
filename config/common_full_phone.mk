bed5408a7cd987d460fb8777cd1faaedf44b7fda
Nolen Johnson <johnsonnolen@gmail.com>
2019-12-05 00:49:59 +0000

    lineage: config: common_full_phone: Be mini phone, but build Recorder
    Change-Id: I097e1a5c264507b59d4516c0b12adde488fbb0f0

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,11 +1,12 @@
 # Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+$(call inherit-product, vendor/lineage/config/common_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
-# Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+# Recorder
+PRODUCT_PACKAGES += \
+    Recorder
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
