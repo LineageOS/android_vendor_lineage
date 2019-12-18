95b8fa97ba80e3996d3ec488d21a5ebc569cdc31
Arne Coucheron <arco68@gmail.com>
2019-12-18 06:34:24 +0100

    default-permissions: Grant default permissions to com.android.exchange
    Change-Id: I9dfcecde0e662b9e481696b661e16ac7e131a75c

--- config/common.mk
+++ config/common.mk
@@ -87,6 +87,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lineage-power-whitelist.xml
 
+# Pre-granted permissions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/lineage-default-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/lineage-default-permissions.xml
+
 # Include AOSP audio files
 include vendor/lineage/config/aosp_audio.mk
 
