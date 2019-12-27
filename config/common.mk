3b663443db9c9e7de510772fb0c6c21173231e4c
Nolen Johnson <johnsonnolen@gmail.com>
2019-12-27 06:57:50 +0000

    lineage: Disable privapp permission enforcement (make it log)
    Change-Id: If8f44fafae051434f0fffdddb678922f09a8bcdb

--- config/common.mk
+++ config/common.mk
@@ -76,7 +76,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
