20c4c0a3450ad3a35f5379006c06aaa4f7e58d37
Aaron Kling <webgeek1234@gmail.com>
2019-12-28 14:51:17 -0600

    Expose Tegra version to blueprint
    Change-Id: I8757063b189cf1ae67b7d111179df6cb1c730285

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -32,6 +32,10 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_tegra_version struct {
+		Cppflags []string
+		Srcs []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -85,6 +89,7 @@ type ProductVariables struct {
 	Supports_legacy_hw_fde  *bool `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_tegra_version  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
