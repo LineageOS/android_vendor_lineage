cf286ca8c50cf039ae16d98078a949579eee1bd8
Davide Garberi <dade.garberi@gmail.com>
2020-01-19 17:44:11 +0100

    backuptool: Run check_{black, white}list with proper system path
     * If any of these two function gets run on a recovery mounting system
       to /system, /system/addon.d won't exist while /system/system/addon.d will.
     * Run the functions with $S as argument to make this work correctly
    
    Change-Id: I02e7b91429a9e74d28bdb77e56955dad97ca75ac

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -126,12 +126,12 @@ case "$1" in
     mount_system
     mkdir -p $C
     if check_prereq; then
-        if check_whitelist system; then
+        if check_whitelist $S; then
             unmount_system
             exit 127
         fi
     fi
-    check_blacklist system
+    check_blacklist $S
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
