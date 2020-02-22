fa0160c0d866f38c6e284c1f950f74d063784d00
Aaron Kling <webgeek1234@gmail.com>
2020-02-22 01:25:46 -0600

    Split mobile and tv configs
    There are a lot of apps installed by default that are unusable with
    a controller. This splits the mobile related settings out of common
    into a common_mobile config and adds common_tv.
    
    Note: The EXCLUDE_AUDIOFX flag was removed from the tv configs
    because the new config path no longer builds it.
    
    Change-Id: I67db32750fca91725d703243f2bcee8bcd9b82cd

---
+++ config/common_mobile.mk
@@ -0,0 +1,65 @@
+# Inherit common mobile Lineage stuff
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Default notification/alarm sounds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+# Thank you, please drive thru!
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
+endif
+
+# Optional packages
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    PhotoTable
+
+# AOSP packages
+PRODUCT_PACKAGES += \
+    Email \
+    ExactCalculator \
+    Exchange2
+
+# Lineage packages
+PRODUCT_PACKAGES += \
+    AudioFX \
+    Backgrounds \
+    Eleven \
+    Jelly \
+    LockClock \
+    Profiles \
+    TrebuchetQuickStep \
+    WeatherProvider
+
+# Accents
+PRODUCT_PACKAGES += \
+    LineageBlackTheme \
+    LineageDarkTheme \
+    LineageBlackAccent \
+    LineageBlueAccent \
+    LineageBrownAccent \
+    LineageCyanAccent \
+    LineageGreenAccent \
+    LineageOrangeAccent \
+    LineagePinkAccent \
+    LineagePurpleAccent \
+    LineageRedAccent \
+    LineageYellowAccent
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Custom off-mode charger
+ifeq ($(WITH_LINEAGE_CHARGER),true)
+PRODUCT_PACKAGES += \
+    lineage_charger_res_images \
+    font_log.png \
+    libhealthd.lineage
+endif
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
