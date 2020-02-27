8387a26b1f79924b71e2a5fe1e33164d268ec7d0
Andreas Schneider <asn@cryptomilk.org>
2020-02-27 18:36:57 +0100

    kernel: Create dt.img
    This creates a dt.img according to:
    https://source.android.com/devices/architecture/dto/partitions
    
    Setting BOARD_CUSTOM_DTIMG_MK a device e.g. required to create a dt.img
    using dtbtool can create its own file.
    
    Change-Id: I3b6ba14a345ab42c34805a643408a81aaf39f852

--- build/tasks/dt_image.mk
+++ build/tasks/dt_image.mk
@@ -1,65 +1,30 @@
-#----------------------------------------------------------------------
-# Generate device tree image (dt.img)
-#----------------------------------------------------------------------
-ifeq ($(strip $(BOARD_CUSTOM_BOOTIMG_MK)),)
+ifneq ($(TARGET_NO_KERNEL),true)
 ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DT)),true)
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
-ifeq ($(strip $(BOARD_KERNEL_PREBUILT_DT)),)
-
-ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
-DTBTOOL_NAME := dtbToolLineage
+ifneq ($(BOARD_CUSTOM_DTIMG_MK),)
+include $(BOARD_CUSTOM_DTIMG_MK)
 else
-DTBTOOL_NAME := $(TARGET_CUSTOM_DTBTOOL)
-endif
 
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/$(DTBTOOL_NAME)$(HOST_EXECUTABLE_SUFFIX)
+MKDTIMG := $(HOST_OUT_EXECUTABLES)/mkdtimg$(HOST_EXECUTABLE_SUFFIX)
 
-ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
-# dtbToolLineage will search subdirectories
-possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-else
-# Most specific paths must come first in possible_dtb_dirs
-possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-endif
+# Most specific paths must come first in possible_dt_dirs
+possible_dt_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts $(KERNEL_OUT)/arch/arm/boot/dts
 
 define build-dtimage-target
-    $(call pretty,"Target dt image: $@")
-    $(hide) for dir in $(possible_dtb_dirs); do \
-        if [ -d "$$dir" ]; then \
-            dtb_dir="$$dir"; \
-            break; \
-        fi; \
-    done; \
-    $(DTBTOOL) $(BOARD_DTBTOOL_ARGS) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ "$$dtb_dir";
+    $(call pretty,"Target dt image: $(BOARD_PREBUILT_DTIMAGE)")
+    $(hide) for dir in $(possible_dt_dirs); do \
+                if [ -d "$$dir" ]; then \
+                    dt_dir="$$dir"; \
+                    break; \
+                fi; \
+            done; \
+            $(MKDTIMG) create $@ --page_size=$(BOARD_KERNEL_PAGESIZE) $$(find "$$dt_dir" -type f -name '*.dtb' | sort)
     $(hide) chmod a+r $@
 endef
 
-ifeq ($(strip $(BOARD_KERNEL_LZ4C_DT)),true)
-LZ4_DT_IMAGE := $(PRODUCT_OUT)/dt-lz4.img
-endif
-
-$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
+$(BOARD_PREBUILT_DTIMAGE): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET)
 	$(build-dtimage-target)
-ifeq ($(strip $(BOARD_KERNEL_LZ4C_DT)),true)
-	prebuilts/tools-lineage/${HOST_OS}-x86/bin/lz4 -9 < $@ > $(LZ4_DT_IMAGE)
-	$(hide) $(ACP) $(LZ4_DT_IMAGE) $@
-endif
-	@echo "Made DT image: $@"
 
-else
-
-$(INSTALLED_DTIMAGE_TARGET) : $(BOARD_KERNEL_PREBUILT_DT) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-endif # BOARD_KERNEL_PREBUILT_DT
-
-ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTIMAGE_TARGET)
-ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_DTIMAGE_TARGET)
-
-.PHONY: dtimage
-dtimage: $(INSTALLED_DTIMAGE_TARGET)
-
-
-endif
-endif
+endif # BOARD_CUSTOM_DTIMG_MK
+endif # BOARD_KERNEL_SEPARATED_DT
+endif # TARGET_NO_KERNEL
