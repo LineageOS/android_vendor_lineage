f5f830ceafb9c44a6d6553128f59dfb8d5affd07
Arne Coucheron <arco68@gmail.com>
2020-02-29 04:29:28 +0100

    config: Don't add mini-debug-info to the boot classpath
    Change-Id: Ibce3cf86ce5534ee6a37a55188d813a219140622

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -3,6 +3,9 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
+# DexPreopt debug info
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
