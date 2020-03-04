LineageOS/android_vendor_lineage

commit fa8a442ba233df8ff5c683bf0ee770f7b57549d8

backuptool: Properly unmount system partition
For non AB devices system partition should be unmounted
if check_prereq function fails.

This patch also refactors backuptool a bit for AB devices
in order to look same as backuptool for non AB devices.

Change-Id: Ia1f4ae95c9e4dae4df844853e81c264bc838f177

prebuilt/common/bin/backuptool.sh
@@ -33,12 +33,13 @@ restore_addon_d() {
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
 if [ ! -r $S/build.prop ]; then
-    exit 127
+    return 0
 fi
 if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
   echo "Not backing up files from incompatible version: $V"
-  exit 127
+  return 0
 fi
+return 1
 }
 
 # Execute /system/addon.d/*.sh scripts with $1 parameter
@@ -80,7 +81,10 @@ case "$1" in
   backup)
     mount_system
     mkdir -p $C
-    check_prereq
+    if ! check_prereq; then
+      unmount_system
+      exit 127
+    end
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
@@ -89,7 +93,10 @@ case "$1" in
   ;;
   restore)
     mount_system
-    check_prereq
+    if ! check_prereq; then
+      unmount_system
+      exit 127
+    end
     run_stage pre-restore
     run_stage restore
     run_stage post-restore

prebuilt/common/bin/backuptool_ab.sh
@@ -47,13 +47,13 @@ restore_addon_d() {
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
 if [ ! -r /system/build.prop ]; then
-    exit 127
+    return 0
 fi
-
-grep -q "^ro.lineage.version=$V.*" /system/build.prop && return 1
-
-echo "Not backing up files from incompatible version: $V"
-exit 127
+if ! grep -q "^ro.lineage.version=$V.*" /system/build.prop; then
+    echo "Not backing up files from incompatible version: $V"
+    return 0
+fi
+return 1
 }
 
 # Execute /system/addon.d/*.sh scripts with $1 parameter
@@ -74,14 +74,18 @@ fi
 case "$1" in
   backup)
     mkdir -p $C
-    check_prereq
+    if ! check_prereq; then
+      exit 127
+    fi
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
   ;;
   restore)
-    check_prereq
+    if ! check_prereq; then
+      exit 127
+    fi
     run_stage pre-restore
     run_stage restore
     run_stage post-restore

commit 2eda70659805cd7836615af919ce77e53923cd0d

backuptool: Fix backup/restore functions
Backup/restore functionality was broken in
commit fa8a442ba233df8ff5c683bf0ee770f7b57549d8
because of incorrect check of the function's result.

Also 'if COMMANDS; then' blocks should be closed
with 'fi' keyword.

Change-Id: Ic977dba675df58a228ef4b882b25beb66cc9d2c6

prebuilt/common/bin/backuptool.sh
@@ -81,10 +81,10 @@ case "$1" in
   backup)
     mount_system
     mkdir -p $C
-    if ! check_prereq; then
+    if check_prereq; then
       unmount_system
       exit 127
-    end
+    fi
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
@@ -93,10 +93,10 @@ case "$1" in
   ;;
   restore)
     mount_system
-    if ! check_prereq; then
+    if check_prereq; then
       unmount_system
       exit 127
-    end
+    fi
     run_stage pre-restore
     run_stage restore
     run_stage post-restore

prebuilt/common/bin/backuptool_ab.sh
@@ -74,7 +74,7 @@ fi
 case "$1" in
   backup)
     mkdir -p $C
-    if ! check_prereq; then
+    if check_prereq; then
       exit 127
     fi
     preserve_addon_d
@@ -83,7 +83,7 @@ case "$1" in
     run_stage post-backup
   ;;
   restore)
-    if ! check_prereq; then
+    if check_prereq; then
       exit 127
     fi
     run_stage pre-restore