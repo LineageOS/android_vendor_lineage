a24144faf2f81a02af9006fb95d60f0273914cb1
dianlujitao <dianlujitao@lineageos.org>
2020-03-11 19:05:29 +0800

    kernel: Add bison and flex to build environment
     * Required since Linux 4.16
    
    Change-Id: I107ea1b097d72a7b5251273c8c7e33ad6585b59c
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -149,6 +149,11 @@ endif
 KERNEL_MAKE_FLAGS += HOSTCC=$(KERNEL_HOST_TOOLCHAIN_ROOT)gcc
 KERNEL_MAKE_FLAGS += HOSTCXX=$(KERNEL_HOST_TOOLCHAIN_ROOT)g++
 
+# Since Linux 4.16, flex and bison are required
+KERNEL_MAKE_FLAGS += LEX=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/flex
+KERNEL_MAKE_FLAGS += YACC=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/bison
+TOOLS_PATH_OVERRIDE += BISON_PKGDATADIR=$(BUILD_TOP)/prebuilts/build-tools/common/bison
+
 # Set the out dir for the kernel's O= arg
 # This needs to be an absolute path, so only set this if the standard out dir isn't used
 OUT_DIR_PREFIX := $(shell echo $(OUT_DIR) | sed -e 's|/target/.*$$||g')
