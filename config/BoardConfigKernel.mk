70635f37a7d40f05a9bb5d93d529cc37b1a142e2
dianlujitao <dianlujitao@lineageos.org>
2020-03-11 19:05:29 +0800

    kernel: Add bison data directory to build environment
    Change-Id: I107ea1b097d72a7b5251273c8c7e33ad6585b59c
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -128,7 +128,8 @@ endif
 TOOLS_PATH_OVERRIDE := \
     PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-lineage/common/perl-base
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-lineage/common/perl-base \
+    BISON_PKGDATADIR=$(BUILD_TOP)/prebuilts/tools-lineage/common/bison
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)
