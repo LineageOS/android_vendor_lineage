f2da88b973dfb283d3dc19e548c470d061996df0
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-26 08:15:14 +0100

    Allow A/B OTA downgrades on debuggable builds
    Change-Id: I361c6d71f7b4316cf023ad0b36fdc488e37d6176

--- config/common.mk
+++ config/common.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/lineage/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
     vendor/lineage/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ota.allow_downgrade=true
+endif
 endif
 
 # Backup Services whitelist
