e540e08ca0349cfbf7443cbbc1dd4f3866611228
theimpulson <aayushgupta219@gmail.com>
2020-03-31 03:41:01 +0000

    soong_config: Add TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>
    Change-Id: I2c56fd1c09ead3e33fa2f2eb782971bd1dd9d666
    (cherry picked from commit 461ebc0b54c6eae3826717eb1e9fb453b70f8853)

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -32,6 +32,9 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_ignores_ftp_pptp_conntrack_failure struct {
+		Cppflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -83,6 +86,11 @@ type ProductVariables struct {
 	Supports_legacy_hw_fde  *bool `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+<<<<<<< HEAD   (c493cb Split mobile and tv configs)
+=======
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
+	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
+>>>>>>> CHANGE (461ebc soong_config: Add TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
