807dae4567b9bb755c1e5787f7dd60841d30ac7d
Aaron Kling <webgeek1234@gmail.com>
2020-04-23 14:47:24 -0500

    atv: Build LeanbackIME
    Change-Id: I8942916c4e63be11fc2b1d9aed756e4c9887498a

--- config/common_tv.mk
+++ config/common_tv.mk
@@ -4,6 +4,11 @@ $(call inherit-product, vendor/lineage/config/common.mk)
 # Inherit Lineage atv device tree
 $(call inherit-product, device/lineage/atv/lineage_atv.mk)
 
+# AOSP packages
+PRODUCT_PACKAGES += \
+    LeanbackIME
+
+# Lineage packages
 PRODUCT_PACKAGES += \
     AppDrawer \
     LineageCustomizer
