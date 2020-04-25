56a0ed9de1230ec3896999b041394159fe41f1bd
dianlujitao <dianlujitao@lineageos.org>
2020-04-25 21:08:10 +0800

    tools: Add updater class to update-sha1sums.py
    Co-authored-by: Pig <pig.priv@gmail.com>
    Change-Id: Idaebe34e18d16d042e247f0f2b06c8012fe4d287

--- build/tools/update_sha1sums.py
+++ build/tools/update_sha1sums.py
@@ -20,54 +20,67 @@ import os
 import sys
 from hashlib import sha1
 
-device = ''
-vendor = ''
+class Updater:
+    @staticmethod
+    def setup(device='', vendor=''):
+        global DEVICE, VENDOR, VENDOR_PATH
+        DEVICE = device
+        VENDOR = vendor
+        VENDOR_PATH = os.path.join(
+        *['..', '..', '..', 'vendor', VENDOR, DEVICE, 'proprietary'])
 
-with open('proprietary-files.txt', 'r') as f:
-    lines = f.read().splitlines()
-vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
-needSHA1 = False
+    def __init__(self, filename):
+        self.filename = filename
+        with open(self.filename, 'r') as f:
+            self.lines = f.read().splitlines()
 
+    def write(self):
+        with open(self.filename, 'w') as f:
+            f.write('\n'.join(self.lines) + '\n')
 
-def cleanup():
-    for index, line in enumerate(lines):
-        # Skip empty or commented lines
-        if len(line) == 0 or line[0] == '#' or '|' not in line:
-            continue
+    def cleanup(self):
+        for index, line in enumerate(self.lines):
+            # Skip empty or commented lines
+            if len(line) == 0 or line[0] == '#' or '|' not in line:
+                continue
 
-        # Drop SHA1 hash, if existing
-        lines[index] = line.split('|')[0]
+            # Drop SHA1 hash, if existing
+            self.lines[index] = line.split('|')[0]
 
+        self.write()
 
-def update():
-    for index, line in enumerate(lines):
-        # Skip empty lines
-        if len(line) == 0:
-            continue
+    def update(self):
+        need_sha1 = False
+        for index, line in enumerate(self.lines):
+            # Skip empty lines
+            if len(line) == 0:
+                continue
 
-        # Check if we need to set SHA1 hash for the next files
-        if line[0] == '#':
-            needSHA1 = (' - from' in line)
-            continue
+            # Check if we need to set SHA1 hash for the next files
+            if line[0] == '#':
+                need_sha1 = (' - from' in line)
+                continue
 
-        if needSHA1:
-            # Remove existing SHA1 hash
-            line = line.split('|')[0]
+            if need_sha1:
+                # Remove existing SHA1 hash
+                line = line.split('|')[0]
 
-            filePath = line.split(';')[0].split(':')[-1]
-            if filePath[0] == '-':
-                filePath = filePath[1:]
+                file_path = line.split(';')[0].split(':')[-1]
+                if file_path[0] == '-':
+                    file_path = file_path[1:]
 
-            with open(os.path.join(vendorPath, filePath), 'rb') as f:
-                hash = sha1(f.read()).hexdigest()
+                with open(os.path.join(VENDOR_PATH, file_path), 'rb') as f:
+                    hash = sha1(f.read()).hexdigest()
 
-            lines[index] = '%s|%s' % (line, hash)
+                self.lines[index] = '{}|{}'.format(line, hash)
 
+        self.write()
 
-if len(sys.argv) == 2 and sys.argv[1] == '-c':
-    cleanup()
-else:
-    update()
-
-with open('proprietary-files.txt', 'w') as file:
-    file.write('\n'.join(lines) + '\n')
+    @staticmethod
+    def finish():
+        for file in ['../../../device/' + VENDOR + '/' + DEVICE + '/proprietary-files.txt']:
+            updater = Updater(file)
+        if len(sys.argv) == 2 and sys.argv[1] == '-c':
+            updater.cleanup()
+        else:
+            updater.update()
