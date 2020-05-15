7913549370d6122b88fbc22a26e6912b092484c8
Chirayu Desai <chirayudesai1@gmail.com>
2020-05-15 18:20:18 +0530

    kernel: Set full path to DTC
    * When OUT_DIR_COMMON_BASE isn't set, this path is relative (i.e. out/),
      which won't work
    
    Change-Id: Ia8d0eb40c0f4153c7125bffd5a8f2b509b8ec142

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -203,7 +203,7 @@ PATH_OVERRIDE += $(TOOLS_PATH_OVERRIDE)
 
 KERNEL_ADDITIONAL_CONFIG_OUT := $(KERNEL_OUT)/.additional_config
 
-KERNEL_MAKE_FLAGS += DTC=$(DTC)
+KERNEL_MAKE_FLAGS += DTC=$(KERNEL_BUILD_OUT_PREFIX)$(DTC)
 
 # Internal implementation of make-kernel-target
 # $(1): output path (The value passed to O=)
