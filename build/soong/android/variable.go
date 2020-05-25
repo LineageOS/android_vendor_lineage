48917cc909c9b5a5548d822c59a173fa501686f3
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:08:31 +0800

    Export qcom UM hal root to soong
    Change-Id: Iabb75e8745dba9069c2eab0d5ef41b2171f4c048

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -14,6 +14,10 @@ type Product_variables struct {
 		Overrides []string
 		Shared_libs []string
 	}
+	Qcom_um_soong_namespace struct {
+		Cflags []string
+		Header_libs []string
+	}
 	Recovery_skip_ev_rel_input struct {
 		Cflags []string
 	}
@@ -66,6 +70,7 @@ type ProductVariables struct {
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Qcom_um_soong_namespace  *string `json:",omitempty"`
 	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
