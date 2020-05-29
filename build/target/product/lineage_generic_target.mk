e999a802427bb0508112abf308bedf0b8b5df606
Sam Mortimer <sam@mortimer.me.uk>
2020-05-29 00:37:31 -0700

    vendor/lineage: emulator should have mobile config
    * It's missing launcher and browser (etc) otherwise after:
      https://review.lineageos.org/q/I67db32750fca91725d703243f2bcee8bcd9b82cd
    
    Change-Id: Ie7c1983eb88c9bf1411c069303e3e55fda150f06

--- build/target/product/lineage_generic_target.mk
+++ build/target/product/lineage_generic_target.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \
