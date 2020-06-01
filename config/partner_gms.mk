2a621ba1aca014a44627878ab2ad76b52fc25943
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-01 08:50:58 -0400

    partner_gms: Support TV GMS
    Change-Id: I16c1f4a868c119b54da3e41c2e5b7e6bf7e0a53e

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,4 +1,7 @@
 ifeq ($(WITH_GMS),true)
+ifeq ($(PRODUCT_IS_ATV),true)
+$(call inherit-product-if-exists, vendor/partner_gms-tv/products/gms.mk)
+else
 ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
 else
@@ -6,6 +9,7 @@ ifeq ($(WITH_GMS_MINIMAL),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms_minimal.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+endif
 endif
 endif
 endif
