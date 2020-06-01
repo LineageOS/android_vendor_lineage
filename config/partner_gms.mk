f5efbd176d7d446f36d62d4fb8628ab2c359b082
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-01 08:50:58 -0400

    partner_gms: Support TV GMS
    Change-Id: I16c1f4a868c119b54da3e41c2e5b7e6bf7e0a53e

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,3 +1,21 @@
 ifeq ($(WITH_GMS),true)
+<<<<<<< HEAD   (d780ff roomservice: support new manifest formats.)
+=======
+ifeq ($(PRODUCT_IS_ATV),true)
+$(call inherit-product-if-exists, vendor/partner_gms-tv/products/gms.mk)
+else
+ifeq ($(WITH_GMS_FI),true)
+$(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
+else
+ifeq ($(WITH_GMS_MINIMAL),true)
+$(call inherit-product-if-exists, vendor/partner_gms/products/gms_minimal.mk)
+else
+>>>>>>> CHANGE (2a621b partner_gms: Support TV GMS)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 endif
+<<<<<<< HEAD   (d780ff roomservice: support new manifest formats.)
+=======
+endif
+endif
+endif
+>>>>>>> CHANGE (2a621b partner_gms: Support TV GMS)
