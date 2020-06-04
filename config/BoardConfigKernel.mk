2668f66d273003497be819cc13084502cfc2d124
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-06-04 19:34:26 +0200

    BoardConfigKernel: Opt-in for bison and flex
    For kernel >4.16 bison and flex are required, but there's a catch:
    not all of them have a very old dtc compiler and new ones do
    actually require a bison version >=3.0 or they will fail to build.
    
    For this reason, let people opt-in for bison and flex: if you
    set the env var TARGET_NEEDS_PREBUILT_FLEX_BISON to true, you will
    build the components with the prebuilt bison/flex, found in the
    Android tree; otherwise, the current system's (your distro's)
    bison/flex will be used instead.
    
    Test: Sony Open Devices kernel msm-4.14 now builds ok again.
    
    Change-Id: Id14b2482f4c633f529046b485c6ac16fcd28c45f

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -150,9 +150,11 @@ KERNEL_MAKE_FLAGS += HOSTCC=$(KERNEL_HOST_TOOLCHAIN_ROOT)gcc
 KERNEL_MAKE_FLAGS += HOSTCXX=$(KERNEL_HOST_TOOLCHAIN_ROOT)g++
 
 # Since Linux 4.16, flex and bison are required
+ifeq ($(TARGET_NEEDS_PREBUILT_FLEX_BISON), true)
 KERNEL_MAKE_FLAGS += LEX=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/flex
 KERNEL_MAKE_FLAGS += YACC=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/bison
 TOOLS_PATH_OVERRIDE += BISON_PKGDATADIR=$(BUILD_TOP)/prebuilts/build-tools/common/bison
+endif
 
 # Set the out dir for the kernel's O= arg
 # This needs to be an absolute path, so only set this if the standard out dir isn't used
