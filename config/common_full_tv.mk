a4c98304019dbd719577786ceb9996dc78bc6f98
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-07 00:47:58 -0400

    lineage: atv: Include device/lineage/atv
    Change-Id: I65e41efe2f729310bd1180a8c2e6cea357351c77

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,9 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
+# Inherit Lineage atv device tree
+$(call inherit-product, device/lineage/atv/lineage_atv.mk)
+
 # AOSP Apps
 PRODUCT_PACKAGES += \
     LeanbackIme
