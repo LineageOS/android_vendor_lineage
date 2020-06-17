5ad978c173c85dbd319100736c4995c4b4c5c4ca
Michael W <baddaemon87@gmail.com>
2020-06-17 17:37:09 +0200

    envsetup: dopush: Correct paths
    * In order to automatically restart the system when pushing SystemUI, the
      path must actually be correct
    * Since android 10 it's in system/*product*/priv-app and 11 will move
      it again, so just match it anywhere
    * Make framework also location independent
    
    Change-Id: I4a9eb1edbbb21ab8abe17815c606921f7a538b7f

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -883,7 +883,7 @@ EOF
                 fi
                 adb shell restorecon "$TARGET"
             ;;
-            /system/priv-app/SystemUI/SystemUI.apk|/system/framework/*)
+            */SystemUI.apk|*/framework/*)
                 # Only need to stop services once
                 if ! $stop_n_start; then
                     adb shell stop
