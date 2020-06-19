d37e04fb53a0c4555d98d19c24d1f54ec8eb1b45
Aayush Gupta <aayushgupta219@gmail.com>
2020-06-19 06:38:45 +0000

    DNM: lineage: Introduce extract-utlis.sh in python3
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Iad7f65388b0acca7973e8bec5ea4eeb142a60895

---
+++ build/templates/extract-files.py
@@ -0,0 +1,5 @@
+from extract_utils import AdvHelpers
+#
+# Path must be absolute in all functions
+AdvHelpers.setup_vendor("<device>", "<oem>", "<lineage_root_path>")
+AdvHelpers.extract_files("<path/to/proprietary-files.txt>")
