426a9d0e53d741060c50c0082aa4941c30d97026
Alexander Koskovich <zvnexus@outlook.com>
2020-06-28 22:24:53 -0400

    soong: Add TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS.
    Change-Id: Ibb4202eae237cc9f167707c45dd86a313d95d7be

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -21,6 +21,7 @@ $(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TAR
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
+$(call add_json_bool, Target_uses_prebuilt_dynamic_partitions,   $(filter true,$(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
 $(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
