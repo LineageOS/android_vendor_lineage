379258d7a83ca30f8fcaa0d925bbdf576b7963a8
Weikai Kong <pig.priv@gmail.com>
2020-08-24 15:52:00 +0200

    Revert "kernel: Set full path to DTC"
    This reverts commit 5061962d73e10ee751825ee7b2a880bf4186e904.
    
    Reason for revert: breaks build with upstreamed dtc >
    
    Change-Id: Idcbb7b95bd54bb6ecfe30f3d55ee90bd57708995

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -209,8 +209,6 @@ PATH_OVERRIDE += $(TOOLS_PATH_OVERRIDE)
 
 KERNEL_ADDITIONAL_CONFIG_OUT := $(KERNEL_OUT)/.additional_config
 
-KERNEL_MAKE_FLAGS += DTC=$(KERNEL_BUILD_OUT_PREFIX)$(DTC)
-
 # Internal implementation of make-kernel-target
 # $(1): output path (The value passed to O=)
 # $(2): target to build (eg. defconfig, modules, dtbo.img)
