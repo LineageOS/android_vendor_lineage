11539133f50a679e7d0e31ca643f4b907ec0232c
Jan Altensen <info@stricted.net>
2020-09-10 15:18:49 +0200

    soong: add TARGET_SPECIFIC_HEADER_PATH
    Change-Id: I582ae8e6e010016b33fd9b020b723e5fc2dc442b

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -86,6 +86,7 @@ type ProductVariables struct {
 	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_specific_header_path  *string `json:",omitempty"`
 	Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
