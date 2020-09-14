0985183a6c79fd48364c3350dee1957df5a4245d
LuK1337 <priv.luk@gmail.com>
2020-09-15 01:01:18 +0200

    lineage: Disable SDK addon for generic arm/arm64 targets
    * Android Studio SDK addon is currently broken for generic arm/arm64 targets
      and besides it only makes sense for x86 targets
    
    Change-Id: I6832cc99e0dcefb60c9fb187811821ab1b40e7e0

--- build/target/product/lineage_x86.mk
+++ build/target/product/lineage_x86.mk
@@ -19,3 +19,6 @@ include vendor/lineage/build/target/product/lineage_generic_target.mk
 TARGET_USES_64_BIT_BINDER := true
 
 PRODUCT_NAME := lineage_x86
+
+PRODUCT_SDK_ADDON_NAME := lineage
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
--- build/target/product/lineage_x86_64.mk
+++ build/target/product/lineage_x86_64.mk
@@ -17,3 +17,6 @@ $(call inherit-product, build/target/product/aosp_x86_64.mk)
 include vendor/lineage/build/target/product/lineage_generic_target.mk
 
 PRODUCT_NAME := lineage_x86_64
+
+PRODUCT_SDK_ADDON_NAME := lineage
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
