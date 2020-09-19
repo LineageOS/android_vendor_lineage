f00682d5bcfb49a81118bc58ee5c44397d8e68e1
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    config: Setup soong namespaces for TARGET_USE_QTI_BT_STACK
    To opt-in for QTI BT addons, enable TARGET_USE_QTI_BT_STACK in BoardConfig.mk
    
    Reference: [https://github.com/LineageOS/android_vendor_qcom_opensource_bluetooth-commonsys-intf/blob/lineage-18.0/bt-system-opensource-product.mk]
    
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -90,3 +90,7 @@ SOONG_CONFIG_lineageGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CA
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_lineageQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
 endif
+
+ifneq ($(TARGET_USE_QTI_BT_STACK),true)
+PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
+endif #TARGET_USE_QTI_BT_STACK
