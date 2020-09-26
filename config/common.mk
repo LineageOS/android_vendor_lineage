52c183c40d6ff28789eb6d98225a9a4b32cfc6cd
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-09-26 19:22:08 +0200

    Use component-override to configure GMS/Vending
    Change-Id: I0a2528fccb54befaed8f598d79cf1b195cb34838

--- config/common.mk
+++ config/common.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lineage-sysconfig.xml
 
+# Lineage-specific component overrides
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/component-overrides.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+
 # Copy all Lineage-specific init rc files
 $(foreach f,$(wildcard vendor/lineage/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
