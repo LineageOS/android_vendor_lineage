9b1bd8aa4270d72ac926b360cbe96158a513de69
LuK1337 <priv.luk@gmail.com>
2020-10-13 10:27:07 +0200

    soong: Add TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD
    Change-Id: I332dcd289d9617938738d1959daee106439de5c6

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -67,6 +67,9 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
+	Uses_pre_uplink_features_netmgrd struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -91,4 +94,5 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Uses_pre_uplink_features_netmgrd  *bool `json:",omitempty"`
 }
