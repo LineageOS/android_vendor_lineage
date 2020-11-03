7a31b36bb0dcdebb7296d186b2b914f8c542b70d
Danny Lin <danny@kdrag0n.dev>
2020-11-03 13:04:47 -0800

    config: Build QuickAccessWallet plugin for SystemUI
    This plugin that provides the wallet (cards and passes) integration in
    the power menu is now open-source, so let's build it.
    
    Change-Id: I5df8a09c8f91805df538bc7ddad6d326f5d9b13e

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -68,3 +68,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
+
+# SystemUI plugins
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
