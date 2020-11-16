cf95a7356b43779ad86cf382101d01c4d5acaa5a
Michael Benedict <michaelbt@live.com>
2020-11-16 23:03:09 +0700

    soong: Add TARGET_FOD_ALWAYS_ON_HBM
    Signed-off-by: Michael Benedict <michaelbt@live.com>
    Change-Id: Iee9d42017211ddf93abd5f0335502ab54ee275c1

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,6 +33,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
+    target_fod_always_on_hbm \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
@@ -63,6 +64,7 @@ endif
 SOONG_CONFIG_lineageGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_lineageGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_lineageGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
+SOONG_CONFIG_lineageGlobalVars_target_fod_always_on_hbm := $(TARGET_FOD_ALWAYS_ON_HBM)
 SOONG_CONFIG_lineageNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_lineageQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_lineageQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
