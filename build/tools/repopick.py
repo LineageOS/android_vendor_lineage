cae3647d49d0046344b7aabdb3b0847b8dc3537d
LuK1337 <priv.luk@gmail.com>
2020-11-23 10:55:24 +0100

    repopick: Use project revision if available
    * Fixes repopicking from repos synced under multiple paths + branches.
    
    Change-Id: Icb165f3d9df67f1b11304e8d7ed955464dcb57bc

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -259,7 +259,7 @@ if __name__ == '__main__':
                 if remote.get('name') == project.get('remote'):
                     revision = remote.get('revision')
             if revision is None:
-                revision = default_revision
+                revision = project.get('revision', default_revision)
 
         if not name in project_name_to_data:
             project_name_to_data[name] = {}
