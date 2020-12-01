73c49dea28de78efaaeae19d3355f532c242dcaa
Bruno Martins <bgcngm@gmail.com>
2020-12-01 21:44:06 +0000

    partner_gms: Add support for Android Go GMS
    Change-Id: I5d98283606ca213327b72969a2a3e3d256b30d05

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -5,10 +5,18 @@ else
 ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
 else
+ifeq ($(WITH_GMS_GO),true)
+$(call inherit-product-if-exists, vendor/partner_gms/products/gms_go.mk)
+else
+ifeq ($(WITH_GMS_GO_2GB),true)
+$(call inherit-product-if-exists, vendor/partner_gms/products/gms_go_2gb.mk)
+else
 ifeq ($(WITH_GMS_MINIMAL),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms_minimal.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+endif
+endif
 endif
 endif
 endif
