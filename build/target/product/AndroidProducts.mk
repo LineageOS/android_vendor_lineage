4a33cebdc4b50539d236570eada5c7f188518d4f
Nolen Johnson <johnsonnolen@gmail.com>
2020-12-08 16:31:31 -0500

    lineage: targets: Add support for ATV GSI builds
    Change-Id: I1faf83e21af5db1b29b98af72a4f74666c05f775

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -18,10 +18,18 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_arm.mk \
     $(LOCAL_DIR)/lineage_arm64.mk \
     $(LOCAL_DIR)/lineage_x86.mk \
-    $(LOCAL_DIR)/lineage_x86_64.mk
+    $(LOCAL_DIR)/lineage_x86_64.mk \
+    $(LOCAL_DIR)/lineage_tv_arm.mk \
+    $(LOCAL_DIR)/lineage_tv_arm64.mk \
+    $(LOCAL_DIR)/lineage_tv_x86.mk \
+    $(LOCAL_DIR)/lineage_tv_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_arm-userdebug \
     lineage_arm64-userdebug \
     lineage_x86-userdebug \
-    lineage_x86_64-userdebug
+    lineage_x86_64-userdebug \
+    lineage_tv_arm-userdebug \
+    lineage_tv_arm64-userdebug \
+    lineage_tv_x86-userdebug \
+    lineage_tv_x86_64-userdebug
