876fc96e43c1f4199a47cf24ba8ff55767db9799
Tianjie <xunchang@google.com>
2020-12-10 17:20:46 -0800

    Include the Pixel common board configuration for qcom
    Bug: 156162446
    Test: treehugger
    Change-Id: Iec908f4cef5d28c2f621e6d8bc845b9c0cb26761

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,6 +1,7 @@
 include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include build/make/target/board/BoardConfigPixelCommon.mk
 include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
