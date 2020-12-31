403c59b0141d549a50c71e1376c88ecde5e674d0
Ethan Chen <intervigil@gmail.com>
2020-12-31 11:51:28 -0800

    partner_gms: Simplify if/else if logic
    Change-Id: Ib5e3dc3741c6784cb47bd0f66150df8a499aaf92

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,23 +1,15 @@
 ifeq ($(WITH_GMS),true)
 ifeq ($(PRODUCT_IS_ATV),true)
 $(call inherit-product-if-exists, vendor/partner_gms-tv/products/gms.mk)
-else
-ifeq ($(WITH_GMS_FI),true)
+else ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
-else
-ifeq ($(WITH_GMS_GO),true)
+else ifeq ($(WITH_GMS_GO),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms_go.mk)
-else
-ifeq ($(WITH_GMS_GO_2GB),true)
+else ifeq ($(WITH_GMS_GO_2GB),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms_go_2gb.mk)
-else
-ifeq ($(WITH_GMS_MINIMAL),true)
+else ifeq ($(WITH_GMS_MINIMAL),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms_minimal.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
-endif
-endif
-endif
-endif
 endif
 endif
