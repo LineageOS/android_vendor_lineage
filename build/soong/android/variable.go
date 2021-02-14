b1f0cc544129ea1ce65412b6075273bfdf28e6d0
Jan Altensen <info@stricted.net>
2021-02-14 21:39:31 +0100

    soong_config: add TARGET_OMX_LEGACY_RESCALING
    Change-Id: I375f1dea02c76394ce11dcb3e0e9b6ac85a6a3a4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -44,6 +44,9 @@ type Product_variables struct {
 	Target_needs_netd_direct_connect_rule struct {
 		Cppflags []string
 	}
+	Target_omx_legacy_rescaling struct {
+		Cppflags []string
+	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
@@ -84,6 +87,7 @@ type ProductVariables struct {
 	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
+	Target_omx_legacy_rescaling  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
