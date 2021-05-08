11c43d2e930b4edadf186d8b7abab97008946f58
Tim Zimmermann <tim@linux4.de>
2021-05-08 16:17:18 +0200

    backuptool: use correct BLK_PATH on a-only dynamic partitions devices
    Change-Id: I295d39e35245e3541648369acbfe79d88ac21865

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -137,7 +137,11 @@ find_block() {
 determine_system_mount
 
 DYNAMIC_PARTITIONS=$(getprop ro.boot.dynamic_partitions)
-BLK_PATH=$(dirname "$SYSDEV")
+if [ "$DYNAMIC_PARTITIONS" = "true" ]; then
+    BLK_PATH="/dev/block/mapper"
+else
+    BLK_PATH=$(dirname "$SYSDEV")
+fi
 
 mount_extra() {
   for partition in $@; do
