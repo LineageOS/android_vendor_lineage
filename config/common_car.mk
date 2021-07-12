326cb58f293aa4fcdb159a2ff9c191a8057a2643
Tom Pratt <tom.pratt@outlook.com>
2021-07-12 12:34:22 +0200

    Add generic targets for automotive
    Makefiles combining core lineage and automotive packages.
    Can be included by devices to turn them into Automotive builds.
    Emulator targets demonstrate using these makefiles and form a basis for Automotive GSIs.
    Change-Id: Ib5d35dfe4de8c459e4e14a7ba6f0b0e682e0d912

---
+++ config/common_car.mk
@@ -0,0 +1,5 @@
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Inherit the main AOSP car makefile that turns this into an Automotive build
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
---
+++ config/common_full_car.mk
@@ -0,0 +1,4 @@
+# This config is for legacy purposes, there are no car product size variants
+
+# Inherit common car Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_car.mk)
