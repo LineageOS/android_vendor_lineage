b7d29a7a2c91dd430d645980c3e0c822e422c04d
Nolen Johnson <johnsonnolen@gmail.com>
2021-07-12 17:12:37 -0400

    lineage: partner_gms: Correctly guard ATV GMS
    * Needs to be an env-var, or when Android.mk is read in partner_gms
      we runinto issues with the var not being set yet.
    
    Change-Id: Ia90f9a6c97ebf406a0f3c1848286300cace2c2c9

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,5 +1,5 @@
 ifeq ($(WITH_GMS),true)
-ifeq ($(PRODUCT_IS_ATV),true)
+ifeq ($(WITH_GMS_TV),true)
 $(call inherit-product-if-exists, vendor/partner_gms-tv/products/gms.mk)
 else ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/fi.mk)
