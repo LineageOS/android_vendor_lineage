06f792fd7f3097abd97f450e781429b69fe6f4a6
programminghoch10 <hoch10@kabelbw.de>
2021-08-04 02:09:40 +0200

    charger: add lineage charger
    Add a new charger as a possible replacement for the AOSP charger.
    
    Co-Authored-By: Tim Zimmermann <tim@linux4.de>
    Change-Id: Icdf7ad770bb353f082759b76fd4e58910e8d72e3

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -58,6 +58,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+ifneq ($(WITH_LINEAGE_CHARGER),false)
+PRODUCT_PACKAGES += \
+    lineage_charger_animation
+endif
+
 # Customizations
 PRODUCT_PACKAGES += \
     IconShapeSquareOverlay \
