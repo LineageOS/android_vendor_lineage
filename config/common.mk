1e5988c988ca85768f8e9d9bb07bb4df97395e55
Alexander Koskovich <zvnexus@outlook.com>
2021-08-07 23:36:47 -0700

    lineage: overlay: Exclude backup transport overlays from RRO.
     * These need to reference Settings resources that they
       don't have access to.
    
    Change-Id: I6e055b141f71f33848cf006679ae65dff43f9d03

--- config/common.mk
+++ config/common.mk
@@ -186,7 +186,10 @@ endif
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/lineage/overlay/common \
+    vendor/lineage/overlay/no-rro
 
 PRODUCT_VERSION_MAJOR = 18
 PRODUCT_VERSION_MINOR = 1
