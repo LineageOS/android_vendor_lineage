aca64b083a0d0d09186845dce9e6067c63092bc3
Arian <arian.kulmer@web.de>
2021-10-05 22:28:51 +0200

    config: Update clang version to clang-r416183b1
    * That's the default clang version on android-12.0.0_r1
    
    Change-Id: I528d9a6aa497f7c9a0f53b0e778d0eeefaac0e9d

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -49,7 +49,7 @@ else
 KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 endif
 
-CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r383902b
+CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r416183b1
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
 # arm64 toolchain
 KERNEL_TOOLCHAIN_arm64 := $(GCC_PREBUILTS)/aarch64/aarch64-linux-android-4.9/bin
