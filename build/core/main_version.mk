a14f9671f1287317105459fcac95ba50365c2c1e
Michael Bestas <mkbestas@lineageos.org>
2021-10-05 23:37:55 +0300

    build: Replace obsolete ADDITIONAL_BUILD_PROPERTIES
    Fixes:
    error: ADDITIONAL_BUILD_PROPERTIES is obsolete.  Please use ADDITIONAL_SYSTEM_PROPERTIES.
    Change-Id: I7eb3c47d9fd705aa8f8d39e10563e47ffbf2d560

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,11 +1,11 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 # LineageOS System Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
     ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
@@ -13,13 +13,13 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.lineagelegal.url=https://lineageos.org/legal
 
 # LineageOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
 
 # LineageOS Platform SDK Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
 
 # LineageOS Platform Internal Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
