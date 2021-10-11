00b0d31995902ba0d75ef1f5cd864aa99b57d291
Michael Bestas <mkbestas@lineageos.org>
2021-10-11 18:31:06 +0300

    Remove color font and icon overlays
    These are not used anymore with the new theming
    
    Change-Id: Iacb086f9dacc36fca5c9a9dedcbd2b2ba4f86e6e

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,16 +3,6 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    LineageLatoFont \
-    LineageRubikFont
-
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -38,11 +38,3 @@ LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
 LOCAL_PRODUCT_MODULE := true
 
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := fonts_customization.xml
-LOCAL_SRC_FILES := etc/fonts_customization.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_PRODUCT_MODULE := true
-include $(BUILD_PREBUILT)
