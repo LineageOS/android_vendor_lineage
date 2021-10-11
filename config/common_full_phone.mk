127800a6b36fffd1107decf492d4fe4e55bbe21f
Arian <arian.kulmer@web.de>
2021-10-11 17:15:40 +0200

    config: Enable support of one-handed mode for phone targets
    Change-Id: Iab7de339e449d396d19783cfc254d540b600a33a

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,4 +8,8 @@ PRODUCT_PACKAGES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 
+# Enable support of one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
 $(call inherit-product, vendor/lineage/config/telephony.mk)
