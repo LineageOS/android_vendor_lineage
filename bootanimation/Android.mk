dae30acd8fc386bbc4890c1c28c53f0593e52ddf
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-10 20:52:20 +0530

    Move bootanimation to product
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I9972653845c64aab944d0289d9357361e2feb344

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -49,9 +49,18 @@ endif
 include $(CLEAR_VARS)
 LOCAL_MODULE := bootanimation.zip
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/media
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/media
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
 $(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
 	@cp $(TARGET_BOOTANIMATION) $@
+
+include $(CLEAR_VARS)
+
+BOOTANIMATION_SYMLINK := $(TARGET_OUT_PRODUCT)/media/bootanimation-dark.zip
+$(BOOTANIMATION_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf bootanimation.zip $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(BOOTANIMATION_SYMLINK)
