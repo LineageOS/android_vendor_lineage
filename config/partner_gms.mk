05ca8b36d740f730f89d585c73770104930f535b
Alexander Koskovich <akoskovich@pm.me>
2022-01-25 07:17:20 -0700

    lineage: Pick up mainline modules as well.
    Change-Id: I1877329d7a1fc9697a63c08af0466daf155f8cba

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,15 +1,20 @@
 ifeq ($(WITH_GMS),true)
 ifeq ($(WITH_GMS_TV),true)
 $(call inherit-product, vendor/partner_gms-tv/products/gms.mk)
+$(call inherit-product, vendor/partner_gms-tv/products/mainline_modules.mk)
 else ifeq ($(WITH_GMS_FI),true)
 $(call inherit-product, vendor/partner_gms/products/fi.mk)
+$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
 else ifeq ($(WITH_GMS_GO),true)
 $(call inherit-product, vendor/partner_gms/products/gms_go.mk)
+$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_s_low_ram.mk)
 else ifeq ($(WITH_GMS_GO_2GB),true)
 $(call inherit-product, vendor/partner_gms/products/gms_go_2gb.mk)
+$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_s_low_ram.mk)
 else ifeq ($(WITH_GMS_MINIMAL),true)
 $(call inherit-product, vendor/partner_gms/products/gms_minimal.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
 endif
 endif
