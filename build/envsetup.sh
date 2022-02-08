79bf73a14b4bd4e65da204e3e3d73efa26ad2a1f
Bruno Martins <bgcngm@gmail.com>
2022-02-08 19:35:40 +0000

    envsetup: Fix long time broken path to sh
    Change-Id: Ia76d14e10571dd6f3ac9f4adf288effae851ffe3

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -837,7 +837,7 @@ function dopush()
         CHKPERM="/data/local/tmp/chkfileperm.sh"
 (
 cat <<'EOF'
-#!/system/xbin/sh
+#!/system/bin/sh
 FILE=$@
 if [ -e $FILE ]; then
     ls -l $FILE | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf("%0o ",k);print}' | cut -d ' ' -f1
