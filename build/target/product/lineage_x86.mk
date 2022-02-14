d8bdaefb4f13de808f574c3ff52df2d8883b7575
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-14 15:57:50 -0500

    lineage: products: lineage_x86{,_64}: Don't include SDK addon
    * This doesn't work, and just errors out similar to arm/arm64.
    
    Change-Id: I53a2261095f1e2acab3b7da20d3f092ccc83e8ab

--- build/target/product/lineage_x86.mk
+++ build/target/product/lineage_x86.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,6 +23,3 @@ TARGET_USES_64_BIT_BINDER := true
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 
 PRODUCT_NAME := lineage_x86
-
-PRODUCT_SDK_ADDON_NAME := lineage
-PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
--- build/target/product/lineage_x86_64.mk
+++ build/target/product/lineage_x86_64.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +21,3 @@ include vendor/lineage/build/target/product/lineage_generic_target.mk
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 
 PRODUCT_NAME := lineage_x86_64
-
-PRODUCT_SDK_ADDON_NAME := lineage
-PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
