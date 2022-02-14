821b3eb34dcb6c5b9ad646786bcf03360dbc832a
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-14 17:38:30 -0500

    lineage: products: Create new Automotive GSI targets
    * And split out emulator includes as needed to do so.
    
    Change-Id: I9a9e02dc9ebf4613bf2da6be9985a8565dac93fc

--- build/target/product/lineage_gsi_car_x86_64.mk
+++ build/target/product/lineage_gsi_car_x86_64.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2020 The LineageOS Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/lineage/config/common_car.mk)
+$(call inherit-product, device/generic/car/gsi_car_arm64.mk)
 
-EMULATOR_VENDOR_NO_SENSORS := true
-EMULATOR_VENDOR_NO_SOUND := true
+include vendor/lineage/build/target/product/lineage_generic_car_target.mk
 
-# Allow building otatools
-TARGET_FORCE_OTA_PACKAGE := true
+TARGET_NO_KERNEL_OVERRIDE := true
+
+# Enable mainline checking
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+
+PRODUCT_NAME := lineage_gsi_car_x86_64
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/app/Home/Home.apk
