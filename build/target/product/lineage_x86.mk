95fcf2fc2255686b7fc3d59d85a64e760641df85
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-14 15:59:04 -0500

    lineage: products: lineage_x86{,_64}: Don't build kernel
    * Results in an error.
    
    Change-Id: Ifab94ca34bab75d8e55b317f69060fc02f068ecf

--- build/target/product/lineage_x86.mk
+++ build/target/product/lineage_x86.mk
@@ -18,6 +18,7 @@ $(call inherit-product, build/target/product/gsi_release.mk)
 include vendor/lineage/build/target/product/lineage_generic_target.mk
 
 TARGET_USES_64_BIT_BINDER := true
+TARGET_NO_KERNEL_OVERRIDE := true
 
 # Enable mainline checking
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
