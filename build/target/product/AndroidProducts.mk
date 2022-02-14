767af67c7fd75eb40f2adbc9e3d9d13bfea06bba
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-14 17:38:30 -0500

    lineage: products: Create new Automotive GSI targets
    Change-Id: I9a9e02dc9ebf4613bf2da6be9985a8565dac93fc

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -19,6 +19,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_arm64.mk \
     $(LOCAL_DIR)/lineage_x86.mk \
     $(LOCAL_DIR)/lineage_x86_64.mk \
+    $(LOCAL_DIR)/lineage_car_arm64.mk \
+    $(LOCAL_DIR)/lineage_car_x86_64.mk \
     $(LOCAL_DIR)/lineage_tv_arm.mk \
     $(LOCAL_DIR)/lineage_tv_arm64.mk \
     $(LOCAL_DIR)/lineage_tv_x86.mk \
@@ -35,6 +37,8 @@ COMMON_LUNCH_CHOICES := \
     lineage_arm64-userdebug \
     lineage_x86-userdebug \
     lineage_x86_64-userdebug \
+    lineage_car_arm64-userdebug \
+    lineage_car_x86_64-userdebug \
     lineage_tv_arm-userdebug \
     lineage_tv_arm64-userdebug \
     lineage_tv_x86-userdebug \
---
+++ build/target/product/lineage_car_arm64.mk
@@ -0,0 +1,28 @@
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/generic/car/gsi_car_arm64.mk)
+$(call inherit-product, build/target/product/gsi_release.mk)
+
+include vendor/lineage/build/target/product/lineage_generic_car_target.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+
+# Enable mainline checking
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+
+PRODUCT_NAME := lineage_car_arm64
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/app/Home/Home.apk
