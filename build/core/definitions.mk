fcb97da38cc897f8087e3a6376be9ae42340f46f
Alexander Koskovich <akoskovich@pm.me>
2022-02-20 11:19:35 -0700

    lineage: Refactor QCOM board detection.
     * Remove MTK board platforms, nothing uses this when
       looking for 'MTK_BOARD_PLATFORMS' across the entire
       organization. This lets us revert the utilities back
       to how they were in the QCOM tree.
    
     * Move board macros and platform inclusions to before
       we pick up BoardConfigQcom so it's early enough so
       we can use 'is-board-platform-in-list' to determine
       if the device is QCOM or not. Exclude Pixels from
       this entierly, they're off doing their own thing.
       This way we can get rid of 'BOARD_USES_QCOM_HARDWARE'
       and just determine board like how QCOM intends.
    
     * In QCOM boards remove all boards from variable names
       except '$(MSMSTEPPE)' and '$(TRINKET)', these are
       the only two boards I found that QCOM refers to them
       by their variable names in makefiles.
    
     * Instead of adding each board platform to QCOM_BOARD_PLATFORMS
       manually, define all the UM families in qcom_boards.mk
       then add all the QCOM families to QCOM_BOARD_PLATFORMS.
       We are also moving all board platform definitions out
       of BoardConfigQcom by doing this.
    
     * Drop QSD8K_BOARD_PLATFORMS, was last mentioned in
       android_vendor_qcom_opensource_kernel-tests_mm-audio
       during CM 9.
    
    Change-Id: I11b51d7aacd641f616b716dbfd40a3805cd1ad7c

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -4,9 +4,3 @@
 define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
-
-# Include board/platform macros
-include vendor/lineage/build/core/utils.mk
-
-# Include vendor platform definitions
-include vendor/lineage/build/core/vendor/*.mk
