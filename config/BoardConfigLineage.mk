afaef4302ecbf96441713aa09ad52b40e61b156b
Alexander Koskovich <akoskovich@pm.me>
2022-02-20 11:19:35 -0700

    lineage: General cleanup of QCOM board stuff
     * In QCOM boards remove all boards from variable names
       except '$(MSMSTEPPE)' and '$(TRINKET)', these are
       the only two boards I found that QCOM refers to them
       by their variable names in makefiles.
    
     * Instead of adding each board platform to QCOM_BOARD_PLATFORMS
       manually, define all the UM families in qcom_boards.mk
       then add all the QCOM families to QCOM_BOARD_PLATFORMS.
       We are also moving all board platform definitions out
       of BoardConfigQcom by doing this.
    
     * Include qcom_boards before we pick up the QCOM BoardConfig
       as it has a lot of stuff that depends off the results
       of it. Before we were just defining stuff twice.
    
     Please enter the commit message for your changes. Lines starting
    
    Change-Id: Ic98f4489199d2239a8eaf82f6280014b10938dba

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,6 +1,9 @@
 include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+# Include QCOM board platforms before the QCOM BoardConfig
+# so we have SoCs listed for respective UM families.
+include vendor/lineage/build/core/vendor/qcom_boards.mk
 include vendor/lineage/config/BoardConfigQcom.mk
 endif
 
