0ccc3dc90d23359e81c331615f04e0cead75b8ee
Alexander Koskovich <akoskovich@pm.me>
2022-02-20 11:19:35 -0700

    lineage: Refactor QCOM board detection.
     * Move board macros and platform inclusions to before
       we pick up BoardConfigQcom so it's early enough so
       we can use 'is-board-platform-in-list' to determine
       if the device is QCOM or not. Exclude Pixels from
       this entierly, they're off doing their own thing.
       This way we can get rid of 'BOARD_USES_QCOM_HARDWARE'
       and just determine board like how QCOM intends.
    
     * In QCOM boards remove all boards from variable names
       except '$(MSMSTEPPE)' and '$(TRINKET)', these are
       the only two boards I found that QCOM refers to them
       by their variable names in makefiles.
    
     * Instead of adding each board platform to QCOM_BOARD_PLATFORMS
       manually, define all the UM families in qcom_boards.mk
       then add all the QCOM families to QCOM_BOARD_PLATFORMS.
       We are also moving all board platform definitions out
       of BoardConfigQcom by doing this.
    
    Change-Id: I11b51d7aacd641f616b716dbfd40a3805cd1ad7c

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,7 +1,14 @@
 include vendor/lineage/config/BoardConfigKernel.mk
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+# Include vendor board platforms and utilities early
+# enough so BoardConfigQcom can read from them.
+include vendor/lineage/build/core/utils.mk
+include vendor/lineage/build/core/vendor/qcom_boards.mk
+
+ifneq ($(PRODUCT_MANUFACTURER), Google)
+ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include vendor/lineage/config/BoardConfigQcom.mk
+endif
 endif
 
 include vendor/lineage/config/BoardConfigSoong.mk
