1fc0feb518062bd5052a5cd44073a61e1ecff552
Tom Pratt <tom.pratt@outlook.com>
2022-03-03 10:54:03 +0100

    Fix GSI targets
    Inherit from AOSP gsi_{arch}.mk. aosp_{arch}.mk files
    are obsolete in Android 12. Rename for constency with
    aosp gsi/sdk targets.
    
    Change-Id: I449390521307ff4f269a9ebf1ad3a5ffc398bc97

--- build/target/product/lineage_gsi_arm.mk
+++ build/target/product/lineage_gsi_arm.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_x86.mk)
-$(call inherit-product, build/target/product/gsi_release.mk)
+$(call inherit-product, device/generic/common/gsi_arm.mk)
 
 include vendor/lineage/build/target/product/lineage_generic_target.mk
 
-TARGET_USES_64_BIT_BINDER := true
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+
 TARGET_NO_KERNEL_OVERRIDE := true
 
-# Enable mainline checking
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
-
-PRODUCT_NAME := lineage_x86
+PRODUCT_NAME := lineage_gsi_arm
