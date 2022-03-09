ea731e7cba7b369713fc644756e9677c3f80c1c4
Alexander Koskovich <akoskovich@pm.me>
2022-03-09 00:56:37 -0700

    Revert "lineage: Dynamically add custom APNs"
     * This was so devices could override Sprint APNs, which was
       acquired by T-Mobile as of April 1st 2020.
    
     * Doing this allows us to override AOSP APNs via inclusion
       order instead of editing AOSP repositories.
    
    This reverts commit 0d2fa55f2f88e73018a749a3fe34f33ec365fbbc.
    
    Change-Id: Ia9ee0d733418909633a1010efe78bc1b5abf9328

--- config/data_only.mk
+++ config/data_only.mk
@@ -1,6 +1,6 @@
 # World APN list
-PRODUCT_PACKAGES += \
-    apns-conf.xml
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
--- config/telephony.mk
+++ config/telephony.mk
@@ -3,8 +3,8 @@ PRODUCT_PACKAGES += \
     sensitive_pn.xml
 
 # World APN list
-PRODUCT_PACKAGES += \
-    apns-conf.xml
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
