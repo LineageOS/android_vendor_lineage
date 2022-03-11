7a1d4ee381a59ca618774f450c26ebd58f1ef7d6
Bruno Martins <bgcngm@gmail.com>
2022-03-11 12:10:45 +0000

    config: Create makefiles for tablets with telephony support
    So far, Tablets with telephony have been inheriting from
    common_full_phone.mk, but that's not really proper because
    such makefile enables one-handed mode support
    (setting prop ro.support_one_handed_mode to true).
    
    Fix this by creating a new makefile that can be used also
    to include more tablet-specific configs moving forward.
    
    Change-Id: I90c22badb17911ef5e873299d986204718300e6c

---
+++ config/common_mini_tablet.mk
@@ -0,0 +1,8 @@
+# Inherit mini common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mini.mk)
+
+# Required packages
+PRODUCT_PACKAGES += \
+    LatinIME
+
+$(call inherit-product, vendor/lineage/config/telephony.mk)
