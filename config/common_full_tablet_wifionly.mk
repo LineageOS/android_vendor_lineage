cb1fb23513f8287fd0c68ac26ae3886d4321423e
Tim Zimmermann <tim@linux4.de>
2022-03-11 12:24:17 +0000

    config: Include androidx.window.extensions in tablet builds
    This enables 2-pane layout in Settings for Android 12L.
    
    Change-Id: I57af5b876c2c33897bf4047a70c10750785a3c76

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -3,6 +3,7 @@ $(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
+    androidx.window.extensions \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
