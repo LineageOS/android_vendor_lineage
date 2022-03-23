734e28d98da31150cc3ea14af0552038bc4a9db7
LuK1337 <priv.luk@gmail.com>
2022-03-23 20:39:30 +0100

    soong: Add TARGET_TRUST_CONTROL_PATH
    Change-Id: Ib245114588fd14c83069e4365f9ca3e9e9536ba9

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -40,6 +40,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
+    target_trust_control_path \
     uses_camera_parameter_lib
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
@@ -86,6 +87,7 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY ?= libcamera_parameters
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
+TARGET_TRUST_CONTROL_PATH ?= /proc/sys/kernel/deny_new_usb
 
 # Soong value variables
 SOONG_CONFIG_lineageGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
@@ -94,6 +96,7 @@ SOONG_CONFIG_lineageGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LI
 SOONG_CONFIG_lineageGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_lineageGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_lineageGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
+SOONG_CONFIG_lineageGlobalVars_target_trust_control_path := $(TARGET_TRUST_CONTROL_PATH)
 SOONG_CONFIG_lineageGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_lineageQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
