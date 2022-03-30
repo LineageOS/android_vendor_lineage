4920c9b2b9b3e53002911aaf13c1e9e133718772
Tom Pratt <tom.pratt@outlook.com>
2022-03-30 14:22:38 +0200

    Disable mobiledata by default with sys prop
    Replaces old SUW logic with AOSP's built in mechanism.
    
    Change-Id: Ia210842f97b2da9bc7ed49044837df836a7a884e

--- config/telephony.mk
+++ config/telephony.mk
@@ -19,3 +19,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # (for devices that check this)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     net.tethering.noprovisioning=true
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.com.android.mobiledata=false
