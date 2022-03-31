363eeb5517102680e3c8e053839c56a905482c1b
Nolen Johnson <johnsonnolen@gmail.com>
2022-03-31 09:15:18 -0400

    partner_gms: Support makefile variants for GMS variants
    * TV needs minimal as well.
    
    Change-Id: I8ef44f9c92974cf97f93bbb34026c3812ca9ff51

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,18 +1,31 @@
 ifeq ($(WITH_GMS),true)
     # Special handling for Android TV
     ifeq ($(WITH_GMS_TV),true)
-        $(call inherit-product, vendor/partner_gms-tv/products/gms.mk)
+        ifneq ($(GMS_MAKEFILE),)
+            # Specify the GMS makefile you want to use, for example:
+            #   - gms.mk            - default Android TV GMS
+            #   - gms_gtv.mk        - default Google TV GMS
+            #   - gms_minimal.mk    - minimal Android TV GMS
+            $(call inherit-product, vendor/partner_gms-tv/products/$(GMS_MAKEFILE))
+        else
+            $(call inherit-product, vendor/partner_gms-tv/products/gms.mk)
+        endif
         $(call inherit-product, vendor/partner_gms-tv/products/mainline_modules.mk)
     # Special handling for Android Automotive
     else ifeq ($(WITH_GMS_CAR),true)
-        $(call inherit-product, vendor/partner_gms-car/products/gms.mk)
-    else
+        ifneq ($(GMS_MAKEFILE),)
+            $(call inherit-product, vendor/partner_gms-car/products/$(GMS_MAKEFILE))
+        else
+            $(call inherit-product, vendor/partner_gms-car/products/gms.mk)
+        endif
+   else
         # Specify the GMS makefile you want to use, for example:
         #   - fi.mk             - Project Fi
         #   - gms.mk            - default GMS
         #   - gms_go.mk         - low ram devices
         #   - gms_go_2gb.mk     - low ram devices (2GB)
         #   - gms_64bit_only.mk - devices supporting 64-bit only
+        #   - gms_minimal.mk    - minimal GMS
         ifneq ($(GMS_MAKEFILE),)
             $(call inherit-product, vendor/partner_gms/products/$(GMS_MAKEFILE))
         else
