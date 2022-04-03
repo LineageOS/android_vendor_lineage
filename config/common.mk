7bd3d1609efa000e2d991dd47b1c37f5b9d3bc90
Michael Bestas <mkbestas@lineageos.org>
2022-04-04 01:30:19 +0300

    config: Include vendor/crowdin overlays
    Include translations for languages not available in AOSP.
    
    This used to be split to each repository but it required forking AOSP
    repositories just to add translations. Including them via overlay
    avoids forking and makes the translations more portable, they can even
    be used on pure AOSP builds.
    
    Make them built in to avoid RRO issues.
    
    Change-Id: Icc4c7b3a583c3d8a139a4a9a856dfca57952e131

--- config/common.mk
+++ config/common.mk
@@ -272,6 +272,10 @@ else
     endif
 endif
 
+# Translations
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
+
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
 
