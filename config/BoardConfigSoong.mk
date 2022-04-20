ad0eb9ebcc9790aa4433ca0da1fdede776c9dae8
Edwin Moquete <edwinmmoquete@gmail.com>
2022-04-20 02:36:25 +0000

    config: Add pixel repos to soong namespaces
    Needed for the common libperfmgr power HAL
    
    Change-Id: I55b11432cda5b453979cfcfd129ca7cc922a6a4a

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -116,3 +116,7 @@ endif
 ifneq ($(TARGET_USE_QTI_BT_STACK),true)
 PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
 endif #TARGET_USE_QTI_BT_STACK
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
