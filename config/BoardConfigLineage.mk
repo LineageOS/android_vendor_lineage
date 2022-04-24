241fecfa6a5c6aaa17b9ad237d572efc23f58e76
Jan Altensen <info@stricted.net>
2022-04-24 19:12:59 +0200

    config: add BoardConfigExynos
    Change-Id: Ie84bcca06685d684d2ff02cc533b7659ee5413a9

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -2,6 +2,8 @@ include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/lineage/config/BoardConfigQcom.mk
+else ifneq (,$(filter exynos%, $(TARGET_SOC)))
+include vendor/lineage/config/BoardConfigExynos.mk
 endif
 
 include vendor/lineage/config/BoardConfigSoong.mk
