bb993175c10f72c51c13340e9f0b4f8cd4c1013c
LuK1337 <priv.luk@gmail.com>
2022-04-29 12:37:39 +0200

    Revert "config: Drop 2-button navigation support on tablets"
    This reverts commit 755d713ebe7b89b4db055b317a257fa280b20d43.
    
    Change-Id: I6f676df2b9b7f18d2461ddb05fa141a8c49cc5f8

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,8 +3,7 @@ $(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    LatinIME \
-    NavigationBarMode2ButtonOverlay
+    LatinIME
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
