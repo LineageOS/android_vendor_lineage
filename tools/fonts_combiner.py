8fe770911f2d34bd131cdaa3caed13df07d77711
althafvly <althafvly@gmail.com>
2022-04-30 16:02:32 +0530

    tools: Dynamically add additional fonts
    - This tool used to combined two fonts_customization xmls
    
    eg: python3 fonts_combiner.py font1.xml font2.xml combined_font.xml
    Change-Id: I22b4685b7c8db011d1b559892e1276a82bf8dfbd

---
+++ tools/fonts_combiner.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import sys
+import os
+from xml.etree import ElementTree
+import xml.dom.minidom as md
+
+
+def combine(files):
+    if not len(files) == 2:
+        raise ValueError("Wrong number of arguments %s" % len(files))
+
+    first = None
+    for filename in files:
+        data = ElementTree.parse(filename).getroot()
+        if first is None:
+            first = data
+        else:
+            first.extend(data)
+    if first is not None:
+        return (ElementTree.tostring(first)).decode()
+
+def pretty(xml):
+    dom = md.parse(sys.argv[3])
+    pretty_xml = dom.toprettyxml()
+    pretty_xml = os.linesep.join([s for s in pretty_xml.splitlines()
+                                  if s.strip()])
+    with open(sys.argv[3], "w") as file1:
+        file1.writelines(pretty_xml)
+
+if __name__ == "__main__":
+    # First 2 args are files to combine.
+    combined_xml = combine(sys.argv[1:3])
+    # output file
+    outfile = sys.argv[3]
+    with open(outfile, "w") as f:
+        f.writelines(combined_xml)
+    pretty(outfile)
