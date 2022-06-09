61e0606d51a3f3520adb31ed7c9e741244e59270
Nicholas Lim <nicknitewolf@lineageos.org>
2022-06-10 00:40:07 +0800

    Soong: Exclude Bluetooth app if BOARD_HAVE_BLUETOOTH is not set
    Change-Id: Id5ff4b6c206d8b757d0a18989890dbb4b7b5ec41

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -109,5 +109,7 @@ SOONG_CONFIG_lineageQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAME
 endif
 
 ifneq ($(TARGET_USE_QTI_BT_STACK),true)
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
+endif #BOARD_HAVE_BLUETOOTH
 endif #TARGET_USE_QTI_BT_STACK
