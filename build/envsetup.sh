fbadddae84a2fb0cb8b5c3885c14df1fb78231e4
Michael Bestas <mkbestas@lineageos.org>
2022-06-09 18:50:47 +0300

    envsetup: Add sort-blobs-list
    Change-Id: Ie871fe0bc9dd432e632ca8e9d7fa70c96a82a8c3

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -18,6 +18,7 @@ Additional LineageOS functions:
 - repolastsync:    Prints date and time of last repo sync.
 - reposync:        Parallel repo sync using ionice and SCHED_BATCH.
 - repopick:        Utility to fetch changes from Gerrit.
+- sort-blobs-list: Sort proprietary-files.txt sections with LC_ALL=C.
 - installboot:     Installs a boot.img to the connected device.
 - installrecovery: Installs a recovery.img to the connected device.
 EOF
@@ -921,6 +922,11 @@ alias cmkap='dopush cmka'
 function repopick() {
     T=$(gettop)
     $T/vendor/lineage/build/tools/repopick.py $@
+}
+
+function sort-blobs-list() {
+    T=$(gettop)
+    $T/tools/extract-utils/sort-blobs-list.py $@
 }
 
 function fixup_common_out_dir() {
