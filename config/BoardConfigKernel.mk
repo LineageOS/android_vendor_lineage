1750a42826c0cd3190aa0908ea42270ab2dc1546
Alexander Koskovich <akoskovich@pm.me>
2022-06-15 23:40:11 -0700

    kernel: Drop -fuse-ld=lld from CFLAGS
    clang-14: error: argument unused during compilation:
    '-fuse-ld=lld' [-Werror,-Wunused-command-line-argument]
    
    Change-Id: Ic863e8707e4c57c7d03183b6cb6d3cd38e919bcb

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -136,9 +136,9 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),false)
 endif
 
 ifeq ($(HOST_OS),darwin)
-  KERNEL_MAKE_FLAGS += HOSTCFLAGS="-I$(BUILD_TOP)/external/elfutils/libelf -I/usr/local/opt/openssl/include -fuse-ld=lld" HOSTLDFLAGS="-L/usr/local/opt/openssl/lib -fuse-ld=lld"
+  KERNEL_MAKE_FLAGS += HOSTCFLAGS="-I$(BUILD_TOP)/external/elfutils/libelf -I/usr/local/opt/openssl/include" HOSTLDFLAGS="-L/usr/local/opt/openssl/lib -fuse-ld=lld"
 else
-  KERNEL_MAKE_FLAGS += CPATH="/usr/include:/usr/include/x86_64-linux-gnu" HOSTCFLAGS="-fuse-ld=lld" HOSTLDFLAGS="-L/usr/lib/x86_64-linux-gnu -L/usr/lib64 -fuse-ld=lld"
+  KERNEL_MAKE_FLAGS += CPATH="/usr/include:/usr/include/x86_64-linux-gnu" HOSTLDFLAGS="-L/usr/lib/x86_64-linux-gnu -L/usr/lib64 -fuse-ld=lld"
 endif
 
 TOOLS_PATH_OVERRIDE := \
