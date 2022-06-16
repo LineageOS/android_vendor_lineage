e75a08175def3ce4c30832eef68a54fdee1cb475
Alexander Koskovich <akoskovich@pm.me>
2022-06-15 22:50:47 -0700

    kernel: Switch to AOSP DTC instead of using scripts/dtc
    Change-Id: I8faf295dfb84ab7481b80d5b7d2add8ed8ddf6d9

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -259,6 +259,10 @@ PATH_OVERRIDE += PATH=$(KERNEL_TOOLCHAIN_PATH_gcc):$$PATH
 # System tools are no longer allowed on 10+
 PATH_OVERRIDE += $(TOOLS_PATH_OVERRIDE)
 
+ifeq (true,$(filter true, $(TARGET_NEEDS_DTBOIMAGE) $(BOARD_KERNEL_SEPARATED_DTBO)))
+    KERNEL_MAKE_FLAGS += DTC_EXT=$(KERNEL_BUILD_OUT_PREFIX)$(DTC)
+endif
+
 # Internal implementation of make-kernel-target
 # $(1): output path (The value passed to O=)
 # $(2): target to build (eg. defconfig, modules, dtbo.img)
