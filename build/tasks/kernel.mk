40ea7413ad85481e843af327c5e5a46d4c00f069
Neel Chauhan <neel@neelc.org>
2022-07-21 09:30:48 -0700

    kernel.mk: Allow building external kernel modules
    Here, we introduce the TARGET_EXTERNAL_MODULES variable for the list of
    external modules, and the TARGET_EXTERNAL_MODULE_PATH variable for the
    root of the external modules.
    
    Change-Id: I1f8b7a48e66bb0c9efbc396692e5a7159f0b835c

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -69,6 +69,9 @@
 #
 #   TARGET_FORCE_PREBUILT_KERNEL       = Optional, use TARGET_PREBUILT_KERNEL even if
 #                                          kernel sources are present
+#
+#   TARGET_EXTERNAL_MODULES            = Optional, the external modules we are
+#                                          building
 
 ifneq ($(TARGET_NO_KERNEL),true)
 ifneq ($(TARGET_NO_KERNEL_OVERRIDE),true)
@@ -275,6 +278,13 @@ define internal-make-kernel-target
 $(PATH_OVERRIDE) $(KERNEL_MAKE_CMD) $(KERNEL_MAKE_FLAGS) -C $(KERNEL_SRC) O=$(KERNEL_BUILD_OUT_PREFIX)$(1) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) $(KERNEL_CLANG_TRIPLE) $(KERNEL_CC) $(KERNEL_LD) $(2)
 endef
 
+# Make a external module target
+# $(1): source path (The value passed to M=)
+# $(2): target to build (eg. defconfig, modules, dtbo.img)
+define make-external-module-target
+$(PATH_OVERRIDE) $(KERNEL_MAKE_CMD) $(KERNEL_MAKE_FLAGS) -C ./ M=$(1) KERNEL_SRC=$(KERNEL_SRC) O=$(KERNEL_BUILD_OUT_PREFIX)$(KERNEL_OUT) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) $(KERNEL_CLANG_TRIPLE) $(KERNEL_CC) $(KERNEL_LD) $(2)
+endef
+
 # Generate kernel .config from a given defconfig
 # $(1): Output path (The value passed to O=)
 # $(2): The defconfig to process (just the filename, no need for full path to file)
@@ -409,6 +419,10 @@ $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_CONFIG) $(DEPMOD) $(DTC)
 	$(hide) if grep -q '=m' $(KERNEL_CONFIG); then \
 			echo "Building Kernel Modules"; \
 			$(call make-kernel-target,modules) || exit "$$?"; \
+			echo "Building External Kernel Modules"; \
+			$(foreach p, $(TARGET_EXTERNAL_MODULES),\
+				mkdir $(KERNEL_BUILD_OUT_PREFIX)$(KERNEL_OUT)/$(mpath); \
+				$(call make-external-module-target,$(p),modules)) \
 			echo "Installing Kernel Modules"; \
 			$(call make-kernel-target,INSTALL_MOD_PATH=$(MODULES_INTERMEDIATES) INSTALL_MOD_STRIP=1 modules_install); \
 			kernel_release=$$(cat $(KERNEL_RELEASE)) \
