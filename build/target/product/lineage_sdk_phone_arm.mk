37ffdef24fd167454fafb5952c14c1efb87370d7
althafvly <althafvly@gmail.com>
2022-07-30 08:42:52 +0530

    lineage: products: Add arm/arm64 phone emulator targets
    Change-Id: Id30dbde4da8671112e19c12a5bc64f54cef957d3

---
+++ build/target/product/lineage_sdk_phone_arm.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/sdk_phone_armv7.mk)
+
+include vendor/lineage/build/target/product/lineage_generic_target.mk
+
+TARGET_USES_64_BIT_BINDER := true
+
+# Enable mainline checking
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+
+# Overrides
+PRODUCT_NAME := lineage_sdk_phone_arm
+PRODUCT_MODEL := LineageOS Android SDK built for arm
+
+PRODUCT_SDK_ADDON_NAME := lineage
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
