6eb11f78835008dfd3b8ce96b5751843f09e5bd5
Alexander Koskovich <akoskovich@pm.me>
2022-08-04 13:00:07 -0700

    lineage: kernel: Use getconf instead of nproc
    Change-Id: Id8215f290a88ddf3a6bc1764d9ef9a09f6aa810d

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -108,7 +108,7 @@ endif
 KERNEL_MAKE_FLAGS :=
 
 # Add back threads, ninja cuts this to $(nproc)/2
-KERNEL_MAKE_FLAGS += -j$(shell prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin/nproc --all)
+KERNEL_MAKE_FLAGS += -j$(shell getconf _NPROCESSORS_ONLN)
 
 ifeq ($(TARGET_KERNEL_CLANG_COMPILE),false)
   ifeq ($(KERNEL_ARCH),arm)
