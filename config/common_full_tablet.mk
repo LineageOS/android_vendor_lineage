eec5c7137321f4fe2b6e7d1a430c24df4ed312d5
LuK1337 <priv.luk@gmail.com>
2022-08-07 21:30:09 +0200

    config: Exclude LatinIME dictionaries from RRO overlays
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4991
    Change-Id: I7722ac86d9d3f02f393b2c120341db5a2b648ff5
    (cherry picked from commit 7551e9ecddec2ce197b7ae48ac4767a55288f32f)

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,10 +1,16 @@
+<<<<<<< HEAD   (e6a75b apn: Update mcc525)
+=======
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
+    androidx.window.extensions \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
+
+$(call inherit-product, vendor/lineage/config/telephony.mk)
+>>>>>>> CHANGE (33fe58 config: Exclude LatinIME dictionaries from RRO overlays)
