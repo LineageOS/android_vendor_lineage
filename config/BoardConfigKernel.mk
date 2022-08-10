12b812b4c953e29ff3ed8868c733b1a893c3e365
Michael Bestas <mkbestas@lineageos.org>
2022-08-10 05:18:59 +0300

    kernel: Allow overriding dtbo target name
    gs101 kernel uses google/dtbo.img target for dtbo.img
    
    Change-Id: Ib03f35c0035f56d303771de9399bcce3d02c0775

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -31,6 +31,9 @@
 #                                        makefile. Can be overriden in device trees
 #                                        in the event of prebuilt kernel.
 #
+#   TARGET_KERNEL_DTBO                 = Name of the kernel Makefile target that
+#                                        generates dtbo.img. Defaults to dtbo.img
+#
 #   KERNEL_TOOLCHAIN_PREFIX            = Overrides TARGET_KERNEL_CROSS_COMPILE_PREFIX,
 #                                          Set this var in shell to override
 #                                          toolchain specified in BoardConfig.mk
@@ -141,7 +144,8 @@ endif
 
 # Set DTBO image locations so the build system knows to build them
 ifeq (true,$(filter true, $(TARGET_NEEDS_DTBOIMAGE) $(BOARD_KERNEL_SEPARATED_DTBO)))
-BOARD_PREBUILT_DTBOIMAGE ?= $(TARGET_OUT_INTERMEDIATES)/DTBO_OBJ/arch/$(KERNEL_ARCH)/boot/dtbo.img
+TARGET_KERNEL_DTBO ?= dtbo.img
+BOARD_PREBUILT_DTBOIMAGE ?= $(TARGET_OUT_INTERMEDIATES)/DTBO_OBJ/arch/$(KERNEL_ARCH)/boot/$(TARGET_KERNEL_DTBO)
 endif
 
 # Set use the full path to the make command
