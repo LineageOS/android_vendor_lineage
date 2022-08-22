c283ef2a48ea25f5857bd8beaa5272b3f77f3f58
Michael Bestas <mkbestas@lineageos.org>
2022-08-22 15:45:24 +0300

    Disable vendor/crowdin overlays for now
    Change-Id: Iebe001810f755eccd508165e45454b1a0a017a73

--- config/common.mk
+++ config/common.mk
@@ -200,8 +200,8 @@ PRODUCT_PACKAGES += \
     TrebuchetOverlay
 
 # Translations
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
+#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
