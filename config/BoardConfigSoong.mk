e020bffce2a1f5a0ba074cede2ce072221a5a6eb
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:44:38 +0300

    config: Remove TARGET_HAS_MEMFD_BACKPORT
    Change-Id: I17444007429f826bf3d7504cba760204c476e664

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,7 +31,6 @@ SOONG_CONFIG_lineageGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     disable_postrender_cleanup \
-    has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
@@ -61,7 +60,6 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
-SOONG_CONFIG_lineageGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_lineageGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_lineageGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
