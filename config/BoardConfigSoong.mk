de2a55c8d366e969a746e689a367a30b03dc3641
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:13:24 +0300

    config: Remove TARGET_CAMERA_BOOTTIME_TIMESTAMP
    This is no longer needed in T
    
    Change-Id: Id14c88a548c14a8c26858a13f262f355ba72745e

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -29,7 +29,6 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += lineageGlobalVars
 SOONG_CONFIG_lineageGlobalVars += \
     additional_gralloc_10_usage_bits \
-    needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
@@ -49,7 +48,6 @@ SOONG_CONFIG_lineageQcomVars += \
     uses_pre_uplink_features_netmgrd
 
 # Soong bool variables
-SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_lineageQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
