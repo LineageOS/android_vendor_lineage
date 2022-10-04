73aed359aac1a4f5e4f6a3eb5672588524245ddf
Edwin Moquete <edwinmmoquete@gmail.com>
2022-10-04 00:44:17 +0000

    soong: Add TARGET_POWERHAL_MODE_EXT
    Change-Id: If04d1f2bc04bddd65e32530c61d612c2b315b55d

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,6 +33,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
+    target_powerhal_mode_ext \
     target_surfaceflinger_udfps_lib \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
@@ -75,6 +76,7 @@ TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
 SOONG_CONFIG_lineageGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_lineageGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_lineageGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_lineageGlobalVars_target_powerhal_mode_ext := $(TARGET_POWERHAL_MODE_EXT)
 SOONG_CONFIG_lineageGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_lineageGlobalVars_target_trust_usb_control_path := $(TARGET_TRUST_USB_CONTROL_PATH)
 SOONG_CONFIG_lineageGlobalVars_target_trust_usb_control_enable := $(TARGET_TRUST_USB_CONTROL_ENABLE)
