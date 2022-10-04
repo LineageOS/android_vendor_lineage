43ba0d67a77b5c4d9730e493c18893f287f29b7b
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-03 21:16:56 -0400

    lineage: common: Exclude ntfs-3g from artifact path requirement
    Change-Id: I6253ac0b326f86ba7fd78f6babb9ee2242ba9133

--- config/common.mk
+++ config/common.mk
@@ -152,6 +152,13 @@ PRODUCT_PACKAGES += \
     mkfs.ntfs \
     mount.ntfs
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
