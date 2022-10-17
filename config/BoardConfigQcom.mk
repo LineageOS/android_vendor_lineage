22eb16e2defa6ea60ee420fa9f814239518ec1d6
SGCMarkus <markusornik@gmail.com>
2022-10-17 19:13:31 +0200

    qcom: Include vendor/qcom/opensource/display namespace only for <5.10 platforms
    hardware/qcom-caf/sm8450/display contains its own copies of
    * libgralloc.qti
    * libqdMetaData
    * libdisplayconfig.qti
    
    Including this namespace too, results in a module already defined error
    
    Change-Id: I7d5c861307f4d517d5b53c6444897a4b90f0095e

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -166,8 +166,13 @@ PRODUCT_SOONG_NAMESPACES += $(QCOM_SOONG_NAMESPACE)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys/display \
-    vendor/qcom/opensource/commonsys-intf/display \
+    vendor/qcom/opensource/commonsys-intf/display
+
+ifeq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/display
+endif
+
 endif
 
 # Add data-ipa-cfg-mgr to PRODUCT_SOONG_NAMESPACES if needed
