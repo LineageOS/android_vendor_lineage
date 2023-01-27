7053b108274e5b24c0a152e0aa9cb2d94c739854
TheScarastic <warabhishek@e.email>
2023-01-27 07:17:57 +0000

    vendor: build modules from source for emulators
    Change-Id: If551711178aa9b436576aa01a381c67c7a0f8eb9

--- build/target/product/lineage_sdk_phone_x86_64.mk
+++ build/target/product/lineage_sdk_phone_x86_64.mk
@@ -19,6 +19,8 @@ include vendor/lineage/build/target/product/lineage_generic_target.mk
 # Enable mainline checking
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 
+PRODUCT_MODULE_BUILD_FROM_SOURCE := true
+
 # Overrides
 PRODUCT_NAME := lineage_sdk_phone_x86_64
 PRODUCT_MODEL := LineageOS Android SDK built for x86_64
