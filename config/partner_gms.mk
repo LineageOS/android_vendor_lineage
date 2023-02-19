98fb219ce0dbe3c72cbb195635ed926f9aba5d9f
Cosmin Tanislav <demonsingur@gmail.com>
2023-02-19 23:42:24 +0200

    lineage: include GMS soong namespaces
    Change-Id: I8fd4f9d3efda9c46a364d9e25ffdc07bf8aed7b9

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -13,6 +13,7 @@ ifeq ($(WITH_GMS),true)
         $(call inherit-product, vendor/partner_gms-tv/products/mainline_modules.mk)
     # Special handling for Android Automotive
     else ifeq ($(PRODUCT_IS_AUTO),true)
+        PRODUCT_SOONG_NAMESPACES += vendor/partner_gms-car
         ifneq ($(GMS_MAKEFILE),)
             $(call inherit-product, vendor/partner_gms-car/products/$(GMS_MAKEFILE))
         else
@@ -26,6 +27,7 @@ ifeq ($(WITH_GMS),true)
         #   - gms_go_2gb.mk     - low ram devices (2GB)
         #   - gms_64bit_only.mk - devices supporting 64-bit only
         #   - gms_minimal.mk    - minimal GMS
+        PRODUCT_SOONG_NAMESPACES += vendor/partner_gms
         ifneq ($(GMS_MAKEFILE),)
             $(call inherit-product, vendor/partner_gms/products/$(GMS_MAKEFILE))
         else
