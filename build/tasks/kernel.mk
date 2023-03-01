ef68678120f9b4e77a119294cdb93f47b85e4b09
dianlujitao <dianlujitao@lineageos.org>
2023-03-01 23:06:18 +0800

    kernel: Force disable LLVM HIP
    Gets rid of the following from clang -v:
    
    ```
    Found HIP installation: /opt/rocm, version 5.4.22804
    ```
    
    Change-Id: Ic58b3fb2dd67d345372bec7f5d2cf023ed2fb34d

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -239,7 +239,7 @@ ifneq ($(TARGET_KERNEL_CLANG_COMPILE),false)
     endif
     PATH_OVERRIDE += PATH=$(TARGET_KERNEL_CLANG_PATH)/bin:$$PATH
     ifeq ($(KERNEL_CC),)
-        KERNEL_CC := CC="$(CCACHE_BIN) clang --cuda-path=/dev/null"
+        KERNEL_CC := CC="$(CCACHE_BIN) clang --cuda-path=/dev/null --hip-path=/dev/null"
     endif
 endif
 
