ad51abb08f28081b99076f76bafa314aab4eb1a7
LuK1337 <priv.luk@gmail.com>
2023-03-04 11:35:56 +0100

    roomservice: Use mirror manifest instead of search API
    Change-Id: Ifc64e42e1d443f36033756a6247a9f253b1ab82d

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -72,18 +72,18 @@ def add_auth(githubreq):
         githubreq.add_header("Authorization","Basic %s" % githubauth)
 
 if not depsonly:
-    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:LineageOS+in:name+fork:true" % device)
-    add_auth(githubreq)
+    githubreq = urllib.request.Request("https://raw.githubusercontent.com/LineageOS/mirror/master/default.xml")
     try:
-        result = json.loads(urllib.request.urlopen(githubreq).read().decode())
+        result = ElementTree.ElementTree(ElementTree.fromstring(
+            urllib.request.urlopen(githubreq).read().decode()))
     except urllib.error.URLError:
         print("Failed to search GitHub")
         sys.exit(1)
     except ValueError:
         print("Failed to parse return data from GitHub")
         sys.exit(1)
-    for res in result.get('items', []):
-        repositories.append(res)
+    for res in result.findall('//project'):
+        repositories.append(res.attrib['name'][10:])
 
 local_manifests = r'.repo/local_manifests'
 if not os.path.exists(local_manifests): os.makedirs(local_manifests)
@@ -289,11 +289,10 @@ if depsonly:
     sys.exit()
 
 else:
-    for repository in repositories:
-        repo_name = repository['name']
+    for repo_name in repositories:
         if re.match(r"^android_device_[^_]*_" + device + "$", repo_name):
-            print("Found repository: %s" % repository['name'])
-            
+            print("Found repository: %s" % repo_name)
+
             manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
             repo_path = "device/%s/%s" % (manufacturer, device)
             revision = get_default_or_fallback_revision(repo_name)
