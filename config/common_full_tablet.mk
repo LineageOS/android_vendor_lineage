330824d4ddf56f6e4f2107cbea2015ce2637f692
Tim Zimmermann <tim@linux4.de>
2023-03-14 07:05:52 +0100

    config: Inherit window_extensions.mk
    * Instead of explicitly building androidx.window.extensions
    
    Change-Id: I107ef8ef49eab77d91aa06fe3ade55eeaae83c71

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,9 +1,10 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    androidx.window.extensions \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,9 +1,10 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Inherit mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    androidx.window.extensions \
     LatinIME
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
