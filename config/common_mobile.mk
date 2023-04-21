86ecd32e7f9ddf1e38615a0488de3c5c6e5f8208
nift4 <nift4@protonmail.com>
2023-04-21 18:44:08 +0200

    Revert "Revert "config: Build NavigationBarMode2ButtonOverlay""
    * Fixed regression in Trebuchet
    
    This reverts commit 5ae7551a203fefe0fb6e8f01c9bee4b14f973149.
    
    Change-Id: I46a0d1d9bd25f20b333c51a76d96e5c695895983

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -85,7 +85,8 @@ PRODUCT_PACKAGES += \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
-    LineageNavigationBarNoHint
+    LineageNavigationBarNoHint \
+    NavigationBarMode2ButtonOverlay
 
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
