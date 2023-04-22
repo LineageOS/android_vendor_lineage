d7fa6a32e2ab84be54ec47427bfef2c429520035
DroidFreak32 <rushabshah32@gmail.com>
2023-04-22 15:50:22 +0530

    lineage: Fix SDK Addon build on x86/x86_64 targets
    - Fixes the following build error:
    
    100% 1/1] analyzing Android.bp files and generating ninja file at ou
    FAILED: out/soong/build.ninja
    cd "$(dirname "out/host/linux-x86/bin/soong_build")" && BUILDER="$PWD/$(basename "out/host/linux-x86/bin/soong_build")" && cd / && env -i  "$BUILDER"     --top "$TOP"     --soong_out "out/soong"     --out "out"     -o out/soong/build.ninja --globListDir build --globFile out/soong/globs-build.ninja -t -l out/.module_paths/Android.bp.list --available_env out/soong/soong.environment.available --used_env out/soong/soong.environment.used.build Android.bp
    error: packages/modules/RuntimeI18n/apex/Android.bp:67:1: dependency "art-bootclasspath-fragment" of "i18n-bootclasspath-fragment" missing variant:
      apex:com.android.art
    available variants:
      os:android,arch:common
    error: packages/modules/RuntimeI18n/apex/Android.bp:67:1: dependency "prebuilt_art-bootclasspath-fragment" of "i18n-bootclasspath-fragment" missing variant:
      apex:com.android.art
    available variants:
      os:android,arch:common
    15:24:42 soong bootstrap failed with: exit status 1
    
     #### failed to build some targets (19 seconds) ####
    
    Change-Id: Iff5aa79b4148f83680bc33bdd0ea9751038feea1

--- build/target/product/lineage_sdk_phone_x86.mk
+++ build/target/product/lineage_sdk_phone_x86.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,9 @@
 $(call inherit-product, build/target/product/sdk_phone_x86.mk)
 
 include vendor/lineage/build/target/product/lineage_generic_target.mk
+
+# Always build modules from source
+PRODUCT_MODULE_BUILD_FROM_SOURCE := true
 
 # Enable mainline checking
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
