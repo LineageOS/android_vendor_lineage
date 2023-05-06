dcf0e8899a3d2c7921de34a88b9abdfc209fb952
DroidFreak32 <rushabshah32@gmail.com>
2023-05-06 14:07:56 +0530

    config: common-full: Build zstd cmdline tools
    * Installs the following:
    unzstd -> zstd
    zstd
    zstdcat -> zstd
    zstdgrep
    zstdless
    zstdmt -> zstd
    
    Change-Id: I3e1efae8028a3ac5f6b9d153c9e39eef1d51a34b

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,6 +7,10 @@ PRODUCT_SIZE := full
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
+# Extra cmdline tools
+PRODUCT_PACKAGES += \
+    zstd
+
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
