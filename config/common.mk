6b1835c8485e90617d4fa66117a31a399051ef78
Nolen Johnson <johnsonnolen@gmail.com>
2023-05-17 14:02:28 -0400

    lineage: config: Allow using an env var to enable insecure ADB
    * Still requires key but most of us do this via vendor/extra anyway.
    
    Change-Id: Ib2d58af850d01a9c9d787be7a67b6d6b8c6ef899

--- config/common.mk
+++ config/common.mk
@@ -20,8 +20,13 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 else
+ifdef WITH_ADB_INSECURE
+# Forcebly disable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+else
 # Enable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
 endif
 
 # Backup Tool
