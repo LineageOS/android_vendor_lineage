8ce5ad47ca46b8dde59c207a624d801b9c701262
Nolen Johnson <johnsonnolen@gmail.com>
2023-05-17 13:28:36 -0400

    soong: Allow Permissive SELinux by make flag
    * Most of us make dumb bringup commits, instead just
      set this as an env var.
    
    Change-Id: I684d7d809605fdf912cbc00f804427412307f180

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -45,7 +45,8 @@ SOONG_CONFIG_lineageGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    with_selinux_permissive
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
 SOONG_CONFIG_lineageNvidiaVars += \
@@ -65,6 +66,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
+SOONG_CONFIG_lineageGlobalVars_with_selinux_permissive := $(WITH_SELINUX_PERMISSIVE)
 SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_lineageQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_lineageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
