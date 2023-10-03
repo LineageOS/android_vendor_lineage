b7a552dafdde3bf500e5dfe311b854a1c6a01267
Bruno Martins <bgcngm@gmail.com>
2023-10-03 11:38:15 +0100

    config: Add LineageOS specific languages via CUSTOM_LOCALES
    Change-Id: If9a8caad7a07ae1c133e0d05c71a8cacc64de92e

--- config/common.mk
+++ config/common.mk
@@ -209,6 +209,11 @@ PRODUCT_PACKAGES += \
     TrebuchetOverlay
 
 # Translations
+CUSTOM_LOCALES += \
+    ast_ES \
+    gd_GB \
+    cy_GB
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
