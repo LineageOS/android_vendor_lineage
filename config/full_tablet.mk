3f0c84c2c087cd7be99e196237a139fa35d1edb1
Davide Garberi <dade.garberi@gmail.com>
2023-10-24 14:23:20 +0200

    config: tablet: Enable freeform windows by default
    Change-Id: Id1ac898bede7b0c26d346af368704eba136d1ed5

--- config/full_tablet.mk
+++ config/full_tablet.mk
@@ -3,3 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 # Settings
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=true
+
+# Freeform window management
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
