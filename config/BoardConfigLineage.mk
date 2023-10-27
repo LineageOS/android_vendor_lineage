093daac0db044c8d94e3ffcd17bb162e40ef2114
Bruno Martins <bgcngm@gmail.com>
2023-10-27 09:54:39 +0100

    lineage: Move QCOM board configurations to hardware/qcom-caf/common
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,7 +1,7 @@
 include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/lineage/config/BoardConfigQcom.mk
+include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/lineage/config/BoardConfigSoong.mk
