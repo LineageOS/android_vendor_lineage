ebf86e1abe1591d999a69226a4cad24ad51f2da9
LuK1337 <priv.luk@gmail.com>
2024-01-08 12:21:43 +0100

    partner_gms: Skip makefile inclusion if vendor/partner_* doesn't exist
    This skips partner_* for some unfortunate devices where someone set
    GMS_MAKEFILE/MAINLINE_MODULES_MAKEFILE.
    
    Change-Id: Idadd7a5df315a3792237181b5c277d40d1bd1431

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,43 +1,51 @@
 ifeq ($(WITH_GMS),true)
     # Special handling for Android TV
     ifeq ($(PRODUCT_IS_ATV),true)
-        ifneq ($(GMS_MAKEFILE),)
-            # Specify the GMS makefile you want to use, for example:
-            #   - gms.mk            - default Android TV GMS
-            #   - gms_gtv.mk        - default Google TV GMS
-            #   - gms_minimal.mk    - minimal Android TV GMS
-            $(call inherit-product, vendor/partner_gms-tv/products/$(GMS_MAKEFILE))
-        else
-            $(call inherit-product, vendor/partner_gms-tv/products/gms.mk)
+        ifneq (,$(wildcard vendor/partner_gms-tv))
+            ifneq ($(GMS_MAKEFILE),)
+                # Specify the GMS makefile you want to use, for example:
+                #   - gms.mk            - default Android TV GMS
+                #   - gms_gtv.mk        - default Google TV GMS
+                #   - gms_minimal.mk    - minimal Android TV GMS
+                $(call inherit-product, vendor/partner_gms-tv/products/$(GMS_MAKEFILE))
+            else
+                $(call inherit-product, vendor/partner_gms-tv/products/gms.mk)
+            endif
+            $(call inherit-product, vendor/partner_gms-tv/products/mainline_modules.mk)
         endif
-        $(call inherit-product, vendor/partner_gms-tv/products/mainline_modules.mk)
     # Special handling for Android Automotive
     else ifeq ($(PRODUCT_IS_AUTO),true)
-        ifneq ($(GMS_MAKEFILE),)
-            $(call inherit-product, vendor/partner_gms-car/products/$(GMS_MAKEFILE))
-        else
-            $(call inherit-product, vendor/partner_gms-car/products/gms.mk)
+        ifneq (,$(wildcard vendor/partner_gms-car))
+            ifneq ($(GMS_MAKEFILE),)
+                $(call inherit-product, vendor/partner_gms-car/products/$(GMS_MAKEFILE))
+            else
+                $(call inherit-product, vendor/partner_gms-car/products/gms.mk)
+            endif
         endif
    else
-        # Specify the GMS makefile you want to use, for example:
-        #   - fi.mk             - Project Fi
-        #   - gms.mk            - default GMS
-        #   - gms_go.mk         - low ram devices
-        #   - gms_go_2gb.mk     - low ram devices (2GB)
-        #   - gms_64bit_only.mk - devices supporting 64-bit only
-        #   - gms_minimal.mk    - minimal GMS
-        ifneq ($(GMS_MAKEFILE),)
-            $(call inherit-product, vendor/partner_gms/products/$(GMS_MAKEFILE))
-        else
-            $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+        ifneq (,$(wildcard vendor/partner_gms))
+            # Specify the GMS makefile you want to use, for example:
+            #   - fi.mk             - Project Fi
+            #   - gms.mk            - default GMS
+            #   - gms_go.mk         - low ram devices
+            #   - gms_go_2gb.mk     - low ram devices (2GB)
+            #   - gms_64bit_only.mk - devices supporting 64-bit only
+            #   - gms_minimal.mk    - minimal GMS
+            ifneq ($(GMS_MAKEFILE),)
+                $(call inherit-product, vendor/partner_gms/products/$(GMS_MAKEFILE))
+            else
+                $(call inherit-product, vendor/partner_gms/products/gms.mk)
+            endif
         endif
 
-        # Specify the mainline module makefile you want to use, for example:
-        #   - mainline_modules.mk              - updatable apex
-        #   - mainline_modules_flatten_apex.mk - flatten apex
-        #   - mainline_modules_low_ram.mk      - low ram devices
-        ifneq ($(MAINLINE_MODULES_MAKEFILE),)
-            $(call inherit-product, vendor/partner_modules/build/$(MAINLINE_MODULES_MAKEFILE))
+        ifneq (,$(wildcard vendor/partner_modules))
+            # Specify the mainline module makefile you want to use, for example:
+            #   - mainline_modules.mk              - updatable apex
+            #   - mainline_modules_flatten_apex.mk - flatten apex
+            #   - mainline_modules_low_ram.mk      - low ram devices
+            ifneq ($(MAINLINE_MODULES_MAKEFILE),)
+                $(call inherit-product, vendor/partner_modules/build/$(MAINLINE_MODULES_MAKEFILE))
+            endif
         endif
     endif
 endif
