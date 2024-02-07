8abd59abc01e6337cf088719ffbfdc8c7a4a4f47
Nolen Johnson <johnsonnolen@gmail.com>
2024-02-07 15:29:50 -0500

    lineage: partner_gms: Allow ATV/Car GMS that isn't partner
    * Other inline packages rely on WITH_GMS for space reservations.
    
    Change-Id: Iacc4b01a49f7d44f800faff7bb7072f239e38495

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,6 +1,7 @@
 ifeq ($(WITH_GMS),true)
     # Special handling for Android TV
     ifeq ($(PRODUCT_IS_ATV),true)
+<<<<<<< HEAD   (4f27f7 vars: redfin,bramble: UP1A.231105.001.B2, Feb 2024)
         ifneq (,$(wildcard vendor/partner_gms-tv))
             ifneq ($(GMS_MAKEFILE),)
                 # Specify the GMS makefile you want to use, for example:
@@ -12,8 +13,19 @@ ifeq ($(WITH_GMS),true)
                 $(call inherit-product, vendor/partner_gms-tv/products/gms.mk)
             endif
             $(call inherit-product, vendor/partner_gms-tv/products/mainline_modules.mk)
+=======
+        ifneq ($(GMS_MAKEFILE),)
+            # Specify the GMS makefile you want to use, for example:
+            #   - gms.mk            - default Android TV GMS
+            #   - gms_gtv.mk        - default Google TV GMS
+            #   - gms_minimal.mk    - minimal Android TV GMS
+            $(call inherit-product, vendor/partner_gms-tv/products/$(GMS_MAKEFILE))
+        else
+            $(call inherit-product-if-exists, vendor/partner_gms-tv/products/gms.mk)
+>>>>>>> CHANGE (52bb8d lineage: partner_gms: Allow ATV/Car GMS that isn't partner)
         endif
     # Special handling for Android Automotive
+<<<<<<< HEAD   (4f27f7 vars: redfin,bramble: UP1A.231105.001.B2, Feb 2024)
     else ifeq ($(PRODUCT_IS_AUTOMOTIVE),true)
         ifneq (,$(wildcard vendor/partner_gms-car))
             ifneq ($(GMS_MAKEFILE),)
@@ -21,6 +33,13 @@ ifeq ($(WITH_GMS),true)
             else
                 $(call inherit-product, vendor/partner_gms-car/products/gms.mk)
             endif
+=======
+    else ifeq ($(PRODUCT_IS_AUTO),true)
+        ifneq ($(GMS_MAKEFILE),)
+            $(call inherit-product, vendor/partner_gms-car/products/$(GMS_MAKEFILE))
+        else
+            $(call inherit-product-if-exists, vendor/partner_gms-car/products/gms.mk)
+>>>>>>> CHANGE (52bb8d lineage: partner_gms: Allow ATV/Car GMS that isn't partner)
         endif
    else
         ifneq (,$(wildcard vendor/partner_gms))
