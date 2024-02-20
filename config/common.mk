4fd9063f459a16a82ac2c6dea1c7d423af46afdb
Michael Bestas <mkbestas@lineageos.org>
2024-02-20 17:48:07 +0200

    config: Define SetupWizard theme properties
    Change-Id: Ib35578aebc19d8cf2ef4cf3bc0b4acb3f04a89a1

--- config/common.mk
+++ config/common.mk
@@ -205,6 +205,11 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
 
+# SetupWizard
+PRODUCT_PRODUCT_PROPERTIES += \
+    setupwizard.theme=glif_v4 \
+    setupwizard.feature.day_night_mode_enabled=true
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/overlay/common \
