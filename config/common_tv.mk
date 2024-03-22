5d26fdd79730790f2c5f72205962ba7363c311ec
Michael Bestas <mkbestas@lineageos.org>
2024-03-23 01:54:48 +0200

    config: Include AudioTv.mk on atv
    Change-Id: Id6cf181c35032b44ab705b62bc9600f5b4f171bb

--- config/common_tv.mk
+++ config/common_tv.mk
@@ -1,6 +1,9 @@
 # Inherit common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common.mk)
 
+# Include AOSP audio files
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioTv.mk)
+
 # Inherit Lineage atv device tree
 $(call inherit-product, device/lineage/atv/lineage_atv.mk)
 
