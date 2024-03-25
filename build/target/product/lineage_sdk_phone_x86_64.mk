105b86ee09b43a15d2eb74ba41a0e58e816efe03
mse1969 <mse1969@posteo.de>
2024-03-25 17:11:04 +0100

    lineage: products: sdk_phone_x86_64 emulator - restore partition sizes
    With the merge of tag 'android-14.0.0_r29', the emulator sdk_phone_x86_64
    is inherited from device/generic/goldfish. Its defined super partition
    size of 1.6GB however is too small to build the LineageOS 21 emulator.
    
    Therefore, overwrite the partition sizes with the values as previously
    set in build/make/target/board/BoardConfigEmuCommon.mk
    
    Change-Id: Ib8443cb3d8cacb42cab68f154845574fadf99b55

--- build/target/product/lineage_sdk_phone_x86_64.mk
+++ build/target/product/lineage_sdk_phone_x86_64.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,3 +28,7 @@ PRODUCT_MODEL := LineageOS Android SDK built for x86_64
 
 PRODUCT_SDK_ADDON_NAME := lineage
 PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
+
+# Increase Partition size: 8G+8M
+BOARD_SUPER_PARTITION_SIZE ?= 8598323200
+BOARD_EMULATOR_DYNAMIC_PARTITIONS_SIZE ?= 8589934592
