c564d0a672cdbb678a8403d6dd1ff59154ff5def
Nolen Johnson <johnsonnolen@gmail.com>
2024-05-03 11:35:48 -0400

    common: config: Set ro.oem.key1 on ATV devices
    * This key is equivilant to `android-google` for mobile.
    
    Change-Id: I51e1c64dc016577fa0f9bf72a9eed9350e788cd2

--- config/common.mk
+++ config/common.mk
@@ -13,6 +13,16 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+ifeq ($(PRODUCT_IS_ATV),true)
+ifeq ($(PRODUCT_ATV_CLIENTID_BASE),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.oem.key1=ATV00100020
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase=$(PRODUCT_ATV_CLIENTID_BASE)
+endif
+endif
+
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
