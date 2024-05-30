9d4dc16c94484fb0588c64c0ef6644b1090f04e8
Bruno Martins <bgcngm@gmail.com>
2024-05-30 21:15:12 +0100

    config: Indent BoardConfig makefiles
    Change-Id: I56c93a0fe64c99987ad4101e7e6ccdd6250828c4

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,10 +1,13 @@
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
 # Recovery
 BOARD_USES_FULL_RECOVERY_IMAGE ?= true
 
 include vendor/lineage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include hardware/qcom-caf/common/BoardConfigQcom.mk
+    include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/lineage/config/BoardConfigSoong.mk
