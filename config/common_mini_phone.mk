6de2fafbbd7124e2f758eb97cd55ce2ce4bf8f52
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 15:45:31 +0200

    config: Rename mobile-specific configs
    Since common_full.mk and common_mini.mk are mobile-specific, rename them
    
    Change-Id: I2c47fd0688350ac815ccecbff7b453d7fab5d5ea

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,5 +1,5 @@
-# Inherit mini common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mini.mk)
+# Inherit mobile mini common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
