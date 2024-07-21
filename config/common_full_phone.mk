c075ec6d827d105cf645d185fba110e2dada0989
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 20:27:31 +0200

    config: Move LatinIME stuff to common_mobile*.mk
    Change-Id: I5697e1cef84972a6a7f76b8c065e575e1ba17ef8

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,14 +1,6 @@
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
-# Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
-
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true
