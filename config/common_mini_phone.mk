a75d417d3f7410d3f00ea20526b973ac4614ad5f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 20:27:31 +0200

    config: Move LatinIME stuff to common_mobile*.mk
    Change-Id: I5697e1cef84972a6a7f76b8c065e575e1ba17ef8

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,8 +1,4 @@
 # Inherit mobile mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
 $(call inherit-product, vendor/lineage/config/telephony.mk)
