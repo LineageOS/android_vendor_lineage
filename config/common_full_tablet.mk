4e89bdb1ff50aa2791322b5e2b673989c9c4913f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 21:04:37 +0200

    config: tablet: Deduplicate full tablet configuration
    Change-Id: I12cc09d4fca28d063cea76301de287526bfec570

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,10 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/full_tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
