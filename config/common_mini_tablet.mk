0c4fb73bf576310d43ff3c2441de076b1fd6611a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 15:45:31 +0200

    config: Dedup tablet stuff
    Change-Id: I2c47fd0688350ac815ccecbff7b453d7fab5d5ea

--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,8 +1,4 @@
 # Inherit mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mini.mk)
 
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
 $(call inherit-product, vendor/lineage/config/telephony.mk)
---
+++ config/tablet.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
+# Inherit full common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full.mk)
+
+# Required packages
+PRODUCT_PACKAGES += \
+    LatinIME
+
+# Include Lineage LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
+
+# Settings
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.settings.large_screen_opt.enabled=true
