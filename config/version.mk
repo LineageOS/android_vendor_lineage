2224749b73bebac609c53e0059f9d34eea95f3c6
Michael Bestas <mkbestas@gmail.com>
2024-09-12 03:51:01 +0300

    build: Move version & legal properties to product
    ADDITIONAL_SYSTEM_PROPERTIES is being deprecated.
    No functional changes.
    
    Change-Id: I01fd45edcc6c700540d3b7352fc466e65677a44e

--- config/version.mk
+++ config/version.mk
@@ -36,3 +36,11 @@ LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_V
 
 # Display version
 LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR)-$(LINEAGE_VERSION_SUFFIX)
+
+# LineageOS version properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lineage.version=$(LINEAGE_VERSION) \
+    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.modversion=$(LINEAGE_VERSION)
