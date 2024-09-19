ccef4860127ba1de5a0ad2e15aaf162ca88c9ebe
Aaron Kling <webgeek1234@gmail.com>
2024-09-19 18:01:32 -0500

    Disable debug logging on non-eng builds
    Debug and verbose logging is typically very chatty to the point of
    slowing down a device. Since Lineage ships userdebug, debug logging
    shouldn't be on by default on userdebug. Setting the persist log tag
    properties can be done at runtime for debugging purposes when needed.
    
    Change-Id: I2e5ec041bb0971b4be160fa889b140aec56dc96f

--- config/common.mk
+++ config/common.mk
@@ -32,6 +32,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+
+# Disable debug and verbose logging by default
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += log.tag=I
 endif
 
 # Backup Tool
