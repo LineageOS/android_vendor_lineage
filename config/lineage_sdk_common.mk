6180ee87801c7296aafebd1981be5a1df13872ce
Michael Bestas <mkbestas@lineageos.org>
2024-09-22 15:38:59 +0300

    build: Drop lineage sdk versions
    Change-Id: I2448822be41835723995734b4e20f191f3ce8987

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -20,20 +20,3 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/framework/oat/%/org.lineageos.platform.vdex \
     system/framework/org.lineageos.platform-res.apk \
     system/framework/org.lineageos.platform.jar
-
-ifndef LINEAGE_PLATFORM_SDK_VERSION
-  # This is the canonical definition of the SDK version, which defines
-  # the set of APIs and functionality available in the platform.  It
-  # is a single integer that increases monotonically as updates to
-  # the SDK are released.  It should only be incremented when the APIs for
-  # the new release are frozen (so that developers don't write apps against
-  # intermediate builds).
-  LINEAGE_PLATFORM_SDK_VERSION := 9
-endif
-
-ifndef LINEAGE_PLATFORM_REV
-  # For internal SDK revisions that are hotfixed/patched
-  # Reset after each LINEAGE_PLATFORM_SDK_VERSION release
-  # If you are doing a release and this is NOT 0, you are almost certainly doing it wrong
-  LINEAGE_PLATFORM_REV := 0
-endif
