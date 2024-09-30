dc34f77675b20e2fc8b998598cba773c991d6fbb
Michael Bestas <mkbestas@lineageos.org>
2024-09-30 18:47:04 +0300

    kernel: Nuke --cuda-path/--hip-path
    Revert "kernel: Check HIP support of clang before disabling it"
    
    This reverts commit 62c1374385b31f33e1ca02d394bc8d6f5ede97a6.
    
    Revert "kernel: Force disable LLVM HIP"
    
    This reverts commit ef68678120f9b4e77a119294cdb93f47b85e4b09.
    
    Revert "kernel: Force disable LLVM CUDA"
    
    This reverts commit ba14020f85e6923c0c983ba1a916d0931f11541e.
    
    Change-Id: I5cb2b4624eb0ec25dbc8c1b5888708d961ae59d7

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -249,11 +249,7 @@ ifneq ($(TARGET_KERNEL_CLANG_COMPILE),false)
     endif
     PATH_OVERRIDE += PATH=$(TARGET_KERNEL_CLANG_PATH)/bin:$$PATH
     ifeq ($(KERNEL_CC),)
-        CLANG_EXTRA_FLAGS := --cuda-path=/dev/null
-        ifeq ($(shell $(TARGET_KERNEL_CLANG_PATH)/bin/clang -v --hip-path=/dev/null >/dev/null 2>&1; echo $$?),0)
-            CLANG_EXTRA_FLAGS += --hip-path=/dev/null
-        endif
-        KERNEL_CC := CC="$(CCACHE_BIN) clang $(CLANG_EXTRA_FLAGS)"
+        KERNEL_CC := CC="$(CCACHE_BIN) clang"
     endif
 endif
 
