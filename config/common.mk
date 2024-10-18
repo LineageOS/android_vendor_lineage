b9f088abc24342833f53e113044e1b6479f366a4
LuK1337 <priv.luk@gmail.com>
2024-10-19 01:48:34 +0200

    common: Update date UTC prop override key
    Change-Id: Ic8779d67778174a7b04f8b79f26b0b3c39556bae

--- config/common.mk
+++ config/common.mk
@@ -3,7 +3,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 
 PRODUCT_BRAND ?= LineageOS
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_BUILD_PROP_OVERRIDES += DateUtc=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
