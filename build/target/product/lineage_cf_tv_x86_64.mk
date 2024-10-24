c665bcbc7596de965596b5362540207b2834b98b
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-24 18:48:53 +0800

    lineage: products: Add Cuttlefish products
    How-to:
    1. Build and install cuttlefish packages by following [1]
    2. Build LineageOS image and hosttar
        ```
        source build/envsetup.sh
        breakfast cf_phone_x86_64
        m hosttar updatepackage
        ```
    3. Extract LineageOS image and hosttar
        ```
        mkdir lineage-cvd
        cd lineage-cvd
        unzip <LineageOS tree>/out/target/product/vsoc_x86_64/lineage_cf_phone_x86_64-img.zip
        tar xzvf <LineageOS tree>/out/host/linux-x86/cvd-host_package.tar.gz
        ```
    4. Run it
        ```
        HOME=$PWD ./bin/launch_cvd
        ```
    5. Open https://127.0.0.1:8443 in browser, and enjoy
    
    [1]: https://github.com/google/android-cuttlefish?tab=readme-ov-file#debian-packages
    
    Change-Id: I7d69db7be99d250f02d7e76fd398ebf5c2ca7c5f

---
+++ build/target/product/lineage_cf_tv_x86_64.mk
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/google/cuttlefish/vsoc_x86_64/tv/aosp_cf.mk)
+
+include vendor/lineage/build/target/product/lineage_generic_tv_target.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+
+# Overrides
+PRODUCT_NAME := lineage_cf_tv_x86_64
+PRODUCT_MODEL := LineageOS Cuttlefish TV built for x86_64
