f46d33a37eabb7c984d2bacf1b5d4d8f26fa1eb2
Ivan Vecera <ivan@cera.cz>
2024-12-16 11:45:49 +0100

    vendor/lineage: Hookup TARGET_CAMERA_IDS_REMAP_PROPERTY
    The change I13a3bd186e7fc48e797e3c6f9039a1dae1fb733f allows to define a name of property
    that contains a string of camera IDs separated by space. This property is usually set
    by camera HAL and its value defines a mapping of camera IDs.
    
    E.g. on Pyxis:
    pyxis:/ $ getprop persist.vendor.camera.xiaomi.remapid
    0 1 21 20 62 60 63 61 91 90 100 101
    
    This example defines mapping: 0->0, 1->1, 2->21, 3->20, ...
    
    Add a new soong config module to handle TARGET_CAMERA_IDS_REMAP_PROPERTY variable.
    
    Change-Id: Ie293254dc7b9af235654537d3f712f6431d57ee8

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,6 +34,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
     gralloc_handle_has_ubwcp_format \
+    target_camera_ids_remap_property \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -107,6 +108,7 @@ TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
 # Soong value variables
 SOONG_CONFIG_lineageGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_lineageGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
+SOONG_CONFIG_lineageGlobalVars_target_camera_ids_remap_property := $(TARGET_CAMERA_IDS_REMAP_PROPERTY)
 SOONG_CONFIG_lineageGlobalVars_target_health_charging_control_charging_path := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH)
 SOONG_CONFIG_lineageGlobalVars_target_health_charging_control_charging_enabled := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED)
 SOONG_CONFIG_lineageGlobalVars_target_health_charging_control_charging_disabled := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED)
