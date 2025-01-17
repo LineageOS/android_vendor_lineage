84b11b17c5e414f70100db4bbd72d5c2d1d6fdc7
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-17 22:05:52 +0100

    oong: Add mkdir module
    Change-Id: I7b98cf5b120817a271036fca3aa0582d77861993

---
+++ build/soong/mkdir/mkdir.go
@@ -0,0 +1,66 @@
+// Copyright 2025 The LineageOS Project.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mkdir
+
+import (
+	"android/soong/android"
+
+	"fmt"
+)
+
+func MkdirFactory() android.Module {
+	module := &Mkdir{}
+	module.AddProperties(&module.properties)
+	android.InitAndroidArchModule(module, android.DeviceSupported, android.MultilibCommon)
+	return module
+}
+
+type MkdirProperties struct {
+	Dir string `android:"arch_variant"`
+}
+
+type Mkdir struct {
+	android.ModuleBase
+
+	properties MkdirProperties
+
+	output android.ModuleOutPath
+}
+
+func (this *Mkdir) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	installDir := android.PathForModuleInstall(ctx, "", this.properties.Dir)
+	this.output = android.PathForModuleOut(ctx, "empty")
+
+	builder := android.NewRuleBuilder(pctx, ctx)
+	builder.Command().Textf("rm -rf %s && mkdir -p %s", installDir, installDir)
+	builder.Command().
+		Textf("rm -f %s && touch", this.output).
+		Output(this.output)
+	builder.Build("mkdir", fmt.Sprintf("mkdir %s", ctx.ModuleName()))
+}
+
+func (this *Mkdir) AndroidMkEntries() []android.AndroidMkEntries {
+	return []android.AndroidMkEntries{{
+		Class: "FAKE",
+		// Need at least one output file in order for this to take effect.
+		OutputFile: android.OptionalPathForPath(this.output),
+		Include:    "$(BUILD_PHONY_PACKAGE)",
+		ExtraEntries: []android.AndroidMkExtraEntriesFunc{
+			func(ctx android.AndroidMkExtraEntriesContext, entries *android.AndroidMkEntries) {
+				entries.AddStrings("LOCAL_ADDITIONAL_DEPENDENCIES", this.output.String())
+			},
+		},
+	}}
+}
