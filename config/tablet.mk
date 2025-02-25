d11d5da4416bf71efea25ab98288614cc29ee054
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-25 22:20:10 +0800

    config: tablet: Include tablet-specific overlay
    Change-Id: I8235aad467d643b06d0ad1dd2c7c0de24bdf4679

--- config/tablet.mk
+++ config/tablet.mk
@@ -7,3 +7,6 @@ PRODUCT_COPY_FILES += \
 # Settings
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=true
+
+# Tablet-specific overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/tablet
