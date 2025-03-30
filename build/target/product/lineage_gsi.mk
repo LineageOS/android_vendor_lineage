79629643d74ddb099ee4b69d84d69d5aa82e463c
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-03-30 12:55:14 +0200

    Switch lineage GSI to soong
    Change-Id: I63223fc2d1ae10b6efd1f4b52def327de462656f

---
+++ build/target/product/lineage_gsi.mk
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_SOONG_DEFINED_SYSTEM_IMAGE := lineage_gsi
+PRODUCT_USE_SOONG_NOTICE_XML := true
+USE_SOONG_DEFINED_SYSTEM_IMAGE := true
