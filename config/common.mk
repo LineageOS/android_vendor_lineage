ec3e54308a3e33e0551af0a017a344cd50f1bbb9
Bharath <bharath@teamb58.org>
2025-04-26 11:05:49 +0530

    Add software credentials permission support
    This is now required for all handheld devices to make use of the
    new software credentials feature
    
    Bug: 344511668
    Test: run cts -m CtsCredentialManagerTestCases -t android.credentials.cts.CtsCredentialManagerDeviceTest
    Change-Id: I7e0211ca5c7f817789d9b33a6a0a1a25054530b4

--- config/common.mk
+++ config/common.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
 
+# Credential storage
+PRODUCT_PACKAGES += \
+    android.software.credentials.prebuilt.xml
+
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
