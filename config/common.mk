986007af819491c75d4fb13c984b42f85b12de80
techyminati <sinha.aryan03@gmail.com>
2025-06-14 23:30:42 +0800

    lineage: Enable Material Design 3 Expressive
    Change-Id: Ib6d3ac2e0f3d76768c508e1b00efc51e141c1b09
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -37,6 +37,10 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
