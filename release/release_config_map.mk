eb3f9aaf88a6a32c45f8c4487091bde5bad8ee5d
basamaryan <basam.aryan@gmail.com>
2025-06-14 21:56:40 -0400

    lineage: Bump aconfig flags extension to BP2A
    Change-Id: I7ee78cbc5ea3b48b85631bd275f9af6aca7940db

--- release/release_config_map.mk
+++ release/release_config_map.mk
@@ -17,6 +17,6 @@
 local_dir := $(dir $(lastword $(MAKEFILE_LIST)))
 
 # Attach the flag value definitions to the various release configurations.
-$(call declare-release-config, bp1a, $(local_dir)build_config/bp1a.scl)
+$(call declare-release-config, bp2a, $(local_dir)build_config/bp2a.scl)
 
 local_dir :=
