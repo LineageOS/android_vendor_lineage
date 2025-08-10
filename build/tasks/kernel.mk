767fd5f733063b62ad2c66844bdc23650a9fbe4a
LuK1337 <priv.luk@gmail.com>
2025-08-10 13:39:53 +0200

    kernel: Introduce macros for comparing versions and make use of them
    Change-Id: Ie7debfea218117987f7e9588a71447061db08416

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -107,8 +107,8 @@ KERNEL_RELEASE := $(KERNEL_OUT)/include/config/kernel.release
 RECOVERY_KERNEL_CONFIG := $(RECOVERY_KERNEL_OUT)/.config
 RECOVERY_KERNEL_RELEASE := $(RECOVERY_KERNEL_OUT)/include/config/kernel.release
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifeq ($(shell expr $(TARGET_KERNEL_VERSION) \>= 5.15), 1)
-ifeq ($(shell expr $(TARGET_KERNEL_VERSION) \<= 6.1), 1)
+ifeq ($(call is-version-greater-or-equal,$(TARGET_KERNEL_VERSION),5.15), 1)
+ifeq ($(call is-version-lower-or-equal,$(TARGET_KERNEL_VERSION),6.1), 1)
 GKI_SUFFIX := /$(shell echo android$(PLATFORM_VERSION)-$(TARGET_KERNEL_VERSION))
 endif
 endif
