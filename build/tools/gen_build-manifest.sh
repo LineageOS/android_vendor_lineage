fcb75a8befc2ba2b9c847e99608a3645bfde138b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-25 01:33:10 +0800

    lineage: Restore Android.mk based build-manifest.xml build rule
    Users don't like the increased delay during `. build/envsetup.sh` or `lunch`...
    
    Change-Id: I1a92a171f2e2ec1ba2453d6c666cefcd9884b39b

--- build/tools/gen_build-manifest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
-echo "- Generating vendor/lineage/prebuilt/generated/build-manifest.xml"
-if [ "$MANIFEST_EXCLUDES" ]; then MANIFEST_EXCLUDES="|${MANIFEST_EXCLUDES}"; fi
-python3 .repo/repo/repo manifest -o - -r | grep -Ev "proprietary_${MANIFEST_EXCLUDES}" > vendor/lineage/prebuilt/generated/build-manifest.xml
