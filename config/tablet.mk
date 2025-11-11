08a34e6bff0b14af6a2d268936ee1e381e37740c
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-11 13:35:40 +0800

    config: tablet: Bring back `android.software.freeform_window_management` feature
    * desktop windowing mode depends on it
    
    Change-Id: Iffdab6b268a3a3ac0fb78027c4900a3fef1eeac5

--- config/tablet.mk
+++ config/tablet.mk
@@ -1,5 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
+# Freeform window management
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
 # Settings
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=true
