c5e1d244395379760159a808edf5bc8f3d7fe7fe
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 02:16:18 +0200

    config: Build LocalContactsBackup
    Change-Id: I71dee43995ca2910ecd2ff824aa6691340516471

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -11,6 +11,7 @@ $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 PRODUCT_PACKAGES += \
     Camelot \
     Etar \
+    LocalContactsBackup \
     Profiles \
     Recorder \
     Seedvault \
