dcffba12f91fbc2118483d0f430b01087d6faa67
Bruno Martins <bgcngm@gmail.com>
2025-12-13 13:19:46 +0000

    config: tablet: Disable desktop windowing mode
    Looks really bad on tablets since BP4A.
    
    Change-Id: I79fb7a5366af60fb0b2a9b5f8ca45da812ea0179

--- config/tablet.mk
+++ config/tablet.mk
@@ -1,8 +1,4 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/large_screen_common.mk)
 
-# Freeform window management
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
-
 # Tablet-specific overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/tablet
