73983ea811f83f3153a250ff703c89d0eab3e243
Akash Suresh <akashsrocks1234@gmail.com>
2026-02-15 12:58:55 +0530

    common.mk: Use prebuilt_addon_d for installing 50-lineage.sh
    - Use the custom "prebuilt_addon_d" module to install 50-lineage.sh.
    - This allows the stricter artifact path requirements introduced in Android 16 QPR2 to be met.
    - The checker in build/soong/fsgen/artifact_path_requirements.go does not recognize PRODUCT_COPY_FILES.
    
    Change-Id: I1f8cc49fa238d227fc64867f69f1970823c46de9

--- config/common.mk
+++ config/common.mk
@@ -46,8 +46,10 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/lineage/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/lineage/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
+    vendor/lineage/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
+
+PRODUCT_PACKAGES += \
+    50-lineage.sh
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/addon.d/50-lineage.sh
