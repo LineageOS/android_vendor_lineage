364682f3bd716840b4df38cac87baf34fb9b5b5c
AnierinB <anierin@evolution-x.org>
2026-06-01 00:15:42 +0000

    overlay: console: Add ConsoleOverlays
    Sets phone/tablet strings to console.
    
    Change-Id: Ic22d54426d89f74f8e559f8f9f12e2db093a3ce6
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/common_full_handheld_console_wifionly.mk
+++ config/common_full_handheld_console_wifionly.mk
@@ -1,2 +1,11 @@
 # Inherit tablet full wifi only common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Console strings
+PRODUCT_PACKAGES += \
+    CompanionDeviceManagerConsoleOverlay \
+    FrameworksConsoleOverlay \
+    PackageInstallerConsoleOverlay \
+    SettingsConsoleOverlay \
+    StorageManagerConsoleOverlay \
+    SystemUIConsoleOverlay
