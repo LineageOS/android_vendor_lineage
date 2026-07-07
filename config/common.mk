7588a4466c7ed8332d457a69a0f16f983d8a82f3
LuK1337 <priv.luk@gmail.com>
2026-07-07 22:13:26 +0200

    lineage: Introduce simple program for disabling adb remount overlays
    Useful for unscrewing your phone without reflashing it.
    
    Change-Id: I10082f18eb957c55e2905ba12cfc357ae289e1c2

--- config/common.mk
+++ config/common.mk
@@ -224,6 +224,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# OverlayFS
+PRODUCT_PACKAGES += \
+    disable-overlays
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
