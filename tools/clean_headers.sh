d20cd3044b5ba7f4f638e66a02ea784183b9cf5b
Alexander Koskovich <akoskovich@pm.me>
2026-07-17 00:33:12 -0400

    tools: clean_headers: Drop asm and asm-generic from kernel headers
    The generated headers are arm64's and get picked up via -I ahead of
    bionic's per-ABI asm headers, breaking 32-bit userspace compiles.
    
    Change-Id: Ie691e2f1784c9ab539839c7003fe0cf65b33b2fa

--- tools/clean_headers.sh
+++ tools/clean_headers.sh
@@ -3,7 +3,7 @@ set -eu
 
 export ANDROID_BUILD_TOP="$PWD"
 
+rm -rf "$1/usr/include/asm" "$1/usr/include/asm-generic"
+
 ./bionic/libc/kernel/tools/clean_header.py -u \
-    "$1/usr/include/asm/signal.h" \
-    "$1/usr/include/asm-generic/signal.h" \
     "$1/usr/include/linux/eventpoll.h"
