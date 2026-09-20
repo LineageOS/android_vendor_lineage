a213bcc00b74843f35519d6a85cce46627c9cd6c
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-20 12:10:56 -0400

    lineage: products: Set TARGET_SUPPORTS_32_BIT_APPS for ARM GSIs
    The GMS (and everywhere else) way to discern if something is a
    32-bit target.
    
    Change-Id: I3769fc40b1fb99f802b7d73f17801e671b84a3ad

--- build/target/product/lineage_gsi_arm.mk
+++ build/target/product/lineage_gsi_arm.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_SUPPORTS_32_BIT_APPS := true
+
 $(call inherit-product, device/generic/common/gsi_arm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
