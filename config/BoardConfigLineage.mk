cfa5abb1fe1b6233ff5615998a8a548f2df23f01
LuK1337 <priv.luk@gmail.com>
2026-09-23 20:27:43 +0200

    config: Make .repo/manifests.git writable in sandbox
    Change-Id: I27755b2fc2da4eb3644e226d62a194b0f8e50d8a

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,6 +1,9 @@
 # SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
+# build-manifest.xml
+BUILD_BROKEN_SRC_DIR_RW_ALLOWLIST := $(abspath .repo/manifests.git)
+
 # Recovery
 BOARD_USES_FULL_RECOVERY_IMAGE ?= true
 
